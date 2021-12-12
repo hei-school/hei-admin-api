91b25e2a4b1e2982a64623c6b3923f31dcac1d5a
HEI <contact@hei.school>
2021-12-12 13:33:55 +0100

    chore: generate rest.model and client and api with openapi-generator

--- src/main/java/school/hei/haapi/endpoint/rest/controller/StudentController.java
+++ src/main/java/school/hei/haapi/endpoint/rest/controller/StudentController.java
@@ -5,8 +5,8 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import school.hei.haapi.endpoint.model.StudentResource;
 import school.hei.haapi.endpoint.rest.mapper.UserMapper;
+import school.hei.haapi.endpoint.rest.model.Student;
 import school.hei.haapi.endpoint.rest.security.model.ApiClient;
 import school.hei.haapi.endpoint.rest.security.model.Role;
 import school.hei.haapi.model.User;
@@ -21,7 +21,7 @@ public class StudentController {
   private final UserMapper userMapper;
 
   @GetMapping(value = "/students/{id}")
-  public StudentResource getStudentById(
+  public Student getStudentById(
       @AuthenticationPrincipal ApiClient client, @PathVariable String id) {
     User user = userService.getById(id);
     String clientRole = client.getRole();
@@ -34,6 +34,6 @@ public class StudentController {
         throw new ForbiddenException("Students can only get their own information");
       }
     }
-    return userMapper.toExternalStudent(user);
+    return userMapper.toRestStudent(user);
   }
 }
