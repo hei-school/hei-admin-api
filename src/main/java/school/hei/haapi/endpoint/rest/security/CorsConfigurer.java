849fb4dccd4175cc92e39ca840d6899a0972d46e
HEI <contact@hei.school>
2021-12-27 23:46:06 +0100

    feat: cors

---
+++ src/main/java/school/hei/haapi/endpoint/rest/security/CorsConfigurer.java
@@ -0,0 +1,13 @@
+package school.hei.haapi.endpoint.rest.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfigurer implements WebMvcConfigurer {
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**");
+  }
+}
