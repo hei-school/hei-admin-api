2ae74eb7c9f98aa231d0b0ae6930026e8326558e
HEI <contact@hei.school>
2022-01-12 23:34:19 +0100

    fix: spring.EnableScheduling

--- src/main/java/school/hei/haapi/HaApiApplication.java
+++ src/main/java/school/hei/haapi/HaApiApplication.java
@@ -2,8 +2,10 @@ package school.hei.haapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class HaApiApplication {
   public static void main(String[] args) {
     SpringApplication.run(HaApiApplication.class, args);
