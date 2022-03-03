60184695156da9b1df525d1a68d15827c6b1ef78
Ryan Andriamahery <ryan@hei.school>
2022-03-03 23:14:22 +0300

    feat: POST and GET /students/{studentId}/fees

--- src/main/java/school/hei/haapi/service/FeeService.java
+++ src/main/java/school/hei/haapi/service/FeeService.java
@@ -1,8 +1,12 @@
 package school.hei.haapi.service;
 
+import java.util.List;
+import javax.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import school.hei.haapi.model.Fee;
+import school.hei.haapi.model.exception.BadRequestException;
+import school.hei.haapi.model.validator.FeeValidator;
 import school.hei.haapi.repository.FeeRepository;
 
 @Service
@@ -10,8 +14,46 @@ import school.hei.haapi.repository.FeeRepository;
 public class FeeService {
 
   private final FeeRepository feeRepository;
+  private final FeeValidator feeValidator;
 
-  public Fee getByUserIdAndId(String userId, String feeId) {
-    return feeRepository.getByUserIdAndId(userId, feeId);
+  public Fee getByStudentIdAndFeeId(String userId, String feeId) {
+    return feeRepository.getByStudentIdAndId(userId, feeId);
+  }
+
+  @Transactional
+  public List<Fee> saveAll(String studentId, List<Fee> fees) {
+    checkStudentFees(studentId, fees);
+    for (Fee f : fees) {
+      f.setRemainingAmount(computeRemainingAmount(f));
+      f.setStatus(getFeeStatus(f));
+    }
+    return feeRepository.saveAll(fees);
+  }
+
+  public List<Fee> getFeesByStudentId(String studentId) {
+    return feeRepository.getByStudentId(studentId);
+  }
+
+  private int computeRemainingAmount(Fee fee) {
+    //TODO : remaining amount is computed from payment status associated to the fee
+    return fee.getTotalAmount(); //By default return total amount
+  }
+
+  private school.hei.haapi.endpoint.rest.model.Fee.StatusEnum getFeeStatus(Fee fee) {
+    //TODO : remaining amount is computed from payment status associated to the fee
+    return school.hei.haapi.endpoint.rest.model.Fee.StatusEnum.UNPAID; //By default return UNPAID
+  }
+
+  private void checkStudentFees(String studentId, Fee toCheck) {
+    if (!studentId.equals(toCheck.getStudentId())) {
+      throw new BadRequestException(
+          "Fee must be associated to student." + toCheck.getStudentId() + " instead of student."
+              + studentId);
+    }
+  }
+
+  private void checkStudentFees(String studentId, List<Fee> fees) {
+    feeValidator.accept(fees);
+    fees.forEach(fee -> checkStudentFees(studentId, fee));
   }
 }
--- src/test/java/school/hei/haapi/integration/FeeIT.java
+++ src/test/java/school/hei/haapi/integration/FeeIT.java
@@ -1,6 +1,8 @@
 package school.hei.haapi.integration;
 
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -12,22 +14,26 @@ import school.hei.haapi.SentryConf;
 import school.hei.haapi.endpoint.rest.api.PayingApi;
 import school.hei.haapi.endpoint.rest.client.ApiClient;
 import school.hei.haapi.endpoint.rest.client.ApiException;
+import school.hei.haapi.endpoint.rest.model.CreateFee;
 import school.hei.haapi.endpoint.rest.model.Fee;
 import school.hei.haapi.endpoint.rest.security.cognito.CognitoComponent;
 import school.hei.haapi.integration.conf.AbstractContextInitializer;
 import school.hei.haapi.integration.conf.TestUtils;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static school.hei.haapi.integration.conf.TestUtils.FEE2_ID;
+import static school.hei.haapi.integration.conf.TestUtils.anAvailableRandomPort;
+import static school.hei.haapi.integration.conf.TestUtils.assertThrowsApiException;
 import static school.hei.haapi.integration.conf.TestUtils.FEE1_ID;
 import static school.hei.haapi.integration.conf.TestUtils.MANAGER1_TOKEN;
+import static school.hei.haapi.integration.conf.TestUtils.TEACHER1_TOKEN;
+import static school.hei.haapi.integration.conf.TestUtils.setUpCognito;
 import static school.hei.haapi.integration.conf.TestUtils.STUDENT1_ID;
 import static school.hei.haapi.integration.conf.TestUtils.STUDENT1_TOKEN;
 import static school.hei.haapi.integration.conf.TestUtils.STUDENT2_ID;
-import static school.hei.haapi.integration.conf.TestUtils.TEACHER1_TOKEN;
-import static school.hei.haapi.integration.conf.TestUtils.anAvailableRandomPort;
-import static school.hei.haapi.integration.conf.TestUtils.assertThrowsForbiddenException;
-import static school.hei.haapi.integration.conf.TestUtils.setUpCognito;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @Testcontainers
@@ -35,6 +41,7 @@ import static school.hei.haapi.integration.conf.TestUtils.setUpCognito;
 @AutoConfigureMockMvc
 class FeeIT {
   @MockBean private SentryConf sentryConf;
+  @MockBean private CognitoComponent cognitoComponentMock;
 
   static class ContextInitializer extends AbstractContextInitializer {
     public static final int SERVER_PORT = anAvailableRandomPort();
@@ -45,15 +52,13 @@ class FeeIT {
     }
   }
 
-  @MockBean private CognitoComponent cognitoComponentMock;
+  private static ApiClient anApiClient(String token) {
+    return TestUtils.anApiClient(token, FeeIT.ContextInitializer.SERVER_PORT);
+  }
 
   @BeforeEach
   void setUp() {
     setUpCognito(cognitoComponentMock);
-  }
-
-  private static ApiClient anApiClient(String token) {
-    return TestUtils.anApiClient(token, FeeIT.ContextInitializer.SERVER_PORT);
   }
 
   static Fee fee1() {
@@ -70,39 +75,139 @@ class FeeIT {
     return fee;
   }
 
+  static CreateFee creatableFee1() {
+    return new CreateFee()
+        .studentId(STUDENT1_ID)
+        .type(CreateFee.TypeEnum.TUITION)
+        .totalAmount(5000)
+        .comment("Comment")
+        .dueDatetime(Instant.parse("2021-12-08T08:25:24.00Z"));
+  }
+
+  static CreateFee creatableFee2() {
+    return new CreateFee()
+        .studentId(STUDENT2_ID)
+        .type(CreateFee.TypeEnum.TUITION)
+        .totalAmount(5000)
+        .comment("Comment")
+        .dueDatetime(Instant.parse("2021-12-08T08:25:24.00Z"));
+  }
+
+  static List<CreateFee> someCreatableFeeList(int number) {
+    List<CreateFee> toCreate = new ArrayList<>();
+    for (int i = 0; i < number; i++) {
+      toCreate.add(creatableFee1());
+    }
+    return toCreate;
+  }
+
   @Test
   void teacher_read_ko() {
     ApiClient teacher1Client = anApiClient(TEACHER1_TOKEN);
-
     PayingApi api = new PayingApi(teacher1Client);
-    assertThrowsForbiddenException(
+
+    assertThrowsApiException(
+        "{\"type\":\"403 FORBIDDEN\",\"message\":\"Access is denied\"}",
         () -> api.getStudentFeeById(STUDENT1_ID, FEE1_ID));
   }
 
   @Test
   void manager_read_ok() throws ApiException {
     ApiClient manager1Client = anApiClient(MANAGER1_TOKEN);
-
     PayingApi api = new PayingApi(manager1Client);
-    Fee actual = api.getStudentFeeById(STUDENT1_ID, FEE1_ID);
 
-    assertEquals(fee1(), actual);
+    List<Fee> actual = api.getStudentFees(STUDENT1_ID);
+
+    assertTrue(actual.contains(fee1()));
   }
 
   @Test
   void student_read_ko() {
     ApiClient student1Client = anApiClient(STUDENT1_TOKEN);
-
     PayingApi api = new PayingApi(student1Client);
-    assertThrowsForbiddenException(() -> api.getStudentFeeById(STUDENT2_ID, FEE1_ID));
+
+    assertThrowsApiException(
+        "{\"type\":\"403 FORBIDDEN\",\"message\":\"Access is denied\"}",
+        () -> api.getStudentFeeById(STUDENT2_ID, FEE2_ID));
   }
 
   @Test
   void student_read_own_ok() throws ApiException {
     ApiClient student1Client = anApiClient(STUDENT1_TOKEN);
-
     PayingApi api = new PayingApi(student1Client);
-    Fee actual = api.getStudentFeeById(STUDENT1_ID, FEE1_ID);
-    assertEquals(fee1(), actual);
+
+    List<Fee> actual = api.getStudentFees(STUDENT1_ID);
+    Fee actualFee = api.getStudentFeeById(STUDENT1_ID, FEE1_ID);
+
+    assertTrue(actual.contains(fee1()));
+    assertEquals(actualFee, fee1());
+  }
+
+  @Test
+  void manager_write_ok() throws ApiException {
+    ApiClient manager1Client = anApiClient(MANAGER1_TOKEN);
+    PayingApi api = new PayingApi(manager1Client);
+
+    List<Fee> actual = api.createStudentFees(STUDENT1_ID, List.of(creatableFee1()));
+
+    List<Fee> expectedFees = api.getStudentFees(STUDENT1_ID);
+    assertTrue(expectedFees.containsAll(actual));
+  }
+
+  @Test
+  void student_write_ko() {
+    ApiClient student1Client = anApiClient(STUDENT1_TOKEN);
+    PayingApi api = new PayingApi(student1Client);
+
+    assertThrowsApiException(
+        "{\"type\":\"403 FORBIDDEN\",\"message\":\"Access is denied\"}",
+        () -> api.createStudentFees(STUDENT1_ID, List.of()));
+  }
+
+  @Test
+  void teacher_write_ko() {
+    ApiClient teacher1Client = anApiClient(TEACHER1_TOKEN);
+    PayingApi api = new PayingApi(teacher1Client);
+
+    assertThrowsApiException(
+        "{\"type\":\"403 FORBIDDEN\",\"message\":\"Access is denied\"}",
+        () -> api.createStudentFees(STUDENT1_ID, List.of()));
+  }
+
+  @Test
+  void manager_write_to_wrong_student_ko() {
+    ApiClient manager1Client = anApiClient(MANAGER1_TOKEN);
+    PayingApi api = new PayingApi(manager1Client);
+
+    assertThrowsApiException(
+        "{\"type\":\"400 BAD_REQUEST\",\"message\":"
+            + "\"Fee must be associated to student.student2_id instead of student.student1_id\"}",
+        () -> api.createStudentFees(STUDENT1_ID, List.of(creatableFee2())));
+  }
+
+  @Test
+  void manager_write_with_some_bad_fields_ko() {
+    ApiClient manager1Client = anApiClient(MANAGER1_TOKEN);
+    PayingApi api = new PayingApi(manager1Client);
+    CreateFee toCreate1 = creatableFee1().totalAmount(null);
+    CreateFee toCreate2 = creatableFee1().totalAmount(-1);
+    CreateFee toCreate3 = creatableFee1()
+        .studentId(null)
+        .dueDatetime(null);
+
+    ApiException exception1 = assertThrows(ApiException.class,
+        () -> api.createStudentFees(STUDENT1_ID, List.of(toCreate1)));
+    ApiException exception2 = assertThrows(ApiException.class,
+        () -> api.createStudentFees(STUDENT1_ID, List.of(toCreate2)));
+    ApiException exception3 = assertThrows(ApiException.class,
+        () -> api.createStudentFees(STUDENT1_ID, List.of(toCreate3)));
+
+    String exceptionMessage1 = exception1.getMessage();
+    String exceptionMessage2 = exception2.getMessage();
+    String exceptionMessage3 = exception3.getMessage();
+    assertTrue(exceptionMessage1.contains("Total amount is mandatory"));
+    assertTrue(exceptionMessage2.contains("Total amount must be positive"));
+    assertTrue(exceptionMessage3.contains("Due datetime is mandatory"));
+    assertTrue(exceptionMessage3.contains("Student is mandatory"));
   }
 }
