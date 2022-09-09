c236843cca609a7c33971bf67f93d24f55e9c054
Ylan <hei.nalisoa@gmail.com>
2022-09-09 15:28:59 +0300

    feat : eventparticipant added

---
+++ src/main/java/school/hei/haapi/model/EventParticipant.java
@@ -0,0 +1,48 @@
+package school.hei.haapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import java.io.Serializable;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+@Builder
+@Entity
+public class EventParticipant implements Serializable {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private String idEventParticipant;
+
+  @Enumerated(EnumType.STRING)
+  @NotBlank
+  @NotEmpty
+  private Status status = Status.EXCEPTED;
+
+  @OneToOne
+  @JoinColumn(name = "id_participant")
+  private User participant;
+
+  @ManyToOne
+  @JoinColumn(name = "id_event")
+  private Event event;
+  public enum Status {
+    EXCEPTED,
+    MISSING,
+    HERE
+  }
+
+}
