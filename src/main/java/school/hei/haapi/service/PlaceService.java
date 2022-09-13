5922a3ef527f5085d606e04fa99a2b61da47dbc8
Tsirimaholy <hei.tsirimaholy@gmail.com>
2022-09-13 09:23:09 +0300

    fix: adding operationId

---
+++ src/main/java/school/hei/haapi/service/PlaceService.java
@@ -0,0 +1,30 @@
+package school.hei.haapi.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import school.hei.haapi.model.Place;
+import school.hei.haapi.repository.PlaceRepository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class PlaceService {
+    private final PlaceRepository placeRepository;
+
+    public List<Place> getAll(){
+        return placeRepository.findAll(Sort.by(Sort.Direction.ASC, "name"));
+    }
+
+    public Place getById(String id){
+        return placeRepository.getById(id);
+    }
+
+    @Transactional
+    public List<Place> createOrUpdate(List<Place> places){
+        return placeRepository.saveAll(places);
+    }
+
+}
