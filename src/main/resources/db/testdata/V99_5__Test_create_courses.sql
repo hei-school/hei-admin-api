753c3fce5887cc051e25e3e34b2dff5c4785a9cc
Lova Fenohasina Lala <fenohasinalala@gmail.com>
2022-09-14 00:26:54 +0300

    feat-add temporary api & localhost:8080 ok

--- src/main/resources/db/testdata/V99_5__Test_create_courses.sql
+++
@@ -1,6 +0,0 @@
-insert into "course"
-    (id, ref, name, credits,total_hours)
-values
-    ('course1_id','PROG1', 'Algorithmics', 1.0f, 40.0f),
-    ('course2_id','WEB1', 'Site web', 1.5f, 45.0f),
-    ('course3_id','THEORY1', 'Maths', 2.0f, 50.0f);
