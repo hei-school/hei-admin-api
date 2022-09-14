4d85e2ddfdae2d31156b73e3c3c5f8e4e82286d1
Minosoa <103509226+MiRandria@users.noreply.github.com>
2022-09-14 20:03:29 +0300

    feat: add /courses

---
+++ src/main/java/school/hei/haapi/service/CourseService.java
@@ -0,0 +1,27 @@
+package school.hei.haapi.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import school.hei.haapi.model.Course;
+import school.hei.haapi.repository.CourseRepository;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class CourseService {
+
+  private final CourseRepository repository;
+
+  public Course getById(String courseId) {
+    return repository.getById(courseId);
+  }
+
+  public List<Course> getAll() {
+    return repository.findAll();
+  }
+
+  public List<Course> saveAll(List<Course> courses) {
+    return repository.saveAll(courses);
+  }
+}
