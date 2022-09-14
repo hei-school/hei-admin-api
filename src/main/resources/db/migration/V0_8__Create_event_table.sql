84191af2ceb39645630dcfbf8b45745837d33e83
Jean Koletto Reichmann GALY <hei.reichmann.4@gmail.com>
2022-09-14 17:39:46 +0300

    update api.yml

---
+++ src/main/resources/db/migration/V0_8__Create_event_table.sql
@@ -0,0 +1,22 @@
+do
+$$
+begin
+    if not exists(select from pg_type where typname = 'event_type') then
+create type event_type as enum ('COURSE', 'EXAMINATION', 'MEETING');
+end if;
+end
+$$;
+
+create table if not exists "event"
+(
+    id                varchar
+    constraint event_pk primary key default uuid_generate_v4(),
+    name              varchar                  not null,
+    event_type        event_type               not null,
+    starting_time     timestamp with time zone not null,
+    ending_time       timestamp with time zone not null,
+                                    supervisor_id     varchar                  not null
+                                    constraint event_user_id_pk references "user"(id),
+    place_id          varchar
+    constraint event_place_id_pk references "place"(id)
+);
