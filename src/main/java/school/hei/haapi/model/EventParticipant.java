9c9ed33e727721121ca16567b9ee4a14c39c91a5
Tsirimaholy <hei.tsirimaholy@gmail.com>
2022-09-15 11:39:31 +0300

    feat: adding event participant entity

---
+++ src/main/java/school/hei/haapi/model/EventParticipant.java
@@ -0,0 +1,56 @@
+package school.hei.haapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import school.hei.haapi.repository.types.PostgresEnumType;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "\"event_participant\"")
+@TypeDef(name = "pgsql_enum", typeClass = PostgresEnumType.class)
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class EventParticipant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "\"status\"")
+    @Type(type = "pgsql_enum")
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+
+    public enum Status {
+        EXPECTED,
+        HERE,
+        MISSING
+    }
+}
