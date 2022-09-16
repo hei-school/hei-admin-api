436acf0f40d86b3e5ab541519c5811ec9fbbf9f7
Onitsiky <hei.onitsiky@gmail.com>
2022-09-17 00:34:46 +0300

    chore: write tests for event ressource

--- src/main/resources/db/testdata/V99_7_Test_create_event.sql
+++ src/main/resources/db/testdata/V99_7_Test_create_event.sql
@@ -1,6 +1,6 @@
 insert into "event"
-    (id, event_name, event_type, start_time,end_time, person_in_charge, place)
+    (id, event_name, event_type, start_date,end_date, person_in_charge, place)
 values
-    ('event1_id', 'course', '2021-11-08T08:25:24.00Z', '2021-11-08T12:25:24.00Z', 1, 1),
-    ('event2_id', 'course', '2022-11-08T08:25:24.00Z', '2022-11-08T12:25:24.00Z', 2, 2)
+    ('event1_id', 'course', 'PROG2', '2021-11-08T08:25:24.00Z', '2021-11-08T12:25:24.00Z','teacher1_id', 'place1_id'),
+    ('event2_id', 'course', 'SYS2', '2022-11-08T08:25:24.00Z', '2022-11-08T12:25:24.00Z','teacher2_id', 'place2_id')
 ;
--- src/test/java/school/hei/haapi/integration/EventIT.java
+++ src/test/java/school/hei/haapi/integration/EventIT.java
@@ -9,10 +9,11 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import school.hei.haapi.SentryConf;
+import school.hei.haapi.endpoint.rest.api.PlacesApi;
 import school.hei.haapi.endpoint.rest.api.TeachingApi;
 import school.hei.haapi.endpoint.rest.client.ApiClient;
 import school.hei.haapi.endpoint.rest.client.ApiException;
-import school.hei.haapi.endpoint.rest.model.Course;
+import school.hei.haapi.endpoint.rest.model.Event;
 import school.hei.haapi.endpoint.rest.security.cognito.CognitoComponent;
 import school.hei.haapi.integration.conf.AbstractContextInitializer;
 import school.hei.haapi.integration.conf.TestUtils;
@@ -32,7 +33,7 @@ import static school.hei.haapi.integration.conf.TestUtils.setUpCognito;
 @Testcontainers
 @ContextConfiguration(initializers = CourseIT.ContextInitializer.class)
 @AutoConfigureMockMvc
-public class CourseIT {
+public class EventIT {
 
   @MockBean
   private SentryConf sentryConf;
@@ -44,46 +45,53 @@ public class CourseIT {
     return TestUtils.anApiClient(token, ContextInitializer.SERVER_PORT);
   }
 
-  public static Course course1() {
-    Course course = new Course();
-    course.setId("course1_id");
-    course.setRef("PROG1");
-    course.setName("Algorithmics");
-    course.setCredits(2);
-    course.setTotalHours(22);
-    return course;
+  public static Event event1(){
+    Event event = new Event();
+    event.setId("event1_id");
+    event.setEventName("PROG2");
+    event.setEventType("course");
+    event.setStartDate("2021-11-08T08:25:24.00Z");
+    event.setEndDate("2021-11-08T12:25:24.00Z");
+    event.setResponsible("teacher1_id");
+    event.setPlace("place1_id");
+    return event;
   }
 
 
-  public static Course course2() {
-    Course course = new Course();
-    course.setId("course2_id");
-    course.setRef("SYS1");
-    course.setName("Operating system");
-    course.setCredits(4);
-    course.setTotalHours(44);
-    return course;
+  public static Event event2(){
+    Event event = new Event();
+    event.setId("event2_id");
+    event.setEventName("SYS2");
+    event.setEventType("course");
+    event.setStartDate("2022-11-08T08:25:24.00Z");
+    event.setEndDate("2022-11-08T12:25:24.00Z");
+    event.setResponsible("teacher2_id");
+    event.setPlace("place2_id");
+    return event;
   }
 
-  public static Course someCreatableCourse1() {
-    Course course = new Course();
-    course.setId("course3_id");
-    course.setRef("SYS2");
-    course.setName("Cloud computing");
-    course.setCredits(2);
-    course.setTotalHours(22);
-    return course;
+  public static Event someCreatableEvent1(){
+    Event event = new Event()
+        .id("event3_id")
+        .eventName("MGT1")
+        .eventType("course")
+        .startDate("2022-11-08T12:25:24.00Z")
+        .endDate("2022-11-08T16:25:24.00Z")
+        .responsible("teacher1_id")
+        .place("place1_id");
+    return event;
   }
-  public static Course someCreatableCourse2() {
-    Course course = new Course();
-    course.setId("course4_id");
-    course.setRef("PROG2");
-    course.setName("API programming");
-    course.setCredits(3);
-    course.setTotalHours(33);
-    return course;
+  public static Event someCreatableEvent2(){
+    Event event = new Event()
+        .id("event4_id")
+        .eventName("WEB1")
+        .eventType("course")
+        .startDate("2022-11-08T12:25:24.00Z")
+        .endDate("2022-11-08T16:25:24.00Z")
+        .responsible("teacher2_id")
+        .place("place2_id");
+    return event;
   }
-
   @BeforeEach
   public void setUp() {
     setUpCognito(cognitoComponentMock);
@@ -93,62 +101,66 @@ public class CourseIT {
   void student_read_ok() throws ApiException {
     ApiClient student1Client = anApiClient(STUDENT1_TOKEN);
 
-    TeachingApi api = new TeachingApi(student1Client);
-    List<Course> courses = api.getCourses();
+    PlacesApi api = new PlacesApi(student1Client);
+    List<Event> events = api.getEvents(1,2);
 
-    assertTrue(courses.contains(course1()));
-    assertTrue(courses.contains(course2()));
+    assertTrue(events.contains(event1()));
+    assertTrue(events.contains(event2()));
   }
 
   @Test
   void student_write_ko() {
     ApiClient student1Client = anApiClient(STUDENT1_TOKEN);
 
-    TeachingApi api = new TeachingApi(student1Client);
-    assertThrowsForbiddenException(() -> api.createOrUpdateCourses(someCreatableCourse1()));
+    PlacesApi api = new PlacesApi(student1Client);
+    assertThrowsForbiddenException(() -> api.createOrUpdateEvents(List.of(someCreatableEvent1(), someCreatableEvent2())));
   }
 
   @Test
   void teacher_read_ok() throws ApiException {
     ApiClient student1Client = anApiClient(TEACHER1_TOKEN);
 
-    TeachingApi api = new TeachingApi(student1Client);
-    List<Course> courses = api.getCourses();
+    PlacesApi api = new PlacesApi(student1Client);
+    List<Event> events = api.getEvents(0,2);
 
-    assertTrue(courses.contains(course1()));
-    assertTrue(courses.contains(course2()));
+    assertTrue(events.contains(event1()));
+    assertTrue(events.contains(event2()));
   }
   @Test
   void teacher_write_ko() {
     ApiClient teacher1Client = anApiClient(TEACHER1_TOKEN);
 
-    TeachingApi api = new TeachingApi(teacher1Client);
-    assertThrowsForbiddenException(() -> api.createOrUpdateCourses(someCreatableCourse1()));
+    PlacesApi api = new PlacesApi(teacher1Client);
+    assertThrowsForbiddenException(() -> api.createOrUpdateEvents(List.of(someCreatableEvent1(), someCreatableEvent2())));
   }
 
   @Test
   void manager_read_ok() throws ApiException {
     ApiClient student1Client = anApiClient(MANAGER1_TOKEN);
 
-    TeachingApi api = new TeachingApi(student1Client);
-    List<Course> courses = api.getCourses();
+    PlacesApi api = new PlacesApi(student1Client);
+    List<Event> events = api.getEvents(0,2);
 
-    assertTrue(courses.contains(course1()));
-    assertTrue(courses.contains(course2()));
+    assertTrue(events.contains(event1()));
+    assertTrue(events.contains(event2()));
   }
 
   @Test
   void manager_write_create_ok() throws ApiException {
     ApiClient manager1Client = anApiClient(MANAGER1_TOKEN);
-    Course toCreate1 = someCreatableCourse1();
+    Event toCreate1 = someCreatableEvent1();
+    Event toCreate2 = someCreatableEvent2();
 
 
-    TeachingApi api = new TeachingApi(manager1Client);
-    Course created = api.createOrUpdateCourses(toCreate1);
-    toCreate1.setId(created.getId());
+    PlacesApi api = new PlacesApi(manager1Client);
+    List<Event> created = api.createOrUpdateEvents(List.of(toCreate1, toCreate2));
+    toCreate1.setId(created.get(1).getId());
+    toCreate2.setId(created.get(2).getId());
 
-    assertEquals(toCreate1, created);
-    assertTrue(isValidUUID(created.getId()));
+    assertEquals(toCreate1, api.getEventById(toCreate2.getId()));
+    assertTrue(isValidUUID(toCreate1.getId()));
+    assertEquals(toCreate2, api.getEventById(toCreate2.getId()));
+    assertTrue(isValidUUID(toCreate2.getId()));
   }
 
   static class ContextInitializer extends AbstractContextInitializer {
