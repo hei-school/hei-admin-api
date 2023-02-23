e361fea0ca7600feb4254167f81476c91d6ab596
Sanmandresy <hei.sanda.3@gmail.com>
2023-02-23 09:46:15 +0300

    chore: integrate ses service

---
+++ src/main/java/school/hei/haapi/service/aws/SesService.java
@@ -0,0 +1,58 @@
+package school.hei.haapi.service.aws;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.awscore.exception.AwsServiceException;
+import software.amazon.awssdk.core.exception.SdkClientException;
+import software.amazon.awssdk.services.ses.SesClient;
+import software.amazon.awssdk.services.ses.model.Body;
+import software.amazon.awssdk.services.ses.model.Content;
+import software.amazon.awssdk.services.ses.model.Destination;
+import software.amazon.awssdk.services.ses.model.Message;
+import software.amazon.awssdk.services.ses.model.SendEmailRequest;
+
+@Component
+@Service
+@AllArgsConstructor
+public class SesService {
+  private final SesClient client;
+
+  public void sendEmail(String sender, String recipient,
+                        String subject, String bodyHtml) {
+
+    Destination destination = Destination.builder()
+        .toAddresses(recipient)
+        .build();
+
+    Content content = Content.builder()
+        .data(bodyHtml)
+        .build();
+
+    Content subj = Content.builder()
+        .data(subject)
+        .build();
+
+    Body body = Body.builder()
+        .html(content)
+        .build();
+
+    Message message = Message.builder()
+        .subject(subj)
+        .body(body)
+        .build();
+
+    SendEmailRequest emailRequest = SendEmailRequest.builder()
+        .destination(destination)
+        .message(message)
+        .source(sender)
+        .build();
+    try {
+      client.sendEmail(emailRequest);
+    } catch (AwsServiceException | SdkClientException e) {
+      throw new RuntimeException(e);
+    }
+
+  }
+
+}
