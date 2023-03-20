d2abbcc388f49d2209a2a4ec96001af52dee210a
herizoran <herizo.ran@gmail.com>
2023-03-20 23:08:19 +0300

    feat: add course service

---
+++ src/main/java/school/hei/haapi/service/CourseService.java
@@ -0,0 +1,9 @@
+package school.hei.haapi.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CourseService {
+}
