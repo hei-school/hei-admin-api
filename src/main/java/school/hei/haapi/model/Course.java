b5e0c8d1d6f2aa605e0e949e1746f2124f279d2d
Lova Fenohasina Lala RAFANOMEZANTSOA <44872158+fenohasinalala@users.noreply.github.com>
2023-03-21 00:00:03 +0300

    feat: add course model ,table and data (#3)

---
+++ src/main/java/school/hei/haapi/model/Course.java
@@ -0,0 +1,48 @@
+package school.hei.haapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.Hibernate;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name = "\"group\"")
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Course implements Serializable {
+  @Id
+  @GeneratedValue(strategy = IDENTITY)
+  private String id;
+  @NotBlank(message = "Code is mandatory")
+  private String code;
+  @NotBlank(message = "Name is mandatory")
+  private String name;
+  private int credits;
+  private int totalHours;
+  @ManyToOne
+  @JoinColumn(name = "teacher_id", nullable = false)
+  private User MainTeacher;
+}
