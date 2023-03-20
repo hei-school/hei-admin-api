e0a0b0f8660f9a6c08ae521c439ec4f319d0c7b6
herizoran <herizo.ran@gmail.com>
2023-03-20 22:49:40 +0300

    feat: add course controller

---
+++ src/main/java/school/hei/haapi/endpoint/rest/controller/CourseController.java
@@ -0,0 +1,9 @@
+package school.hei.haapi.endpoint.rest.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class CourseController {
+}
