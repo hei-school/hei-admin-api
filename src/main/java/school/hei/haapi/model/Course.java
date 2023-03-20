df9590d6091908e5028a628afa4b352bd39e219f
Razafimamonjisoa Mendrika Harizo <hei.mendrikaharizo@gmail.com>
2023-03-20 21:18:12 +0300

    feat: create course model

---
+++ src/main/java/school/hei/haapi/model/Course.java
@@ -0,0 +1,37 @@
+package school.hei.haapi.model;
+
+import lombok.*;
+import org.hibernate.annotations.TypeDef;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name = "\"user\"")
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Course implements Serializable {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private String id;
+
+    @NotBlank(message = "Code is mandatory")
+    private String code;
+
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+
+    @NotBlank(message = "Credits name is mandatory")
+    private int credits;
+
+    @OneToMany
+    @JoinColumn(name = "main_teacher")
+    private User.Role main_teacher;
+}
