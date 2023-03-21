6165dbbe39dbbd0bf39fde3a300487ba44276d76
vohizy <hei.vohizy@gmail.com>
2023-03-21 10:00:33 +0300

    feat: service course

---
+++ src/main/java/school/hei/haapi/service/CourseService.java
@@ -0,0 +1,12 @@
+package school.hei.haapi.service;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class CourseService {
+
+}
