8ff0119a4ff75e58ff35070095af2c62f809ac9c
Lova Fenohasina Lala <fenohasinalala@gmail.com>
2023-03-21 11:21:53 +0300

    feat: add security conf for put student course

--- src/main/java/school/hei/haapi/endpoint/rest/security/SecurityConf.java
+++ src/main/java/school/hei/haapi/endpoint/rest/security/SecurityConf.java
@@ -86,6 +86,7 @@ public class SecurityConf extends WebSecurityConfigurerAdapter {
         .antMatchers(POST, "/students/*/fees").hasAnyRole(MANAGER.getRole())
             .antMatchers(GET, "/students/*/courses").hasAnyRole(MANAGER.getRole())
             .requestMatchers(new SelfMatcher(GET, "/students/*/courses")).hasAnyRole(STUDENT.getRole())
+            .antMatchers(PUT, "/students/*/courses").hasAnyRole(MANAGER.getRole())
         .requestMatchers(new SelfMatcher(GET, "/students/*/fees/*/payments")).hasAnyRole(
             STUDENT.getRole())
         .antMatchers(GET, "/students/*/fees/*/payments").hasAnyRole(MANAGER.getRole())
