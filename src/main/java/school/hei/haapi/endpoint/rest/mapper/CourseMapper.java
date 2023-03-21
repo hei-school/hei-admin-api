476f0e2158aa8b6f9c25da7552949b34536a32a6
vohizy <hei.vohizy@gmail.com>
2023-03-21 11:34:32 +0300

    feat:add course mapper

--- src/main/java/school/hei/haapi/endpoint/rest/mapper/CourseMapper.java
+++ src/main/java/school/hei/haapi/endpoint/rest/mapper/CourseMapper.java
@@ -1,2 +1,23 @@
-package school.hei.haapi.endpoint.rest.mapper;public class CourseMapper {
+package school.hei.haapi.endpoint.rest.mapper;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+import school.hei.haapi.model.User;
+import school.hei.haapi.service.UserService;
+
+@Component
+@AllArgsConstructor
+public class CourseMapper {
+    private UserService userService;
+    public Course toRestFee(school.hei.haapi.model.Course course, String studentId) {
+        return new Course()
+                .id(course.getId())
+                .code(course.getCode())
+                .name(course.getName())
+                .credits(course.getCredits())
+                .totals_hours(course.getTotalHours())
+                .main_teacher(course.getMainTeacher())
+                .status(restCourse.getStatus())
+                .student(userService.getById(studentId));
+    }
 }
