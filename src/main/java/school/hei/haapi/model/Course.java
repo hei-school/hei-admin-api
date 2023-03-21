d495ab6c91eab0ab2c227b413eb49f2d60f5e774
Cedric <99885260+Cenikl@users.noreply.github.com>
2023-03-21 08:25:38 +0300

    chore: add courses to database

---
+++ src/main/java/school/hei/haapi/model/Course.java
@@ -0,0 +1,107 @@
+package school.hei.haapi.model;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.Hibernate;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import school.hei.haapi.repository.types.PostgresEnumType;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name = "\"course\"")
+@TypeDef(name = "pgsql_enum", typeClass = PostgresEnumType.class)
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Course implements Serializable {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private String id;
+
+    @NotBlank(message = "First name is mandatory")
+    private String firstName;
+
+    @NotBlank(message = "Last name is mandatory")
+    private String lastName;
+
+    @NotBlank(message = "Email is mandatory")
+    @Email(message = "Email must be valid")
+    private String email;
+
+    @NotBlank(message = "Reference is mandatory")
+    private String ref;
+
+    @Type(type = "pgsql_enum")
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @NotBlank(message = "Phone number is mandatory")
+    private String phone;
+
+    private LocalDate birthDate;
+
+    private Instant entranceDatetime;
+
+    @Type(type = "pgsql_enum")
+    @Enumerated(EnumType.STRING)
+    private Sex sex;
+
+    @NotBlank(message = "Address is mandatory")
+    private String address;
+
+    @Column(name = "\"role\"")
+    @Type(type = "pgsql_enum")
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) {
+            return false;
+        }
+        User user = (User) o;
+        return id != null && Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+
+    public enum Sex {
+        M, F
+    }
+
+    public enum Status {
+        ENABLED, DISABLED
+    }
+
+    public enum Role {
+        STUDENT, TEACHER, MANAGER
+    }
+}
---
+++ src/main/resources/db/testdata/V99_6_Test_create_student_courses.sql
@@ -0,0 +1,4 @@
+insert into "student_course"
+(course_id, student_id, status)
+values ('course1_id', 'student1_id', 'LINKED'),
+       ('course2_id', 'student2_id', 'UNLIKED');
