fa5113697ebe17f89c731ba64835d346a3e271fa
vohizy <hei.vohizy@gmail.com>
2023-03-21 10:28:35 +0300

    feat: getCoursesByStudentIdAndStatus getByStudentId in course

--- src/main/java/school/hei/haapi/repository/CourseRespository.java
+++ src/main/java/school/hei/haapi/repository/CourseRespository.java
@@ -1,11 +1,19 @@
 package school.hei.haapi.repository;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import school.hei.haapi.model.Course;
 import school.hei.haapi.model.Fee;
 
+import java.util.List;
+
 @Repository
 public interface CourseRespository extends JpaRepository<Course , String> {
     Course getByStudentIdAndId(String studentId, String courseId);
+    List<Course> getCoursesByStudentIdAndStatus(String studentId,
+                                          CourseStatus status,
+                                          Pageable pageable);
+    List<Course> getByStudentId(String studentId, Pageable pageable);
+
 }
