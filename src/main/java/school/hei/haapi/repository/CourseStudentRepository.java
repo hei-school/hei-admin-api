6d93eb1c0bb167cfd320a4a6a6ebf27f578f8070
njaina <hei.njaina.2@gmail.com>
2023-03-21 13:21:20 +0300

    feat: update

--- src/main/java/school/hei/haapi/repository/CourseStudentRepository.java
+++ src/main/java/school/hei/haapi/repository/CourseStudentRepository.java
@@ -3,11 +3,16 @@ package school.hei.haapi.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import school.hei.haapi.endpoint.rest.model.CourseStatus;
+import school.hei.haapi.model.Course;
 import school.hei.haapi.model.CourseStudent;
+import school.hei.haapi.model.User;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CourseStudentRepository extends JpaRepository<CourseStudent, String> {
     List<CourseStudent> getAllByStudentIdAndStatus(String student_id, CourseStatus status);
+    List<CourseStudent> findAllByStudent(User student);
+    Optional<CourseStudent> findByStudentAndCourse(User student, Course course);
 }
--- src/main/java/school/hei/haapi/service/CourseService.java
+++ src/main/java/school/hei/haapi/service/CourseService.java
@@ -4,17 +4,18 @@ import lombok.AllArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import school.hei.haapi.model.BoundedPageSize;
-import school.hei.haapi.model.Course;
-import school.hei.haapi.model.PageFromOne;
+import school.hei.haapi.model.*;
 import school.hei.haapi.repository.CourseRepository;
+import school.hei.haapi.repository.CourseStudentRepository;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
 public class CourseService {
     private final CourseRepository repository;
+    private final CourseStudentRepository courseStudentRepository;
     private final UserService userService;
     public List<Course> getAll(PageFromOne page, BoundedPageSize pageSize){
         Pageable pageable =
@@ -25,4 +26,22 @@ public class CourseService {
   public List<Course> saveAll(List<Course> courses) {
     return repository.saveAll(courses);
   }
+    public List<Course> findCoursesByStudent(String studentId) {
+        User student = userService.getById(studentId);
+        return courseStudentRepository.findAllByStudent(student).stream()
+                .map(CourseStudent::getCourse)
+                .collect(Collectors.toList());
+    }
+    public void updateCourseStudentStatus(String studentId, String courseId, CourseStudent.Status newStatus) {
+        User student = userService.getById(studentId);
+        Course course = repository.findById(courseId)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid course id: " + courseId));
+        CourseStudent courseStudent = courseStudentRepository.findByStudentAndCourse(student, course)
+                .orElseThrow(() -> new IllegalArgumentException("No course student found for student " + studentId + " and course " + courseId));
+        courseStudent.setStatus(newStatus);
+        courseStudentRepository.save(courseStudent);
+    }
+
+
+
 }
