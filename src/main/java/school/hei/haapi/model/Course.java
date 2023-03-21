f14c03da3c101375f12f9229cdaf7ade7df90721
vohizy <hei.vohizy@gmail.com>
2023-03-21 09:51:07 +0300

    feat: model course

---
+++ src/main/java/school/hei/haapi/model/Course.java
@@ -0,0 +1,58 @@
+package school.hei.haapi.model;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "\"course\"")
+@Getter
+@Setter
+@TypeDef(name = "pgsql_enum", typeClass = PostgresEnumType.class)
+@ToString
+@Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class Course {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private String id;
+
+        private String code;
+
+        private String name;
+
+        private int credits;
+
+        private int totalHours;
+
+        @ManyToOne
+        @JoinColumn(name = "main_teacher_id", nullable = false)
+        private User mainTeacher;
+
+        @Type(type="pgsql_enum")
+        @Enumerated(EnumType.STRING)
+        private CourseStatus status;
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) {
+                return false;
+            }
+            Course user = (Course) o;
+            return id != null && Objects.equals(id, user.id);
+        }
+
+        @Override
+        public int hashCode() {
+            return getClass().hashCode();
+        }
+
+}
