93ff730a312f5e1b844d08d858fe842caeae17ea
vohizy <hei.vohizy@gmail.com>
2023-03-21 10:08:21 +0300

    feat: getByStudentIdAndId in course

--- src/main/java/school/hei/haapi/repository/CourseRespository.java
+++ src/main/java/school/hei/haapi/repository/CourseRespository.java
@@ -3,7 +3,9 @@ package school.hei.haapi.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import school.hei.haapi.model.Course;
+import school.hei.haapi.model.Fee;
 
 @Repository
 public interface CourseRespository extends JpaRepository<Course , String> {
+    Course getByStudentIdAndId(String studentId, String courseId);
 }
