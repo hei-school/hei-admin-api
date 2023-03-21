60c6e61e9c59f8b1c53a82f33521f3ab4ae63c58
vohizy <hei.vohizy@gmail.com>
2023-03-21 11:49:01 +0300

    refactor: name methode toRestCourse

--- src/main/java/school/hei/haapi/endpoint/rest/mapper/CourseMapper.java
+++ src/main/java/school/hei/haapi/endpoint/rest/mapper/CourseMapper.java
@@ -5,11 +5,13 @@ import org.springframework.stereotype.Component;
 import school.hei.haapi.model.User;
 import school.hei.haapi.service.UserService;
 
+import java.util.List;
+
 @Component
 @AllArgsConstructor
 public class CourseMapper {
     private UserService userService;
-    public Course toRestFee(school.hei.haapi.model.Course course, String studentId) {
+    public Course toRestCourse(school.hei.haapi.model.Course course, String studentId) {
         return new Course()
                 .id(course.getId())
                 .code(course.getCode())
@@ -18,6 +20,6 @@ public class CourseMapper {
                 .totals_hours(course.getTotalHours())
                 .main_teacher(course.getMainTeacher())
                 .status(restCourse.getStatus())
-                .student(userService.getById(studentId));
+                .students(List.of(userService.getById(studentId)));
     }
 }
