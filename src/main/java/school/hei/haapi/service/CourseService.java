ee61d49febfc39bfb46e947cc62b6309b1b0366c
haritiana adriano <hei.adriano.4@gmail.com>
2023-03-21 11:18:42 +0300

    feat: get courses

--- src/main/java/school/hei/haapi/service/CourseService.java
+++ src/main/java/school/hei/haapi/service/CourseService.java
@@ -1,11 +1,15 @@
 package school.hei.haapi.service;
 
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import school.hei.haapi.endpoint.rest.mapper.CourseMapper;
 import school.hei.haapi.endpoint.rest.model.BadRequestException;
 import school.hei.haapi.endpoint.rest.model.CourseStatus;
+import school.hei.haapi.model.BoundedPageSize;
 import school.hei.haapi.model.Course;
+import school.hei.haapi.model.PageFromOne;
 import school.hei.haapi.model.StudentCourse;
 import school.hei.haapi.repository.CourseRepository;
 import school.hei.haapi.repository.StudentCourseRepository;
@@ -28,5 +32,20 @@ public class CourseService {
     public List<StudentCourse> getByStudentIdAndStatus(String studentId, StudentCourse.CourseStatus status) {
         return studentCourseRepository.getStudentCourseByStudentIdAndStatus(studentId,status);
 
+    }
+
+    public List<Course> getCourses(PageFromOne page, BoundedPageSize pageSize){
+        if(page==null){
+            Pageable pageable = PageRequest.of(1, pageSize.getValue());
+            return repository.findAll(pageable).toList();
+        }
+        if(pageSize==null){
+            Pageable pageable = PageRequest.of(page.getValue()-1, 15);
+            return repository.findAll(pageable).toList();
+        }
+        else{
+            Pageable pageable = PageRequest.of(page.getValue()-1, pageSize.getValue());
+            return repository.findAll(pageable).toList();
+        }
     }
 }
