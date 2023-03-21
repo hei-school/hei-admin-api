fdb55ffd5f1a580320699b8d2046d5f6ae1c2541
Roussel Rakoto <hei.roussel11@gmail.com>
2023-03-21 12:46:43 +0300

    feat: add courses test

---
+++ src/main/resources/db/testdata/V99_5_Test_create_courses.sql
@@ -0,0 +1,5 @@
+insert into "course"
+    (id, code, name, credits, total_hours, main_teacher_id)
+values ('course1_id', 'SYS1', 'CISCO', 6, 20, 'teacher1_id'),
+       ('course2_id', 'PROG3', 'Unit Test', 6, 24, 'teacher2_id'),
+       ('course3_id', 'SYS3', 'AWS', 6, 22, 'teacher3_id');
