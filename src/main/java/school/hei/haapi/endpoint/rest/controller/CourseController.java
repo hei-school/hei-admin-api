30937fdb08ef46b55ff87c4078fc47484ed3c299
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2023-03-21 08:52:25 +0300

    fix: fixing bug on course controller
    Signed-off-by: Safidy-Randriantseheno <hei.safidy.7@gmail.com>

--- src/main/java/school/hei/haapi/endpoint/rest/controller/CourseController.java
+++ src/main/java/school/hei/haapi/endpoint/rest/controller/CourseController.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import school.hei.haapi.endpoint.rest.mapper.CourseMapper;
 import school.hei.haapi.endpoint.rest.model.Courses;
 import school.hei.haapi.service.CourseService;
 
@@ -13,12 +14,13 @@ import java.util.List;
 @AllArgsConstructor
 public class CourseController {
     private final CourseService courseService;
+    private final CourseMapper courseMapper;
     @GetMapping(value = "/courses")
     public List<Courses> getCourses(
             @RequestParam int page ,
             @RequestParam int pageSize
     ) {
-        return courseService.getCourses(page, pageSize);
+        return courseMapper.toRest(courseService.getCourses(page,pageSize));
     }
 }
 
