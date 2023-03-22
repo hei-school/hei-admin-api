09b9afb92f0f9f1119b62549b921ee1a2a619b50
haritiana adriano <hei.adriano.4@gmail.com>
2023-03-22 13:56:19 +0300

    chore: TD-4 suitable work

---
+++ src/main/java/school/hei/haapi/model/StudentCourse.java
@@ -0,0 +1,58 @@
+package school.hei.haapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import school.hei.haapi.repository.types.PostgresEnumType;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name = "\"student_course\"")
+@TypeDef(name = "pgsql_enum", typeClass = PostgresEnumType.class)
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class StudentCourse implements Serializable {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private String id;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id",nullable = false)
+    private User student;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id",nullable = false)
+    private Course course;
+
+    @Column(name = "\"status\"")
+    @Type(type = "pgsql_enum")
+    @Enumerated(EnumType.STRING)
+    private CourseStatus status;
+
+    public enum CourseStatus {
+        LINKED, UNLINKED
+    }
+}
