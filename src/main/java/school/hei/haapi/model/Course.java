aef5472dead30f86e1a6e981bc08561415e2197d
NiceGuyMe <loica.etudiant@gmail.com>
2023-03-22 16:30:57 +0300

    feat: course entity

---
+++ src/main/java/school/hei/haapi/model/Course.java
@@ -0,0 +1,33 @@
+package school.hei.haapi.model;
+
+import lombok.*;
+import org.hibernate.annotations.TypeDef;
+import school.hei.haapi.repository.types.PostgresEnumType;
+
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name = "\"course\"")
+@Getter
+@Setter
+@TypeDef(name = "pgsql_enum", typeClass = PostgresEnumType.class)
+@ToString
+@Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class Course {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private String id;
+    private String code ;
+    private String name;
+    private Integer credits;
+    private Integer totalHours;
+
+    @ManyToOne
+    private User mainTeacher;
+
+
+}
