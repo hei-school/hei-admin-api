dffc196d390d51baae5edfb91dbd84937608e0e4
Miaritiana <hei.miaritiana@gmail.com>
2023-03-22 22:52:48 +0300

    feat: add Course service

---
+++ src/main/java/school/hei/haapi/service/CourseService.java
@@ -0,0 +1,22 @@
+package school.hei.haapi.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import school.hei.haapi.model.Course;
+import school.hei.haapi.repository.CourseRepository;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class CourseService {
+    private final CourseRepository courseRepository;
+
+    public List<Course> getAllCourses() {
+        return courseRepository.findAll();
+    }
+
+    public List<Course> updateOrCreateCourses(List<Course> toUpdate) {
+        return courseRepository.saveAll(toUpdate);
+    }
+}
