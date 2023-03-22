ab176a2d908ba82a81a9d7b020d865470a198388
Micael Jerry <hei.jerry.2@gmail.com>
2023-03-22 15:45:27 +0300

    chore: create table course in flyway migration

---
+++ src/main/resources/db/migration/V0_9__Create_course_table.sql
@@ -0,0 +1,14 @@
+create extension if not exists "uuid-ossp";
+
+create table if not exists "course"
+(
+    id                varchar
+        constraint course_pk primary key                 default uuid_generate_v4(),
+    code              varchar                  not null
+        constraint course_code_unique unique,
+    name              varchar                  not null,
+    credits           integer                  default 0,
+    total_hours       integer                  default 0,
+    user_id           varchar                  not null
+            constraint course_user_id_fk references "user"(id)
+);
