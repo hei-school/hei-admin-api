fb2f904be17869f37f76cca21ca3f04ade081499
Lova Fenohasina Lala <fenohasinalala@gmail.com>
2023-03-23 22:23:55 +0300

    feat: add CourseManagerDao and update get /courses

--- src/main/java/school/hei/haapi/endpoint/rest/controller/CourseController.java
+++ src/main/java/school/hei/haapi/endpoint/rest/controller/CourseController.java
@@ -61,17 +61,17 @@ public class CourseController {
     @GetMapping("/courses")
     public List<Course> getCourses(
 
-            @RequestParam(name = "code", required = false, defaultValue = "")String code,
-            @RequestParam(name = "name", required = false, defaultValue = "")String name,
-            @RequestParam(name = "credits", required = false, defaultValue = "0")Integer credits,
-            @RequestParam(name = "teacher_first_name", required = false, defaultValue = "")String teacher_first_name,
-            @RequestParam(name = "teacher_last_name", required = false, defaultValue = "")String teacher_last_name,
-            @RequestParam(name = "creditsOrder", required = false, defaultValue = "")String creditsOrder,
-            @RequestParam(name = "codeOrder", required = false, defaultValue = "")String codeOrder,
+            @RequestParam(name = "code", required = false, defaultValue = "") String code,
+            @RequestParam(name = "name", required = false, defaultValue = "") String name,
+            @RequestParam(name = "credits", required = false, defaultValue = "") Integer credits,
+            @RequestParam(name = "teacher_first_name", required = false, defaultValue = "") String teacher_first_name,
+            @RequestParam(name = "teacher_last_name", required = false, defaultValue = "") String teacher_last_name,
+            @RequestParam(name = "creditsOrder", required = false, defaultValue = "") String creditsOrder,
+            @RequestParam(name = "codeOrder", required = false, defaultValue = "") String codeOrder,
             @RequestParam(name = "page", required = false)PageFromOne page,
             @RequestParam(name = "page_size", required = false)BoundedPageSize pageSize
             ){
-        return courseService.getCourses(page, pageSize).stream()
+        return courseService.getCourses(page, pageSize, code, name, credits, teacher_first_name, teacher_last_name, creditsOrder, codeOrder).stream()
                 .map(courseMapper::toRestCourse)
                 .collect(Collectors.toList());
     }
---
+++ src/main/java/school/hei/haapi/repository/dao/CourseManagerDao.java
@@ -0,0 +1,75 @@
+package school.hei.haapi.repository.dao;
+
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.query.QueryUtils;
+import org.springframework.stereotype.Repository;
+import school.hei.haapi.model.Course;
+import school.hei.haapi.model.User;
+
+
+import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+@Repository
+@AllArgsConstructor
+public class CourseManagerDao {
+  private EntityManager entityManager;
+
+  public List<Course> findByCriteria(String code, String name, Integer credits, String teacher_first_name, String teacher_last_name, String creditsOrder, String codeOrder,
+                                     Pageable pageable) {
+    CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+    CriteriaQuery<Course> query = builder.createQuery(Course.class);
+    Root<Course> root = query.from(Course.class);
+      Join<Object,Object> teacher = root.join("MainTeacher");
+
+    Predicate hasCourseCode =
+        builder.or(
+            builder.like(builder.lower(root.get("code")), "%" + code.toLowerCase() + "%"),
+            builder.like(root.get("code"), "%" + code + "%")
+        );
+
+    Predicate hasCourseName =
+        builder.or(
+            builder.like(builder.lower(root.get("name")), "%" + name.toLowerCase() + "%"),
+            builder.like(root.get("name"), "%" + name + "%")
+        );
+
+    Predicate hasTeacherFirstName =
+              builder.or(
+                      builder.like(builder.lower(teacher.get("firstName")), "%" + teacher_first_name.toLowerCase() + "%"),
+                      builder.like(teacher.get("firstName"), "%" + teacher_first_name + "%")
+              );
+      Predicate hasTeacherLastName =
+              builder.or(
+                      builder.like(builder.lower(teacher.get("lastName")), "%" + teacher_last_name.toLowerCase() + "%"),
+                      builder.like(teacher.get("lastName"), "%" + teacher_last_name + "%")
+              );
+
+    Predicate hasCourseCredits = builder.equal(root.get("credits"), credits);
+
+if (credits==null){
+    query
+            .where(builder.and(hasCourseCode,hasCourseName,builder.or(hasTeacherFirstName,hasTeacherLastName)))
+            .orderBy(QueryUtils.toOrders(pageable.getSort(), root, builder));
+}else {
+    query
+            .where(builder.and(hasCourseCode,hasCourseName,builder.or(hasTeacherFirstName,hasTeacherLastName,hasCourseCredits)))
+            .orderBy(QueryUtils.toOrders(pageable.getSort(), root, builder));
+}
+
+
+
+    return entityManager.createQuery(query)
+        .setFirstResult((pageable.getPageNumber()) * pageable.getPageSize())
+        .setMaxResults(pageable.getPageSize())
+        .getResultList();
+  }
+
+
+}
