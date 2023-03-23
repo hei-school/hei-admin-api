f000330c53489595967806a5dde5302ab2f78543
hidden-machina <hei.franco.3@gmail.com>
2023-03-23 20:10:48 +0300

    fix:queryBuilder return correct jpql query now

--- src/main/java/school/hei/haapi/repository/utils/QueryBuilder.java
+++ src/main/java/school/hei/haapi/repository/utils/QueryBuilder.java
@@ -18,14 +18,14 @@ public class QueryBuilder {
         String query = "select c from course c join \"user\" u on u.id = c.user.id where ";
         int acc=0;
         if(code !=null){
-            query=query.concat("lower(c.code) like lower(concat('%',"+code+",'%')) ");
+            query=query.concat("lower(c.code) like lower(concat('%','"+code+"','%')) ");
             acc+=1;
         }
         if(name!=null){
             if(acc!=0){
-                query=query.concat("and lower(c.name) like lower(concat('%',"+name+",'%')) ");
+                query=query.concat("and lower(c.name) like lower(concat('%','"+name+"','%')) ");
             }else {
-                query=query.concat("lower(c.name) like lower(concat('%',"+name+",'%')) ");
+                query=query.concat("lower(c.name) like lower(concat('%','"+name+"','%')) ");
                 acc+=1;
             }
         }
@@ -39,17 +39,17 @@ public class QueryBuilder {
         }
         if(firstName!=null){
             if(acc!=0){
-                query=query.concat("and lower(u.first_name) like lower(concat('%',"+firstName+",'%')) ");
+                query=query.concat("and lower(u.first_name) like lower(concat('%','"+firstName+"','%')) ");
             }else {
-                query=query.concat("lower(u.first_name) like lower(concat('%',"+firstName+",'%')) ");
+                query=query.concat("lower(u.first_name) like lower(concat('%','"+firstName+"','%')) ");
                 acc+=1;
             }
         }
         if(lastName!=null){
             if(acc!=0){
-                query=query.concat("and lower(u.last_name) like lower(concat('%',"+lastName+",'%')) ");
+                query=query.concat("and lower(u.last_name) like lower(concat('%','"+lastName+"','%')) ");
             }else {
-                query=query.concat("lower(u.last_name) like lower(concat('%',"+lastName+",'%')) ");
+                query=query.concat("lower(u.last_name) like lower(concat('%','"+lastName+"','%')) ");
             }
         }
         if(creditOrder!=null && codeOrder!=null){
