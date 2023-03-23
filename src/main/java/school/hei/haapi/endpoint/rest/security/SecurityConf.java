6b5b9d6afddd08954f325a6faed7ce6988d2cffa
Lova Fenohasina Lala <fenohasinalala@gmail.com>
2023-03-24 01:07:26 +0300

    feat: add validator concerning credits and code order for get /courses

--- src/main/java/school/hei/haapi/endpoint/rest/security/SecurityConf.java
+++ src/main/java/school/hei/haapi/endpoint/rest/security/SecurityConf.java
@@ -84,6 +84,7 @@ public class SecurityConf extends WebSecurityConfigurerAdapter {
         .antMatchers(POST, "/students/*/fees/*/payments").hasAnyRole(MANAGER.getRole())
         .antMatchers(GET, "/students/*/fees").hasAnyRole(MANAGER.getRole())
         .antMatchers(POST, "/students/*/fees").hasAnyRole(MANAGER.getRole())
+            .antMatchers(GET, "/students/*/courses/**").hasAnyRole(MANAGER.getRole())
             .antMatchers(GET, "/students/*/courses").hasAnyRole(MANAGER.getRole())
             .requestMatchers(new SelfMatcher(GET, "/students/*/courses")).hasAnyRole(STUDENT.getRole())
             .antMatchers(PUT, "/students/*/courses").hasAnyRole(MANAGER.getRole())
