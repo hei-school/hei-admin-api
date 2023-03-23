32fb6ccc9348d5571385b366702129514432dcf0
lianafinaritra <hei.lianafinaritra@gmail.com>
2023-03-24 00:20:17 +0300

    feat: add filter to course repository

--- src/main/java/school/hei/haapi/repository/CourseRepository.java
+++ src/main/java/school/hei/haapi/repository/CourseRepository.java
@@ -4,6 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import school.hei.haapi.model.Course;
 
+import java.util.List;
+
 @Repository
 public interface CourseRepository extends JpaRepository<Course, String> {
+    /* List<Course> findAllByNameIgnoreCaseAndOrMainTeacherFirstNameIgnoreCaseAndOrMainTeacherLastNameIgnoreCaseAndCodeAndCredits();*/
+    List<Course> findAllByNameIgnoreCaseAndOrMainTeacherFirstNameAndOrMainTeacherLastNameAndOrCodeAnAndOrCreditsOrderByCodeDescCreditsDesc(
+            String name, String firstName, String lastName, String code, int credits);
 }
