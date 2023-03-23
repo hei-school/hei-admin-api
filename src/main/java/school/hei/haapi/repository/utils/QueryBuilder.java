6db076473616a0f3f45f2a6c538ac6fea9dcb5d7
hidden-machina <hei.franco.3@gmail.com>
2023-03-23 14:26:44 +0300

    backups for my change

--- src/main/java/school/hei/haapi/repository/utils/QueryBuilder.java
+++ src/main/java/school/hei/haapi/repository/utils/QueryBuilder.java
@@ -8,13 +8,13 @@ import lombok.NoArgsConstructor;
 public class QueryBuilder {
     public String getCourseAndFilterQuery(String code,String name,Integer credits,String firstName,String lastName){
         String query = "select c from course c where ";
-        if(code.equals(null) == false){
-            query.concat("c.code = "+code+" ");
+        if(code != null){
+            query = query.concat("c.code = "+code+" ");
         }
-        if(name.equals(null) == false){
+        if(name != null){
             query.concat("c.name = "+name+" ");
         }
-        if(credits!=null){
+        if(credits != null){
             query.concat("c.credits = "+credits+" ");
         }
         return query;
