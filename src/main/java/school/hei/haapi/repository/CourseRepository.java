2940be1ac82bb16c2119b9bd08c181807f9c585c
Micael Jerry <hei.jerry.2@gmail.com>
2023-03-23 22:10:11 +0300

    chore: jpql query to filter course

--- src/main/java/school/hei/haapi/repository/CourseRepository.java
+++ src/main/java/school/hei/haapi/repository/CourseRepository.java
@@ -12,14 +12,65 @@ public interface CourseRepository extends JpaRepository<Course, String> {
     @Query("SELECT c FROM Course c WHERE LOWER(c.code) LIKE LOWER(CONCAT('%', :code, '%')) "
             + "AND LOWER(c.name) LIKE LOWER(CONCAT('%', :name, '%')) "
             + "AND (:credits IS NULL OR c.credits = :credits) "
+    )
+    List<Course> findAllContainsIgnoreCaseByCodeAndNameAndCredits(
+            @Param("code") String code,
+            @Param("name") String name,
+            @Param("credits") Integer credits,
+            Pageable pageable
+    );
+
+    default List<Course> findAllContainsIgnoreCaseByCodeAndNameAndCreditsAndTeacher(
+            String code, String name, Integer credits, String teacherFirstName,
+            String teacherLastName, Pageable pageable
+    ) {
+        return (teacherFirstName != null && teacherLastName == null)
+                ? this.findAllContainsIgnoreCaseByCodeAndNameAndCreditsAndTeacher_firstName(
+                code, name, credits, teacherFirstName, pageable)
+                : (teacherFirstName == null && teacherLastName != null)
+                ? this.findAllContainsIgnoreCaseByCodeAndNameAndCreditsAndTeacher_lastName(
+                code, name, credits, teacherLastName, pageable)
+                : this.findAllContainsIgnoreCaseByCodeAndNameAndCreditsAndTeacher_firstNameAndTeacher_lastName(
+                code, name, credits, teacherFirstName, teacherLastName, pageable);
+    }
+
+    @Query("SELECT c FROM Course c WHERE LOWER(c.code) LIKE LOWER(CONCAT('%', :code, '%')) "
+            + "AND LOWER(c.name) LIKE LOWER(CONCAT('%', :name, '%')) "
+            + "AND (:credits IS NULL OR c.credits = :credits) "
             + "AND (LOWER(c.mainTeacher.firstName) LIKE LOWER(CONCAT('%', :teacher_first_name, '%')) "
             + "OR LOWER(c.mainTeacher.lastName) LIKE LOWER(CONCAT('%', :teacher_last_name, '%')))"
     )
-    List<Course> findCoursesByCriteria(
+    List<Course> findAllContainsIgnoreCaseByCodeAndNameAndCreditsAndTeacher_firstNameAndTeacher_lastName(
             @Param("code") String code,
             @Param("name") String name,
             @Param("credits") Integer credits,
             @Param("teacher_first_name") String teacherFirstName,
+            @Param("teacher_last_name") String teacherLastName,
+            Pageable pageable
+    );
+
+    @Query("SELECT c FROM Course c WHERE LOWER(c.code) LIKE LOWER(CONCAT('%', :code, '%')) "
+            + "AND LOWER(c.name) LIKE LOWER(CONCAT('%', :name, '%')) "
+            + "AND (:credits IS NULL OR c.credits = :credits) "
+            + "AND LOWER(c.mainTeacher.firstName) LIKE LOWER(CONCAT('%', :teacher_first_name, '%'))"
+    )
+    List<Course> findAllContainsIgnoreCaseByCodeAndNameAndCreditsAndTeacher_firstName(
+            @Param("code") String code,
+            @Param("name") String name,
+            @Param("credits") Integer credits,
+            @Param("teacher_first_name") String teacherFirstName,
+            Pageable pageable
+    );
+
+    @Query("SELECT c FROM Course c WHERE LOWER(c.code) LIKE LOWER(CONCAT('%', :code, '%')) "
+            + "AND LOWER(c.name) LIKE LOWER(CONCAT('%', :name, '%')) "
+            + "AND (:credits IS NULL OR c.credits = :credits) "
+            + "AND LOWER(c.mainTeacher.lastName) LIKE LOWER(CONCAT('%', :teacher_last_name, '%'))"
+    )
+    List<Course> findAllContainsIgnoreCaseByCodeAndNameAndCreditsAndTeacher_lastName(
+            @Param("code") String code,
+            @Param("name") String name,
+            @Param("credits") Integer credits,
             @Param("teacher_last_name") String teacherLastName,
             Pageable pageable
     );
