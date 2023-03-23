a14f02c9c76c4b27886485e3e2e437763ea58818
Micael Jerry <hei.jerry.2@gmail.com>
2023-03-23 16:17:01 +0300

    chore: sort courses service

--- src/main/java/school/hei/haapi/service/CourseService.java
+++ src/main/java/school/hei/haapi/service/CourseService.java
@@ -3,10 +3,12 @@ package school.hei.haapi.service;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import school.hei.haapi.model.BoundedPageSize;
 import school.hei.haapi.model.Course;
 import school.hei.haapi.model.PageFromOne;
+import school.hei.haapi.model.exception.BadRequestException;
 import school.hei.haapi.repository.CourseRepository;
 
 import java.util.List;
@@ -25,13 +27,30 @@ public class CourseService {
     }
 
     public List<Course> getAllByCriteria(
-            PageFromOne page, BoundedPageSize pageSize, String code,
-            String name, Integer credits, String teacherFirstName, String teacherLastName) {
-        Pageable pageable = PageRequest.of(
-                page.getValue() - 1,
-                pageSize.getValue()
-        );
+            PageFromOne page, BoundedPageSize pageSize, String code, String name, Integer credits,
+            String teacherFirstName, String teacherLastName, String codeOrder, String creditsOrder) {
         return courseRepository
-                .findCoursesByCriteria(code, name, credits, teacherFirstName, teacherLastName, pageable);
+                .findCoursesByCriteria(code, name, credits, teacherFirstName, teacherLastName,
+                        this.pagination(page, pageSize, codeOrder, creditsOrder));
+    }
+
+    private Pageable pagination(PageFromOne page, BoundedPageSize pageSize, String codeOrder, String creditsOrder) {
+        Sort sortCode = this.order("code", codeOrder);
+        Sort sortCredits = this.order("credits", creditsOrder);
+        Sort sort = (sortCode != null && sortCredits != null) ? sortCode.and(sortCredits)
+                : (sortCode != null) ? sortCode
+                : sortCredits;
+        return (sort != null) ? PageRequest.of(page.getValue() - 1, pageSize.getValue(), sort)
+                : PageRequest.of(page.getValue() - 1, pageSize.getValue());
+    }
+
+    private Sort order(String orderBy, String direction) {
+        if (direction == null) {
+            return null;
+        } else if (!direction.equals("ASC") && !direction.equals("DESC")) {
+            throw new BadRequestException(orderBy + "Order value must be ASC or DESC");
+        } else {
+            return Sort.by(Sort.Direction.valueOf(direction), orderBy);
+        }
     }
 }
