f7eb37ce3efd137d0bc4cb5f23771b78e79080a3
NiceGuyMe <loica.etudiant@gmail.com>
2023-03-24 00:45:40 +0300

    feat: add pagination to courses

--- src/main/java/school/hei/haapi/repository/CourseRepository.java
+++ src/main/java/school/hei/haapi/repository/CourseRepository.java
@@ -3,18 +3,18 @@ package school.hei.haapi.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import school.hei.haapi.model.Course;
-import javax.validation.constraints.NotBlank;
+import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 @Repository
 public interface CourseRepository extends JpaRepository<Course, String> {
 
-    List<Course> getCoursesByMainTeacherFirstName(String mainTeacher_firstName);
+    List<Course> getCoursesByMainTeacherFirstName(String mainTeacherFirstName,Pageable pageable);
 
-    List<Course> getCoursesByMainTeacherLastName(String mainTeacher_lastName);
-    List<Course> getCoursesByCodeContainingIgnoreCase(String code);
+    List<Course> getCoursesByMainTeacherLastName(String mainTeacherLastName,Pageable pageable);
+    List<Course> getCoursesByCodeContainingIgnoreCase(String code,Pageable pageable);
 
-    List<Course> getCoursesByNameContainingIgnoreCase(String name);
+    List<Course> getCoursesByNameContainingIgnoreCase(String name,Pageable pageable);
 
-    List<Course> getCoursesByCredits(Integer credits);
+    List<Course> getCoursesByCredits(Integer credits,Pageable pageable);
 }
--- src/main/java/school/hei/haapi/service/CourseService.java
+++ src/main/java/school/hei/haapi/service/CourseService.java
@@ -1,10 +1,13 @@
 package school.hei.haapi.service;
 
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import school.hei.haapi.model.Course;
 import school.hei.haapi.repository.CourseRepository;
-
+import school.hei.haapi.model.PageFromOne;
+import school.hei.haapi.model.BoundedPageSize;
 import java.util.List;
 
 @Service
@@ -15,23 +18,29 @@ public class CourseService {
     public List<Course> getAllCourse (){
         return courseRepository.findAll();
     }
-    public List<Course> getAllCoursesByTeacherFirstName (String teacherFirstName){
-        return courseRepository.getCoursesByMainTeacherFirstName(teacherFirstName);
+    public List<Course> getAllCoursesByTeacherFirstName (PageFromOne page,BoundedPageSize pageSize,String teacherFirstName){
+        Pageable pageable = PageRequest.of(page.getValue() -1 ,pageSize.getValue());
+
+        return courseRepository.getCoursesByMainTeacherFirstName(teacherFirstName, pageable);
     }
-    public List<Course> getAllCourseByTeacherLastName (String teacherLastName){
-        return courseRepository.getCoursesByMainTeacherLastName(teacherLastName);
+    public List<Course> getAllCourseByTeacherLastName (PageFromOne page,BoundedPageSize pageSize,String teacherLastName){
+        Pageable pageable = PageRequest.of(page.getValue() -1 ,pageSize.getValue());
+        return courseRepository.getCoursesByMainTeacherLastName(teacherLastName, pageable);
     }
 
-    public List<Course> getAllCourseByCode (String code){
-        return courseRepository.getCoursesByCodeContainingIgnoreCase(code);
+    public List<Course> getAllCourseByCode (PageFromOne page,BoundedPageSize pageSize,String code){
+        Pageable pageable = PageRequest.of(page.getValue() -1 ,pageSize.getValue());
+        return courseRepository.getCoursesByCodeContainingIgnoreCase(code, pageable);
     }
 
-    public List<Course> getAllCourseByName(String name){
-        return courseRepository.getCoursesByNameContainingIgnoreCase(name);
+    public List<Course> getAllCourseByName(PageFromOne page,BoundedPageSize pageSize,String name){
+        Pageable pageable = PageRequest.of(page.getValue() -1 ,pageSize.getValue());
+        return courseRepository.getCoursesByNameContainingIgnoreCase(name, pageable);
     }
 
-    public List<Course> getAllCourseByCredits(Integer credits){
-        return courseRepository.getCoursesByCredits(credits);
+    public List<Course> getAllCourseByCredits(PageFromOne page,BoundedPageSize pageSize,Integer credits){
+        Pageable pageable = PageRequest.of(page.getValue() -1 ,pageSize.getValue());
+        return courseRepository.getCoursesByCredits(credits, pageable);
     }
 
 }
