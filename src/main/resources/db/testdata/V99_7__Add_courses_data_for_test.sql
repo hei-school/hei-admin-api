a8e9772b98b2ed0237d5429013efd5e8ec8d82ed
Iloniavo <hei.iloniavo@gmail.com>
2023-03-23 10:49:45 +0300

    chore: add data tests for courses

---
+++ src/main/resources/db/testdata/V99_7__Add_courses_data_for_test.sql
@@ -0,0 +1,6 @@
+insert into "course"
+(id_course, code, "name", credits, total_hours, id_teacher)
+values
+    ('course3_id','MGT1', 'Travail collaboratif', 10, 200, 'teacher1_id'),
+    ('course4_id','PROG3', 'Qualite et surete des applications', 8, 150, 'teacher3_id'),
+    ('course5_id','SYS1', 'Système d informations', 8, 150, 'teacher2_id')
