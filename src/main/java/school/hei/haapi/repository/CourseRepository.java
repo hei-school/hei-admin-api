cdf62302aa2f526b24f3d14a0a9f86615118e40a
Micael Jerry <hei.jerry.2@gmail.com>
2023-03-23 10:38:37 +0300

    chore: jpql query to filter course

--- src/main/java/school/hei/haapi/repository/CourseRepository.java
+++ src/main/java/school/hei/haapi/repository/CourseRepository.java
@@ -1,7 +1,26 @@
 package school.hei.haapi.repository;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import school.hei.haapi.model.Course;
 
+import java.util.List;
+
 public interface CourseRepository extends JpaRepository<Course, String> {
+    @Query("SELECT c FROM Course c WHERE LOWER(c.code) LIKE LOWER(CONCAT('%', :code, '%')) "
+            + "AND LOWER(c.name) LIKE LOWER(CONCAT('%', :name, '%')) "
+            + "AND (:credits IS NULL OR c.credits = :credits) "
+            + "AND (LOWER(c.mainTeacher.firstName) LIKE LOWER(CONCAT('%', :teacher_first_name, '%')) "
+            + "OR LOWER(c.mainTeacher.lastName) LIKE LOWER(CONCAT('%', :teacher_last_name, '%')))"
+    )
+    List<Course> findCoursesByCriteria(
+            @Param("code") String code,
+            @Param("name") String name,
+            @Param("credits") Integer credits,
+            @Param("teacher_first_name") String teacherFirstName,
+            @Param("teacher_last_name") String teacherLastName,
+            Pageable pageable
+    );
 }
