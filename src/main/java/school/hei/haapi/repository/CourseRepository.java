4c3624037bb30ae91878b67b0ea13d5ceab273a4
Razafimamonjisoa Mendrika Harizo <hei.mendrikaharizo@gmail.com>
2023-03-23 14:06:47 +0300

    backup version of code

--- src/main/java/school/hei/haapi/repository/CourseRepository.java
+++ src/main/java/school/hei/haapi/repository/CourseRepository.java
@@ -3,14 +3,7 @@ package school.hei.haapi.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 import school.hei.haapi.model.Course;
-import school.hei.haapi.model.User;
-
-import java.util.List;
-
-
 
 @Service
 public interface CourseRepository extends JpaRepository<Course, String> {
-
-
 }
