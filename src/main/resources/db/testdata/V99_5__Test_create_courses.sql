984d0407795904964dbe7ac780a071041d6701bc
Micael Jerry <hei.jerry.2@gmail.com>
2023-03-23 17:38:17 +0300

    chore: update course test data

--- src/main/resources/db/testdata/V99_5__Test_create_courses.sql
+++ src/main/resources/db/testdata/V99_5__Test_create_courses.sql
@@ -1,4 +1,5 @@
 insert into "course"
     (id, code, name, credits, total_hours, user_id)
-values ('course1_id', 'CRS21001', 'Name of course one', 1, 1, 'teacher1_id'),
-       ('course2_id', 'CRS21002', 'Name of course two', 2, 2, 'teacher1_id');
+values ('course1_id', 'CRS21001', 'Name of course one', 6, 12, 'teacher1_id'),
+       ('course2_id', 'CRS21002', 'Name of course two', 6, 48, 'teacher2_id'),
+       ('course3_id', 'CRS21003', 'Name of course three', 4, 24, 'teacher1_id')
