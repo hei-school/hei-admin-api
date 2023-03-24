dcf409059e773ebe9b523250520544cdf06333e7
heiherilala <hei.herilala@gmail.com>
2023-03-24 06:26:01 +0300

    fix: CourseManagerDao.java

--- src/main/java/school/hei/haapi/repository/dao/CourseManagerDao.java
+++ src/main/java/school/hei/haapi/repository/dao/CourseManagerDao.java
@@ -15,86 +15,66 @@ import javax.persistence.criteria.Join;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import java.util.List;
-import java.util.Objects;
 
 @Repository
 @AllArgsConstructor
 public class CourseManagerDao {
-  private EntityManager entityManager;
+    private EntityManager entityManager;
 
-  public List<Course> findByCriteria(String code, String name, Integer credits, String teacher_first_name, String teacher_last_name, String creditsOrder, String codeOrder,
-                                     Pageable pageable) {
-    CriteriaBuilder builder = entityManager.getCriteriaBuilder();
-    CriteriaQuery<Course> query = builder.createQuery(Course.class);
-    Root<Course> root = query.from(Course.class);
-      Join<Object,Object> teacher = root.join("MainTeacher");
+    public List<Course> findByCriteria(String code, String name, Integer credits, String teacher_first_name, String teacher_last_name, String creditsOrder, String codeOrder,
+                                       Pageable pageable) {
+        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Course> query = builder.createQuery(Course.class);
+        Root<Course> root = query.from(Course.class);
+        Join<Object,Object> teacher = root.join("MainTeacher");
 
-    Predicate hasCourseCode =
-        builder.or(
-            builder.like(builder.lower(root.get("code")), "%" + code.toLowerCase() + "%"),
-            builder.like(root.get("code"), "%" + code + "%")
-        );
+        Predicate hasCourseCode =
+                builder.or(
+                        builder.like(builder.lower(root.get("code")), "%" + code.toLowerCase() + "%"),
+                        builder.like(root.get("code"), "%" + code + "%")
+                );
 
-    Predicate hasCourseName = name.length()==0?
-        builder.or(
-            builder.like(builder.lower(root.get("name")), "%" + name.toLowerCase() + "%"),
-            builder.like(root.get("name"), "%" + name + "%"),
-            builder.isNull(root.get("name"))
-        ):
-        builder.or(
-            builder.like(builder.lower(root.get("name")), "%" + name.toLowerCase() + "%"),
-            builder.like(root.get("name"), "%" + name + "%")
-        );
+        Predicate hasCourseName = name.length()==0?
+                builder.or(
+                        builder.like(builder.lower(root.get("name")), "%" + name.toLowerCase() + "%"),
+                        builder.like(root.get("name"), "%" + name + "%"),
+                        builder.isNull(root.get("name"))
+                ):
+                builder.or(
+                        builder.like(builder.lower(root.get("name")), "%" + name.toLowerCase() + "%"),
+                        builder.like(root.get("name"), "%" + name + "%")
+                );
 
-    Predicate hasTeacherFirstName =
-              builder.or(
-                      builder.like(builder.lower(teacher.get("firstName")), "%" + teacher_first_name.toLowerCase() + "%"),
-                      builder.like(teacher.get("firstName"), "%" + teacher_first_name + "%")
-              );
-    Predicate hasTeacherLastName =
-              builder.or(
-                      builder.like(builder.lower(teacher.get("lastName")), "%" + teacher_last_name.toLowerCase() + "%"),
-                      builder.like(teacher.get("lastName"), "%" + teacher_last_name + "%")
-              );
+        Predicate hasTeacherFirstName =
+                builder.or(
+                        builder.like(builder.lower(teacher.get("firstName")), "%" + teacher_first_name.toLowerCase() + "%"),
+                        builder.like(teacher.get("firstName"), "%" + teacher_first_name + "%")
+                );
+        Predicate hasTeacherLastName =
+                builder.or(
+                        builder.like(builder.lower(teacher.get("lastName")), "%" + teacher_last_name.toLowerCase() + "%"),
+                        builder.like(teacher.get("lastName"), "%" + teacher_last_name + "%")
+                );
 
-    Predicate hasCourseCredits = builder.equal(root.get("credits"), credits);
+        Predicate hasTeacherFirstNameAndHasTeacherLastName = null;
+        hasTeacherFirstNameAndHasTeacherLastName = (teacher_first_name.length()>0 && teacher_last_name.length()>0)?
+                builder.or(hasTeacherFirstName,hasTeacherLastName):
+                builder.and(hasTeacherFirstName,hasTeacherLastName);
 
-      Predicate hasTeacherFirstNameAndHasTeacherLastName = null;
-      hasTeacherFirstNameAndHasTeacherLastName = (teacher_first_name.length()>0 && teacher_last_name.length()>0)?
-        builder.or(hasTeacherFirstName,hasTeacherLastName):
-        builder.and(hasTeacherFirstName,hasTeacherLastName);
+        Predicate hasCourseCredits = builder.equal(root.get("credits"), credits);
 
-
-if (credits==null){
-    query
-            .where(builder.and(hasCourseCode,hasCourseName,hasTeacherFirstNameAndHasTeacherLastName));
-}else {
-    query
-            .where(builder.and(hasCourseCode,hasCourseName,hasTeacherFirstNameAndHasTeacherLastName,hasCourseCredits));
-}
-
-      javax.persistence.criteria.Order codeArrange = builder.desc(root.get("code"));
-      if (Objects.equals(codeOrder, "ASC")){
-          codeArrange = builder.asc(root.get("code"));
-      }
-      javax.persistence.criteria.Order creditsArrange = builder.desc(root.get("credits"));
-      if (Objects.equals(creditsOrder, "ASC")){
-          creditsArrange = builder.asc(root.get("credits"));
-      }
-
-if (creditsOrder.length()>0 && codeOrder.length()>0){
-    query.orderBy(codeArrange,creditsArrange);
-} else if (creditsOrder.length()>0) {
-    query.orderBy(creditsArrange);
-} else if (codeOrder.length()>0) {
-    query.orderBy(codeArrange);
-}
-
-    return entityManager.createQuery(query)
-        .setFirstResult((pageable.getPageNumber()) * pageable.getPageSize())
-        .setMaxResults(pageable.getPageSize())
-        .getResultList();
-  }
-
-
+        if (credits==null){
+            query
+                    .where(builder.and(hasCourseCode,hasCourseName,hasTeacherFirstNameAndHasTeacherLastName,hasTeacherLastName))
+                    .orderBy(QueryUtils.toOrders(pageable.getSort(), root, builder));
+        }else {
+            query
+                    .where(builder.and(hasCourseCode,hasCourseName,hasTeacherFirstNameAndHasTeacherLastName,hasCourseCredits))
+                    .orderBy(QueryUtils.toOrders(pageable.getSort(), root, builder));
+        }
+        return entityManager.createQuery(query)
+                .setFirstResult((pageable.getPageNumber()) * pageable.getPageSize())
+                .setMaxResults(pageable.getPageSize())
+                .getResultList();
+    }
 }
