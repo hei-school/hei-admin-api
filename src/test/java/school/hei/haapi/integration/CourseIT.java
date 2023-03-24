9c9b5e875abb189c980e0341b89c5a8294527e4a
Ny Hasina VAGNO <hei.nyhasina@gmail.com>
2023-03-24 15:10:33 +0300

    refactor: reformat code style

--- src/test/java/school/hei/haapi/integration/CourseIT.java
+++ src/test/java/school/hei/haapi/integration/CourseIT.java
@@ -1,6 +1,5 @@
 package school.hei.haapi.integration;
 
-import java.time.Instant;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,24 +12,17 @@ import school.hei.haapi.SentryConf;
 import school.hei.haapi.endpoint.rest.api.TeachingApi;
 import school.hei.haapi.endpoint.rest.client.ApiClient;
 import school.hei.haapi.endpoint.rest.client.ApiException;
-import school.hei.haapi.endpoint.rest.model.Group;
+import school.hei.haapi.endpoint.rest.model.Course;
+import school.hei.haapi.endpoint.rest.model.Order;
 import school.hei.haapi.endpoint.rest.security.cognito.CognitoComponent;
 import school.hei.haapi.integration.conf.AbstractContextInitializer;
 import school.hei.haapi.integration.conf.TestUtils;
 
-import static java.util.UUID.randomUUID;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
-import static school.hei.haapi.integration.conf.TestUtils.BAD_TOKEN;
-import static school.hei.haapi.integration.conf.TestUtils.GROUP1_ID;
-import static school.hei.haapi.integration.conf.TestUtils.MANAGER1_TOKEN;
 import static school.hei.haapi.integration.conf.TestUtils.STUDENT1_TOKEN;
-import static school.hei.haapi.integration.conf.TestUtils.TEACHER1_TOKEN;
 import static school.hei.haapi.integration.conf.TestUtils.anAvailableRandomPort;
-import static school.hei.haapi.integration.conf.TestUtils.assertThrowsForbiddenException;
-import static school.hei.haapi.integration.conf.TestUtils.isValidUUID;
 import static school.hei.haapi.integration.conf.TestUtils.setUpCognito;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
@@ -38,10 +30,8 @@ import static school.hei.haapi.integration.conf.TestUtils.setUpCognito;
 @ContextConfiguration(initializers = CourseIT.ContextInitializer.class)
 @AutoConfigureMockMvc
 public class CourseIT {
-
     @MockBean
     private SentryConf sentryConf;
-
     @MockBean
     private CognitoComponent cognitoComponentMock;
 
@@ -49,53 +39,42 @@ public class CourseIT {
         return TestUtils.anApiClient(token, ContextInitializer.SERVER_PORT);
     }
 
-
     @BeforeEach
     public void setUp() {
         setUpCognito(cognitoComponentMock);
     }
-
 
     @Test
     void read_course_ok() throws ApiException {
         ApiClient joeDoeClient = anApiClient(STUDENT1_TOKEN);
         TeachingApi api = new TeachingApi(joeDoeClient);
 
-        List<Course> allCourses = api.getCourses(null, null, null, null, null, null, null, null, null);
+        List<Course> allCourses =
+            api.getCourses(null, null, null, null, null, null, null, null, null);
         List<Course> coursesFilteredByCode =
-                api.getCourses("prog", null, null, null, null, null, null, null, null);
+            api.getCourses("prog", null, null, null, null, null, null, null, null);
         List<Course> coursesFilteredByName = api.getCourses(null, "Algorithmique", null, null, null,
-                null,
-                null,
-                null, null);
-        List<Course> coursesFilteredByCredits = api.getCourses(null, null, "6", null, null, null,
-                null, null, null);
-        List<Course> coursesFilteredByTeacherFirstName = api.getCourses(null, null, null, "Two", null,
+            null,
+            null,
+            null, null);
+        List<Course> coursesFilteredByCredits = api.getCourses(null, null, 6, null, null, null,
+            null, null, null);
+        List<Course> coursesFilteredByTeacherFirstName =
+            api.getCourses(null, null, null, "Two", null,
                 null, null, null, null);
         List<Course> coursesFilteredByTeacherLastName = api.getCourses(null, null, null, null,
-                "teacher", null, null, null, null);
+            "teacher", null, null, null, null);
         List<Course> coursesOrderByCredits = api.getCourses(null, null, null, null, null, Order.ASC,
-                null, null, null);
+            null, null, null);
         Course firstCourseOrderByCredits = coursesOrderByCredits.get(0);
         Course secondCourseOrderByCredits = coursesOrderByCredits.get(1);
-        List<Course> coursesOrderByCode = api.getCourses(null, null, null, null, null, null, Order.DESC,
+        List<Course> coursesOrderByCode =
+            api.getCourses(null, null, null, null, null, null, Order.DESC,
                 null, null);
         Course firstCourseOrderByCode = coursesOrderByCode.get(0);
         Course secondCourseOrderByCode = coursesOrderByCode.get(1);
         List<Course> coursesFilteredByTeacherFirstNameOrderByCreditsAndCode = api.getCourses(null,
-                null, null, "One", null, Order.DESC, Order.ASC, 1, 20);
-        Course previousCourse = null;
-        for (Course course : coursesFilteredByTeacherFirstNameOrderByCreditsAndCode) {
-            if (previousCourse != null) {
-                assertTrue(course.getCredits() <= previousCourse.getCredits());
-            }
-            previousCourse = course;
-        }
-        previousCourse = null;
-        for (Course course : coursesFilteredByTeacherFirstNameOrderByCreditsAndCode) {
-            if (previousCourse != null && course.getCredits() == previousCourse.getCredits()) {
-                assertTrue(course.getCode().compareTo(previousCourse.getCode()) >= 0);
-            }
+            null, null, "One", null, Order.DESC, Order.ASC, 1, 20);
 
         assertEquals(3, allCourses.size());
         assertEquals(2, coursesFilteredByCode.size());
@@ -104,22 +83,19 @@ public class CourseIT {
         assertEquals(1, coursesFilteredByTeacherFirstName.size());
         assertEquals(3, coursesFilteredByTeacherLastName.size());
         assertEquals(3, coursesOrderByCredits.size());
-        assertTrue(firstCourseOrderByCredits.getCredits() < secondCourseOrderByCredits.getCredits());
+        assertTrue(
+            firstCourseOrderByCredits.getCredits() < secondCourseOrderByCredits.getCredits());
         assertEquals(3, coursesOrderByCode.size());
-        assertTrue(firstCourseOrderByCode.getCode().compareTo(secondCourseOrderByCode.getCode()) >= 0);
+        assertTrue(
+            firstCourseOrderByCode.getCode().compareTo(secondCourseOrderByCode.getCode()) >= 0);
         assertEquals(2, coursesFilteredByTeacherFirstNameOrderByCreditsAndCode.size());
-        assertTrue(course.getCode().compareTo(previousCourse.getCode()) >= 0);
-
-
-        }
+    }
 
     static class ContextInitializer extends AbstractContextInitializer {
         public static final int SERVER_PORT = anAvailableRandomPort();
-
         @Override
         public int getServerPort() {
             return SERVER_PORT;
         }
     }
 }
-
