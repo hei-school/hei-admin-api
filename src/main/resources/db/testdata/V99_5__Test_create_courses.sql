3d50e8ed2d59b220bfa134835e6da9d18fbedc73
Jean Koletto Reichmann GALY <hei.reichmann.4@gmail.com>
2023-03-24 15:01:15 +0300

    feat: update test data

--- src/main/resources/db/testdata/V99_5__Test_create_courses.sql
+++ src/main/resources/db/testdata/V99_5__Test_create_courses.sql
@@ -1,5 +1,6 @@
 insert into "course"
     (id, code, name, credits, total_hours, main_teacher_id)
 values ('course1_id', 'PROG1', 'Algorithmics', 6, 20, 'teacher1_id'),
-       ('course2_id', 'WEB1', 'Web interface', 6, 24, 'teacher2_id'),
-       ('course3_id', 'SYS1', 'Operating system', 6, 22, 'teacher3_id');
+       ('course2_id', 'WEB1', 'Web interface', 7, 24, 'teacher2_id'),
+       ('course3_id', 'SYS1', 'Operating system', 8, 22, 'teacher3_id'),
+       ('course4_id', 'PROG2', 'Object-oriented programming', 6, 21, 'teacher1_id');
