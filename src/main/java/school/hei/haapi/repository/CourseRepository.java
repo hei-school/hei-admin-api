942679ca10df9f76e0c2f45dedd08d1cbe8e5799
Mahefa Ny Anjara <hei.mahefa@gmail.com>
2023-03-24 15:05:30 +0300

    chore: add filter by teacher firstname and teacher lastname

--- src/main/java/school/hei/haapi/repository/CourseRepository.java
+++ src/main/java/school/hei/haapi/repository/CourseRepository.java
@@ -10,10 +10,14 @@ import school.hei.haapi.model.Course;
 public interface CourseRepository extends JpaRepository<Course, String> {
   @Query("select c from Course c where " + "lower(c.code) like concat('%', lower(:code) , '%') and "
       + "lower(c.name) like concat('%', lower(:name), '%') and "
-      + "(:credits is null or c.credits = :credits)")
+      + "(:credits is null or c.credits = :credits) and "
+      + "(lower(c.mainTeacher.firstName) like concat('%', lower(:teacherFirstName), '%') or "
+      + "lower(c.mainTeacher.lastName) like concat('%', lower(:teacherLastName), '%'))")
   List<Course> findByCriteria(
       @Param("code") String code,
       @Param("name") String name,
       @Param("credits") Integer credits,
+      @Param("teacherFirstName") String teacherFirstName,
+      @Param("teacherLastName") String teacherLastName,
       Pageable pageable);
 }
