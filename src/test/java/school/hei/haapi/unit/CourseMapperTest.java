15f6fff3fa25eb9455c64b9a988e65879b52e573
RyanIaro <hei.ryan.3@gmail.com>
2023-03-24 16:32:33 +0300

    fix: fixed typing issues in CourseMapperTest.java

--- src/test/java/school/hei/haapi/unit/CourseMapperTest.java
+++ src/test/java/school/hei/haapi/unit/CourseMapperTest.java
@@ -3,39 +3,39 @@ package school.hei.haapi.unit;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import school.hei.haapi.endpoint.rest.mapper.CourseMapper;
-import school.hei.haapi.endpoint.rest.mapper.UserMapper;
-import school.hei.haapi.endpoint.rest.model.Courses;
-import school.hei.haapi.endpoint.rest.model.User;
+import school.hei.haapi.endpoint.rest.model.Course;
+import school.hei.haapi.endpoint.rest.model.Teacher;
+import school.hei.haapi.model.Courses;
+import school.hei.haapi.model.User;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-import java.util.List;
 
 @Slf4j
 public class CourseMapperTest {
     CourseMapper courseMapper = mock(CourseMapper.class);
     @Test
     void course_to_domain_ok(){
-        Course courses = Courses.builder()
-                .id("1")
-                .name("Algorithme")
-                .code("PROG1")
-                .total_hours("0")
-                .main_teacher(User.builder()
-                        .role(User.Role.TEACHER)
-                        .build())
+
+        Teacher teacher1 = new Teacher();
+        User user1 = User.builder().build();
+
+        Course course = new Course();
+        course.setId("1");
+        course.setName("Algorithmique");
+        course.setCode("PROG1");
+        course.setCredits(6);
+        course.setTotalHours(30);
+        course.setMainTeacher(teacher1);
+        Courses expected = Courses.builder()
+                .id(course.getId())
+                .credits(course.getCredits())
+                .main_teacher(user1)
+                .total_hours(course.getTotalHours())
+                .name(course.getName())
+                .code(course.getCode())
                 .build();
-        Courses exepted = Courses.builder()
-                .id(courses.getId())
-                .credits(courses.getCredits())
-                .main_teacher(courses.getMain_teacher())
-                .total_hours(courses.getTotal_hours())
-                .name(courses.getName())
-                .code(courses.getCode())
-                .build();
-        when(courseMapper.toDomain(courses)).thenReturn(courses);
-        Courses actual = courseMapper.toDomain(courses);
-        assertEquals(exepted, actual);
+        Courses actual = courseMapper.toDomain(course);
+        assertEquals(expected, actual);
     }
 }
