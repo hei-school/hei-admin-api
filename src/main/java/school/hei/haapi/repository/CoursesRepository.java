1a428dea1a1616580531808bf1ce2a6d0918f3d3
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2023-03-24 10:11:34 +0300

    feat: add repo-filter
    Signed-off-by: Safidy-Randriantseheno <hei.safidy.7@gmail.com>

--- src/main/java/school/hei/haapi/repository/CoursesRepository.java
+++ src/main/java/school/hei/haapi/repository/CoursesRepository.java
@@ -1,8 +1,15 @@
 package school.hei.haapi.repository;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import school.hei.haapi.model.Courses;
+import school.hei.haapi.model.User;
+
+import java.util.List;
 
 public interface CoursesRepository extends JpaRepository<Courses, String> {
-
+    List<Courses> findByCodeAndRefContainingIgnoreCaseAndNameContainingIgnoreCaseAndCreditsContainingIgnoreCaseAndTeacher_first_NameContainingIgnoreCaseAndTeacher_last_nameContainingIgnoreCase(
+             String code, String name, Integer credits, String teacher_first_name, String teacher_last_name, Pageable pageable);
 }
+
+
