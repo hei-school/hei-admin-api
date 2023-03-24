f90198d99bd7e486e5c58ce12eb1b32d588fecdd
Jean Koletto Reichmann GALY <hei.reichmann.4@gmail.com>
2023-03-24 16:50:24 +0300

    fix: fix course service and CourseManagerDao

--- src/main/java/school/hei/haapi/repository/dao/CourseManagerDao.java
+++ src/main/java/school/hei/haapi/repository/dao/CourseManagerDao.java
@@ -2,7 +2,6 @@ package school.hei.haapi.repository.dao;
 
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.query.QueryUtils;
 import org.springframework.stereotype.Repository;
 import school.hei.haapi.model.Course;
 import school.hei.haapi.model.User;
@@ -22,12 +21,18 @@ import java.util.Objects;
 public class CourseManagerDao {
     private EntityManager entityManager;
 
-    public List<Course> findByCriteria(String code, String name, Integer credits, String teacher_first_name, String teacher_last_name, String creditsOrder, String codeOrder,
+    public List<Course> findByCriteria(String code,
+                                       String name,
+                                       Integer credits,
+                                       String firstName,
+                                       String lastName,
+                                       String creditsOrder,
+                                       String codeOrder,
                                        Pageable pageable) {
         CriteriaBuilder builder = entityManager.getCriteriaBuilder();
         CriteriaQuery<Course> query = builder.createQuery(Course.class);
         Root<Course> root = query.from(Course.class);
-        Join<Object,Object> teacher = root.join("MainTeacher");
+        Join<Course, User> teacher = root.join("mainTeacher");
 
         Predicate hasCourseCode =
                 builder.or(
@@ -35,12 +40,7 @@ public class CourseManagerDao {
                         builder.like(root.get("code"), "%" + code + "%")
                 );
 
-        Predicate hasCourseName = name.length()==0?
-                builder.or(
-                        builder.like(builder.lower(root.get("name")), "%" + name.toLowerCase() + "%"),
-                        builder.like(root.get("name"), "%" + name + "%"),
-                        builder.isNull(root.get("name"))
-                ):
+        Predicate hasCourseName =
                 builder.or(
                         builder.like(builder.lower(root.get("name")), "%" + name.toLowerCase() + "%"),
                         builder.like(root.get("name"), "%" + name + "%")
@@ -48,19 +48,19 @@ public class CourseManagerDao {
 
         Predicate hasTeacherFirstName =
                 builder.or(
-                        builder.like(builder.lower(teacher.get("firstName")), "%" + teacher_first_name.toLowerCase() + "%"),
-                        builder.like(teacher.get("firstName"), "%" + teacher_first_name + "%")
+                        builder.like(builder.lower(teacher.get("firstName")), "%" + firstName.toLowerCase() + "%"),
+                        builder.like(teacher.get("firstName"), "%" + firstName + "%")
                 );
         Predicate hasTeacherLastName =
                 builder.or(
-                        builder.like(builder.lower(teacher.get("lastName")), "%" + teacher_last_name.toLowerCase() + "%"),
-                        builder.like(teacher.get("lastName"), "%" + teacher_last_name + "%")
+                        builder.like(builder.lower(teacher.get("lastName")), "%" + lastName.toLowerCase() + "%"),
+                        builder.like(teacher.get("lastName"), "%" + lastName + "%")
                 );
 
         Predicate hasCourseCredits = builder.equal(root.get("credits"), credits);
 
         Predicate hasTeacherFirstNameAndHasTeacherLastName = null;
-        hasTeacherFirstNameAndHasTeacherLastName = (teacher_first_name.length()>0 && teacher_last_name.length()>0)?
+        hasTeacherFirstNameAndHasTeacherLastName = (firstName.length()>0 && lastName.length()>0)?
                 builder.or(hasTeacherFirstName,hasTeacherLastName):
                 builder.and(hasTeacherFirstName,hasTeacherLastName);
         
--- src/main/java/school/hei/haapi/service/CourseService.java
+++ src/main/java/school/hei/haapi/service/CourseService.java
@@ -7,8 +7,6 @@ import org.springframework.stereotype.Service;
 import school.hei.haapi.model.BoundedPageSize;
 import school.hei.haapi.model.Course;
 import school.hei.haapi.model.PageFromOne;
-import school.hei.haapi.model.exception.BadRequestException;
-import school.hei.haapi.repository.CourseRepository;
 import school.hei.haapi.repository.dao.CourseManagerDao;
 
 import java.util.List;
@@ -16,10 +14,9 @@ import java.util.List;
 @Service
 @AllArgsConstructor
 public class CourseService {
-    private final CourseRepository courseRepository;
     private final CourseManagerDao courseManagerDao;
 
-    public List<Course> getCourses(PageFromOne page, BoundedPageSize pageSize,String code,String name,Integer credits,String teacher_first_name,String teacher_last_name,String creditsOrder,String codeOrder){
+    public List<Course> getCourses(PageFromOne page, BoundedPageSize pageSize,String code,String name,Integer credits,String firstName,String lastName,String creditsOrder,String codeOrder){
         if (page == null){
             page = new PageFromOne(1);
         }
@@ -31,6 +28,6 @@ public class CourseService {
                 page.getValue() -1,
                 pageSize.getValue());
         return courseManagerDao.findByCriteria(
-                code, name, credits, teacher_first_name, teacher_last_name, creditsOrder, codeOrder, pageable);
+                code, name, credits, firstName, lastName, creditsOrder, codeOrder, pageable);
     }
 }
