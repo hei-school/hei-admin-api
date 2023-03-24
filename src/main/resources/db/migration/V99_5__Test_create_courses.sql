e056a4f4518540b86ca8d9af2c5f596374bfa7e0
soamariaka <hei.soa.2@gmail.com>
2023-03-24 15:59:59 +0300

    feat: course migration added

---
+++ src/main/resources/db/migration/V99_5__Test_create_courses.sql
@@ -0,0 +1,6 @@
+insert into "course"
+    (id, code, name, credits, main_teacher_id, total_hours)
+values
+    ('course_ID', 'PROG1', 'JS', 6, 'teacher1_id', 20),
+    ('course2_ID', 'WEB1', 'HTML Basic', 4, 'teacher2_id', 19),
+    ('course3_ID', 'PROG3', 'JAVA', 6, 'teacher3_id', 28);
