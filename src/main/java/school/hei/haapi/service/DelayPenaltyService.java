03a33e36b3ffc6d6f57664bdfce541d97d4f3542
Micael Jerry <hei.jerry.2@gmail.com>
2023-03-28 18:08:17 +0300

    chore: get current delay penalty service

--- src/main/java/school/hei/haapi/service/DelayPenaltyService.java
+++ src/main/java/school/hei/haapi/service/DelayPenaltyService.java
@@ -1,4 +1,16 @@
 package school.hei.haapi.service;
 
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import school.hei.haapi.model.DelayPenalty;
+import school.hei.haapi.repository.DelayPenaltyRepository;
+
+@Service
+@AllArgsConstructor
 public class DelayPenaltyService {
+    private final DelayPenaltyRepository repository;
+
+    public DelayPenalty getDelayPenalty() {
+        return repository.findFirstBy();
+    }
 }
