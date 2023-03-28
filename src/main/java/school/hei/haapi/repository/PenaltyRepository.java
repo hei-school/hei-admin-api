6fd0629a6d8f8de78cef0db4983ec2225a3cde04
NiceGuyMe <loica.etudiant@gmail.com>
2023-03-28 13:47:15 +0300

    feat: add penalty services

--- src/main/java/school/hei/haapi/repository/PenaltyRepository.java
+++ src/main/java/school/hei/haapi/repository/PenaltyRepository.java
@@ -1,7 +1,8 @@
 package school.hei.haapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import school.hei.haapi.model.Penalty;
-
+@Repository
 public interface PenaltyRepository extends JpaRepository<Penalty, String> {
 }
---
+++ src/main/java/school/hei/haapi/service/PenaltyService.java
@@ -0,0 +1,13 @@
+package school.hei.haapi.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import school.hei.haapi.model.Penalty;
+import school.hei.haapi.repository.PenaltyRepository;
+
+@Service
+@AllArgsConstructor
+public class PenaltyService {
+    private final PenaltyRepository penaltyRepository;
+
+}
