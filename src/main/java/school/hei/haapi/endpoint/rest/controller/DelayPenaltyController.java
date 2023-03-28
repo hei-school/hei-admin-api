0870d5bba88d0373451b209e99b7576af6dcbe83
Micael Jerry <hei.jerry.2@gmail.com>
2023-03-28 18:08:54 +0300

    chore: GET /delay_penalty controller

--- src/main/java/school/hei/haapi/endpoint/rest/controller/DelayPenaltyController.java
+++ src/main/java/school/hei/haapi/endpoint/rest/controller/DelayPenaltyController.java
@@ -1,2 +1,20 @@
-package school.hei.haapi.endpoint.rest.controller;public class DelayPenaltyController {
+package school.hei.haapi.endpoint.rest.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import school.hei.haapi.endpoint.rest.mapper.DelayPenaltyMapper;
+import school.hei.haapi.endpoint.rest.model.DelayPenalty;
+import school.hei.haapi.service.DelayPenaltyService;
+
+@RestController
+@AllArgsConstructor
+public class DelayPenaltyController {
+    private final DelayPenaltyService delayPenaltyService;
+    private final DelayPenaltyMapper delayPenaltyMapper;
+
+    @GetMapping("/delay_penalty")
+    public DelayPenalty getDelayPenalty() {
+        return delayPenaltyMapper.toRest(delayPenaltyService.getDelayPenalty());
+    }
 }
