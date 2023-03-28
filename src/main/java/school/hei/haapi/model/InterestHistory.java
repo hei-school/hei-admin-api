dfe3be77b41b2cacfcbd363f5304713ef6b7596b
Lova Fenohasina Lala <fenohasinalala@gmail.com>
2023-03-28 08:11:33 +0300

    fix: equal in InterestHistory ; feat:add table for InterestHistory

--- src/main/java/school/hei/haapi/model/InterestHistory.java
+++ src/main/java/school/hei/haapi/model/InterestHistory.java
@@ -2,6 +2,7 @@ package school.hei.haapi.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -24,6 +25,7 @@ import java.util.Date;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode
 public class InterestHistory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
