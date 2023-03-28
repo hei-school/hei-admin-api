f9290947a640d10529819dadab08fb2758bdb67d
Lakara2 <lbryanlakara@gmail.com>
2023-03-28 14:11:53 +0300

    add: PenaltyController

---
+++ src/main/java/school/hei/haapi/endpoint/rest/controller/PenaltyController.java
@@ -0,0 +1,25 @@
+package school.hei.haapi.endpoint.rest.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import school.hei.haapi.model.Penalty;
+import school.hei.haapi.repository.PenaltyRepository;
+import school.hei.haapi.service.PenaltyService;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+public class PenaltyController {
+    private PenaltyService service;
+
+    private PenaltyRepository repository;
+
+
+    @GetMapping("/delay_penalty")
+    public List<Penalty> getAllCourse(){
+        return service.getAll();
+    }
+
+}
