6b6554318f14eb63524a36e3723586148c75144a
Lova Fenohasina Lala <fenohasinalala@gmail.com>
2023-03-28 16:43:26 +0300

    feat: feeMapper modification

--- src/main/java/school/hei/haapi/endpoint/rest/mapper/FeeMapper.java
+++ src/main/java/school/hei/haapi/endpoint/rest/mapper/FeeMapper.java
@@ -5,10 +5,13 @@ import org.springframework.stereotype.Component;
 import school.hei.haapi.endpoint.rest.model.CreateFee;
 import school.hei.haapi.endpoint.rest.model.Fee;
 import school.hei.haapi.endpoint.rest.validator.CreateFeeValidator;
+import school.hei.haapi.model.DelayPenalty;
+import school.hei.haapi.model.InterestHistory;
 import school.hei.haapi.model.Payment;
 import school.hei.haapi.model.User;
 import school.hei.haapi.model.exception.BadRequestException;
 import school.hei.haapi.model.exception.NotFoundException;
+import school.hei.haapi.service.DelayPenaltyService;
 import school.hei.haapi.service.InterestHistoryService;
 import school.hei.haapi.service.PaymentService;
 import school.hei.haapi.service.UserService;
@@ -17,6 +20,7 @@ import java.time.Instant;
 import java.time.LocalDate;
 import java.time.Period;
 import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.util.List;
 import java.util.Objects;
 
@@ -27,35 +31,67 @@ import static school.hei.haapi.endpoint.rest.model.Fee.StatusEnum.UNPAID;
 @AllArgsConstructor
 public class FeeMapper {
 
+  private final DelayPenaltyService delayPenaltyService;
   private final InterestHistoryService interestHistoryService;
   private final PaymentService paymentService;
   private final UserService userService;
   private final CreateFeeValidator createFeeValidator;
 
   private int computeTotalAmount(school.hei.haapi.model.Fee currentFee){
-    //TODO: change getInterestAmount() to getInterestAmountByFeeId()
-    return currentFee.getTotalAmount()+interestHistoryService.getInterestAmount();
+    return currentFee.getTotalAmount()+interestHistoryService.getInterestAmount(currentFee.getId());
   }
   private int computeRemainingAmountAmount(school.hei.haapi.model.Fee currentFee){
-    //TODO: change getInterestAmount() to getInterestAmountByFeeId()
-    int paymentAmount = 0;
+    int sumPayment = 0;
     List<Payment> payments = paymentService.getAllPaymentByStudentIdAndFeeId(currentFee.getStudent().getId(),currentFee.getId());
     for (Payment p:payments) {
-      paymentAmount = paymentAmount + p.getAmount();
+      sumPayment = sumPayment + p.getAmount();
     }
-    return computeTotalAmount(currentFee)-paymentAmount;
+    return computeTotalAmount(currentFee)-sumPayment;
+  }
+
+  private int convertInterestTimeRateToDayNumber( school.hei.haapi.endpoint.rest.model.DelayPenalty.InterestTimerateEnum interestTimeRate){
+    switch (interestTimeRate) {
+      case DAILY:
+        return 1;
+      default:
+        throw new BadRequestException(
+                "Unexpected delay Penalty Interest Time rate: " + interestTimeRate.getValue());
+    }
   }
 
   public Fee toRestFee(school.hei.haapi.model.Fee fee) {
-
-
     if (!fee.getStatus().equals(Fee.StatusEnum.PAID)){
       ZoneId zone = ZoneId.of("UTC");
       LocalDate today = LocalDate.ofInstant(Instant.now(), zone);
       LocalDate dueTime = LocalDate.ofInstant(fee.getDueDatetime(), zone);
-//TODO change dueTime.plusDays
-      if ( Period.between(today, dueTime.plusDays(0000)).getDays()>=0){
-
+      DelayPenalty generalRule = delayPenaltyService.getAll().get(0);
+      if ( Period.between(today, dueTime.plusDays(generalRule.getGraceDelay())).getDays()>=0){
+        List<InterestHistory> interestHistories = interestHistoryService.getAllByFeeId(fee.getId());
+        if (interestHistories.size()==0){
+          //toDO create InterestHistory
+          java.util.Date InterestStart = java.util.Date.from(dueTime.plusDays(generalRule.getGraceDelay()+1).atTime(12,0).toInstant(ZoneOffset.UTC));
+          java.util.Date InterestEnd = java.util.Date.from(dueTime.plusDays(generalRule.getGraceDelay()+1+ generalRule.getApplicabilityDelayAfterGrace()).atTime(12,0).toInstant(ZoneOffset.UTC));
+          InterestHistory toCreate = InterestHistory.builder()
+                  .fee(fee)
+                  .interestRate(generalRule.getInterestPercent())
+                  .interestTimeRate(convertInterestTimeRateToDayNumber(generalRule.getInterestTimeRate()))
+                  .interestStart(InterestStart)
+                  .interestEnd(InterestEnd)
+                  .build();
+          interestHistoryService.saveAll(List.of(toCreate));
+          interestHistories = interestHistoryService.getAllByFeeId(fee.getId());
+        }
+        return new Fee()
+                .id(fee.getId())
+                .studentId(fee.getStudent().getId())
+                .status(fee.getStatus())
+                .type(fee.getType())
+                .totalAmount(computeTotalAmount(fee))
+                .remainingAmount(computeRemainingAmountAmount(fee))
+                .comment(fee.getComment())
+                .creationDatetime(fee.getCreationDatetime())
+                .updatedAt(fee.getUpdatedAt())
+                .dueDatetime(fee.getDueDatetime());
       }
     }
     return new Fee()
