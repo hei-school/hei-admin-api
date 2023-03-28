26071173ce5bfcd5352e9520a5f4e76010d7369d
Lakara2 <lbryanlakara@gmail.com>
2023-03-28 14:13:25 +0300

    add: get all penality config

--- src/main/java/school/hei/haapi/service/PenaltyService.java
+++ src/main/java/school/hei/haapi/service/PenaltyService.java
@@ -2,11 +2,18 @@ package school.hei.haapi.service;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import school.hei.haapi.model.Penalty;
 import school.hei.haapi.repository.PenaltyRepository;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class PenaltyService {
     private final PenaltyRepository penaltyRepository;
+
+    public List<Penalty> getAll() {
+        return penaltyRepository.findAll();
+    }
 
 }
