76271ecf0c72cb4d22dc845d5976df050ad0f962
Lova Fenohasina Lala <fenohasinalala@gmail.com>
2023-03-28 10:20:26 +0300

    feat: insert data for interest_history

---
+++ src/main/resources/db/testdata/V99_6__Test_create_interest_history.sql
@@ -0,0 +1,8 @@
+insert into "interest_history"
+    (id, fee_id, interest_rate, interest_time_rate,interest_start, interest_end)
+values
+    ('interest_history1_id','fee3_id', 2, 1, '2021-12-14', '2021-12-19'),
+    ('interest_history2_id','fee3_id', 4, 1, '2021-12-20', '2021-12-30'),
+    ('interest_history3_id','fee4_id', 2, 1, '2021-12-14', '2021-12-19'),
+    ('interest_history4_id','fee4_id', 2, 3, '2021-12-20', '2021-12-25'),
+    ('interest_history5_id','fee5_id', 2, 1, '2021-12-12', '2021-12-22');
