194d25115b3f1404c5c35b85279e90b14a1ab5af
herizoran <herizo.ran@gmail.com>
2023-03-29 13:46:51 +0300

    chore: add DelayPenalty IT

---
+++ src/test/java/school/hei/haapi/integration/DelayPenaltyIT.java
@@ -0,0 +1,81 @@
+package school.hei.haapi.integration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static school.hei.haapi.integration.conf.TestUtils.MANAGER1_TOKEN;
+import static school.hei.haapi.integration.conf.TestUtils.STUDENT1_TOKEN;
+import static school.hei.haapi.integration.conf.TestUtils.anAvailableRandomPort;
+import static school.hei.haapi.integration.conf.TestUtils.setUpCognito;
+import java.time.Instant;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import school.hei.haapi.SentryConf;
+import school.hei.haapi.endpoint.rest.api.PayingApi;
+import school.hei.haapi.endpoint.rest.client.ApiClient;
+import school.hei.haapi.endpoint.rest.client.ApiException;
+import school.hei.haapi.endpoint.rest.model.DelayPenalty;
+import school.hei.haapi.endpoint.rest.security.cognito.CognitoComponent;
+import school.hei.haapi.integration.conf.AbstractContextInitializer;
+import school.hei.haapi.integration.conf.TestUtils;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@Testcontainers
+@ContextConfiguration(initializers = DelayPenaltyIT.ContextInitializer.class)
+@AutoConfigureMockMvc
+public class DelayPenaltyIT {
+  @MockBean
+  private SentryConf sentryConf;
+  @MockBean
+  private CognitoComponent cognitoComponentMock;
+
+  private static ApiClient anApiClient(String token) {
+    return TestUtils.anApiClient(token, DelayPenaltyIT.ContextInitializer.SERVER_PORT);
+  }
+
+  public static DelayPenalty delayPenalty1() {
+    DelayPenalty delayPenalty = new DelayPenalty();
+    delayPenalty.setId("delay_penalty1_id");
+    delayPenalty.setInterestPercent(2);
+    delayPenalty.setCreationDatetime(Instant.parse("2023-03-08T08:30:24Z"));
+    delayPenalty.setInterestTimerate(DelayPenalty.InterestTimerateEnum.DAILY);
+    delayPenalty.setGraceDelay(5);
+    delayPenalty.setApplicabilityDelayAfterGrace(10);
+    return delayPenalty;
+  }
+
+  @BeforeEach
+  void setUp() {
+    setUpCognito(cognitoComponentMock);
+  }
+
+  @Test
+  void student_read_ok() throws ApiException {
+    ApiClient student1Client = anApiClient(STUDENT1_TOKEN);
+    PayingApi api = new PayingApi(student1Client);
+    DelayPenalty actualDelayPenalty = api.getDelayPenalty();
+    assertEquals(delayPenalty1(), actualDelayPenalty);
+  }
+
+  @Test
+  void manager_read_ok() throws ApiException {
+    ApiClient manager1Client = anApiClient(MANAGER1_TOKEN);
+    PayingApi api = new PayingApi(manager1Client);
+    DelayPenalty actualDelayPenalty = api.getDelayPenalty();
+    assertEquals(delayPenalty1(), actualDelayPenalty);
+  }
+
+
+  static class ContextInitializer extends AbstractContextInitializer {
+    public static final int SERVER_PORT = anAvailableRandomPort();
+
+    @Override
+    public int getServerPort() {
+      return SERVER_PORT;
+    }
+  }
+}
