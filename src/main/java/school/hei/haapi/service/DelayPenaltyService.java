90fcd3cde837d3b9d371a805700f86a7a7bc368a
Lakara2 <lbryanlakara@gmail.com>
2023-03-30 18:17:53 +0300

    feat: fix error by modif

--- src/main/java/school/hei/haapi/service/DelayPenaltyService.java
+++ src/main/java/school/hei/haapi/service/DelayPenaltyService.java
@@ -3,13 +3,13 @@ package school.hei.haapi.service;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import school.hei.haapi.model.DelayPenalty;
-import school.hei.haapi.repository.PenaltyRepository;
+import school.hei.haapi.repository.DelayPenaltyRepository;
 
 import java.util.List;
 @Service
 @AllArgsConstructor
 public class DelayPenaltyService {
-    private final PenaltyRepository penaltyRepository;
+    private final DelayPenaltyRepository penaltyRepository;
 
     public List<DelayPenalty> getAll(){
         return penaltyRepository.findAll();
