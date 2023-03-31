34c3c249ef771dac29d92c2b2bdc2b92e4385f7d
njaina <hei.njaina.2@gmail.com>
2023-03-31 11:00:25 +0300

    feat: update validator structure

--- src/main/java/school/hei/haapi/model/validator/DelayPenaltyValidator.java
+++ src/main/java/school/hei/haapi/model/validator/DelayPenaltyValidator.java
@@ -2,15 +2,33 @@ package school.hei.haapi.model.validator;
 
 import org.springframework.stereotype.Component;
 import school.hei.haapi.model.DelayPenalty;
+import school.hei.haapi.model.exception.BadRequestException;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 @Component
 public class DelayPenaltyValidator implements Consumer<DelayPenalty> {
     @Override
     public void accept(DelayPenalty delayPenalty) {
-
+    Set<String> violationMessages = new HashSet<>();
+    if (delayPenalty.getInterestPercent() < 0 || delayPenalty.getInterestPercent() > 100) {
+        violationMessages.add("Interest percent must be between 0 and 100");
     }
-
-
+    if (delayPenalty.getGraceDelay() < 0) {
+        violationMessages.add("Grace delay must be positive");
+    }
+    if (delayPenalty.getApplicabilityDelayAfterGrace() < 0) {
+        violationMessages.add("Applicability delay after grace must be positive");
+    }
+    if (!violationMessages.isEmpty()) {
+        String formattedViolationMessages = violationMessages.stream()
+                .map(String::toString)
+                .collect(Collectors.joining(". "));
+        throw new BadRequestException(formattedViolationMessages);
+        }
+    }
 }
--- src/main/java/school/hei/haapi/service/DelayPenaltyService.java
+++ src/main/java/school/hei/haapi/service/DelayPenaltyService.java
@@ -1,16 +1,16 @@
 package school.hei.haapi.service;
 
 import lombok.AllArgsConstructor;
-import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 import school.hei.haapi.model.DelayPenalty;
+import school.hei.haapi.model.validator.DelayPenaltyValidator;
 import school.hei.haapi.repository.DelayPenaltyRepository;
 
 @Service
 @AllArgsConstructor
 public class DelayPenaltyService {
     private final DelayPenaltyRepository repository;
-
+    private final DelayPenaltyValidator validator;
     public DelayPenalty getDelayPenalty() {
         return repository.findFirstBy();
     }
@@ -20,6 +20,7 @@ public class DelayPenaltyService {
             currentValue.setInterestTimerate(newValues.getInterestTimerate());
             currentValue.setGraceDelay(newValues.getGraceDelay());
             currentValue.setApplicabilityDelayAfterGrace(newValues.getApplicabilityDelayAfterGrace());
+            validator.accept(currentValue);
             return repository.save(currentValue);
     }
 }
