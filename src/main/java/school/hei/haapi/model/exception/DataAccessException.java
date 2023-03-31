9fa9cc8490245b9a78a95861e945750e00fce9b3
njaina <hei.njaina.2@gmail.com>
2023-03-31 11:14:55 +0300

    feat: update

--- src/main/java/school/hei/haapi/model/exception/DataAccessException.java
+++
@@ -1,7 +0,0 @@
-package school.hei.haapi.model.exception;
-
-public class DataAccessException extends ApiException {
-    public DataAccessException( String message) {
-        super(ExceptionType.CLIENT_EXCEPTION,message);
-    }
-}
