22759d2a4d7627a0ca4b69007546903a72c37260
Razafimamonjisoa Mendrika Harizo <hei.mendrikaharizo@gmail.com>
2023-03-31 13:37:34 +0300

    chore: add delay_penalty to db/migration

---
+++ src/main/resources/db/migration/V0_10__Create_delay_penalty_table.sql
@@ -0,0 +1,21 @@
+do
+$$
+    begin
+        if not exists(select from pg_type where typname = 'delay_penalty_type') then
+            create type "delay_penalty_type" as enum ('DAILY');
+        end if;
+    end
+$$;
+
+create table if not exists "delay_penalty"
+(
+    id                varchar
+        constraint delay_penalty_pk primary key default uuid_generate_v4(),
+    interest_percent  integer                  not null,
+    interest_timerate delay_penalty_type       not null,
+    grace_delay       integer                  not null,
+    applicability_delay_after_grace           integer                  not null,
+    creation_datetime timestamp with time zone not null default now(),
+    due_datetime      timestamp with time zone not null
+);
+
