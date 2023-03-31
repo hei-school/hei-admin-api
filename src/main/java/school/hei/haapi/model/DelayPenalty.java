d11068b556d6f645867b3cc95dcefb1bb5b8e03a
NiceGuyMe <loica.etudiant@gmail.com>
2023-03-31 19:21:26 +0300

    chore: add constraint

--- src/main/java/school/hei/haapi/model/DelayPenalty.java
+++ src/main/java/school/hei/haapi/model/DelayPenalty.java
@@ -2,34 +2,53 @@ package school.hei.haapi.model;
 
 import lombok.*;
 import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import school.hei.haapi.repository.types.PostgresEnumType;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
 import java.time.Instant;
 
 import static javax.persistence.GenerationType.IDENTITY;
+
 @Entity
 @Table(name = "\"delay_penalty\"")
+@TypeDef(name = "pgsql_enum", typeClass = PostgresEnumType.class)
 @Getter
 @Setter
 @ToString
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode
 public class DelayPenalty implements Serializable {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private String id;
 
+    @NotBlank
+    @Column(name = "interest_percent")
     private Integer interestPercent;
 
-    @Type(type="pgsql_enum")
+    @Column(name = "interest_timerate")
+    @Type(type = "pgsql_enum")
     @Enumerated(EnumType.STRING)
-    private school.hei.haapi.endpoint.rest.model.DelayPenalty.InterestTimerateEnum interestTimeRate;
+    private InterestTimerateEnum InterestTimeRate;
 
+    @NotBlank
+    @Column(name = "grace_delay")
     private Integer graceDelay;
 
+    @NotBlank
+    @Column(name = "applicability_delay_after_grace")
     private Integer applicabilityDelayAfterGrace;
 
+    @NotBlank
+    @Column(name = "creation_datetime")
     private Instant creationDateTime;
+
+    public enum InterestTimerateEnum {
+        DAILY
+    }
 }
