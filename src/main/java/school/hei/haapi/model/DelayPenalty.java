b7d56c87111b92850e886a973f484d561af91f20
Tsirimaholy <hei.tsirimaholy@gmail.com>
2023-03-31 20:52:00 +0300

    feat: delay penalty model

--- src/main/java/school/hei/haapi/model/DelayPenalty.java
+++ src/main/java/school/hei/haapi/model/DelayPenalty.java
@@ -1,14 +1,25 @@
 package school.hei.haapi.model;
 
-import lombok.*;
-import org.hibernate.annotations.CreationTimestamp;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 import org.hibernate.annotations.Type;
 import org.hibernate.annotations.TypeDef;
 import school.hei.haapi.repository.types.PostgresEnumType;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.io.Serializable;
 import java.time.Instant;
+
+import static javax.persistence.GenerationType.IDENTITY;
 
 @Entity
 @Table(name = "\"delay_penalty\"")
@@ -20,16 +31,18 @@ import java.time.Instant;
 @AllArgsConstructor
 @NoArgsConstructor
 public class DelayPenalty implements Serializable {
-  private String id;
-  private int interestPercent;
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private String id;
+    private int interestPercent;
 
-  @Type(type = "pgsql_enum")
-  @Enumerated(EnumType.STRING)
-  private school.hei.haapi.endpoint.rest.model.DelayPenalty.InterestTimerateEnum interestTimerate;
+    @Type(type = "pgsql_enum")
+    @Enumerated(EnumType.STRING)
+    private school.hei.haapi.endpoint.rest.model.DelayPenalty.InterestTimerateEnum interestTimerate;
 
-  private int graceDelay;
+    private int graceDelay;
 
-  private int applicabilityDelayAfterGrace;
+    private int applicabilityDelayAfterGrace;
 
-  private Instant creationDatetime;
-  }
+    private Instant creationDatetime;
+}
