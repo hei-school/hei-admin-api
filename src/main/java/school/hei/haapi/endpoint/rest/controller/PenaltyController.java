e04d0a520361f41460dad30dd35204b37069a075
lianafinaritra <hei.lianafinaritra@gmail.com>
2023-03-31 09:27:09 +0300

    feat: add test for update delay penalty

--- src/main/java/school/hei/haapi/endpoint/rest/controller/PenaltyController.java
+++ src/main/java/school/hei/haapi/endpoint/rest/controller/PenaltyController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import school.hei.haapi.endpoint.rest.model.CreateDelayPenaltyChange;
 import school.hei.haapi.model.Penalty;
 import school.hei.haapi.service.PenaltyService;
 
@@ -21,6 +22,6 @@ public class PenaltyController {
     }
 
     @PutMapping("delay_penalty_change")
-    public Penalty createOrUpdatePenalty(@RequestBody Penalty penalty)
-    { return penaltyService.save(penalty);}
+    public Penalty createOrUpdatePenalty(@RequestBody CreateDelayPenaltyChange restPenalty)
+    { return penaltyService.update(restPenalty);}
 }
--- src/main/java/school/hei/haapi/service/PenaltyService.java
+++ src/main/java/school/hei/haapi/service/PenaltyService.java
@@ -2,10 +2,14 @@ package school.hei.haapi.service;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import school.hei.haapi.endpoint.rest.model.CreateDelayPenaltyChange;
+import school.hei.haapi.model.Group;
 import school.hei.haapi.model.Penalty;
 import school.hei.haapi.repository.PenaltyRepository;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -16,5 +20,20 @@ public class PenaltyService {
         return penaltyRepository.findAll();
     }
 
-    public Penalty save(Penalty penalty) { return penaltyRepository.save(penalty); }
+    public Penalty update(CreateDelayPenaltyChange restPenalty) {
+        Optional<Penalty> optionalPenalty = penaltyRepository.findAll().stream().findFirst();
+        if (optionalPenalty.isPresent()) {
+            Penalty firstPenalty = optionalPenalty.get();
+            return penaltyRepository.save(Penalty.builder()
+                    .id(firstPenalty.getId())
+                            .interestPercent(restPenalty.getInterestPercent())
+                            .interestTimeRate(Penalty.TimeRate.valueOf(restPenalty.getInterestTimerate().getValue()))
+                            .graceDelay(restPenalty.getGraceDelay())
+                            .applicabilityDelayAfterGrace(restPenalty.getApplicabilityDelayAfterGrace())
+                            .creationDateTime(firstPenalty.getCreationDateTime())
+                    .build());
+        } else {
+            throw new NoSuchElementException("Aucune pénalité trouvée à modifier");
+        }
+    }
 }
