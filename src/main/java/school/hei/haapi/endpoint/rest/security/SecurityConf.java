7d235b666d4c8792c12271928ab32db662841247
Anjaraniaina RATSIMBAZAFY <98638459+Anjaraniaina@users.noreply.github.com>
2023-03-31 12:21:14 +0300

    feat(delay-penalty): add database migration (#2)
    * feat(delay-penalty): add database migration

--- src/main/java/school/hei/haapi/endpoint/rest/security/SecurityConf.java
+++ src/main/java/school/hei/haapi/endpoint/rest/security/SecurityConf.java
@@ -100,6 +100,8 @@ public class SecurityConf extends WebSecurityConfigurerAdapter {
         .antMatchers(GET, "/groups").authenticated()
         .antMatchers(GET, "/groups/*").authenticated()
         .antMatchers(PUT, "/groups/**").hasAnyRole(MANAGER.getRole())
+            .antMatchers(GET, "/delay_penalty").authenticated()
+            .antMatchers(PUT, "/delay_penalty_change").hasAnyRole(MANAGER.getRole())
         .antMatchers("/**").denyAll()
 
         // disable superfluous protections
