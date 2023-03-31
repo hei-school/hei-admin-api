c6545cc7ff238831aa5bdb2b5465b9675028981e
Jean Koletto Reichmann GALY <hei.reichmann.4@gmail.com>
2023-03-31 17:04:07 +0300

    feat: create and add data to delay_penalty table

---
+++ src/main/resources/db/testdata/V99_5__Test_create_delay_penalty.sql
@@ -0,0 +1,4 @@
+insert into "delay_penalty"
+    (id, interest_percent, interest_timerate, grace_delay, applicability_delay_after_grace, creation_datetime)
+values
+    ('delay_penalty_id', 2, 'DAILY', 5, 10, '2023-03-30T08:25:25.00Z');
