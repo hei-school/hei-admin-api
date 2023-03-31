3016b7ac7bc536cb3ba5bf591e964b955261b8cd
Razafimamonjisoa Mendrika Harizo <hei.mendrikaharizo@gmail.com>
2023-03-31 14:10:53 +0300

    refactor: remove an unwanted column to Create_delay_penalty_table

--- src/main/resources/db/migration/V0_10__Create_delay_penalty_table.sql
+++ src/main/resources/db/migration/V0_10__Create_delay_penalty_table.sql
@@ -15,7 +15,6 @@ create table if not exists "delay_penalty"
     interest_timerate delay_penalty_type       not null,
     grace_delay       integer                  not null,
     applicability_delay_after_grace           integer                  not null,
-    creation_datetime timestamp with time zone not null default now(),
-    due_datetime      timestamp with time zone not null
+    creation_datetime timestamp with time zone not null default now()
 );
 
