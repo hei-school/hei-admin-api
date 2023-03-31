649e7d3f69803a418bbf279e375d690ff8d6a947
Razafimamonjisoa Mendrika Harizo <hei.mendrikaharizo@gmail.com>
2023-03-31 21:24:21 +0300

    feat: add service to DelayPenalty

---
+++ src/main/java/school/hei/haapi/service/DelayPenaltyService.java
@@ -0,0 +1,20 @@
+package school.hei.haapi.service;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import school.hei.haapi.model.DelayPenalty;
+import school.hei.haapi.repository.DelayPenaltyRepository;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class DelayPenaltyService {
+    private DelayPenaltyRepository repository;
+
+    public List<DelayPenalty> getAll(){
+        return repository.findAll();
+    }
+}
