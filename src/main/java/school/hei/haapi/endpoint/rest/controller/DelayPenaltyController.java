112808c8b5b9eabbf6b96eba42626ad68dc4feb4
Jean Koletto Reichmann GALY <hei.reichmann.4@gmail.com>
2023-03-31 16:56:15 +0300

    feat: add get /delay_penalty endpoint

---
+++ src/main/java/school/hei/haapi/endpoint/rest/controller/DelayPenaltyController.java
@@ -0,0 +1,20 @@
+package school.hei.haapi.endpoint.rest.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import school.hei.haapi.endpoint.rest.mapper.DelayPenaltyMapper;
+import school.hei.haapi.endpoint.rest.model.DelayPenalty;
+import school.hei.haapi.service.DelayPenaltyService;
+
+@RestController
+@AllArgsConstructor
+public class DelayPenaltyController {
+    private DelayPenaltyService service;
+    private DelayPenaltyMapper mapper;
+
+    @GetMapping("/delay_penalty")
+    public DelayPenalty getDelayPenalty() {
+        return mapper.toRest(service.getDelayPenalty());
+    }
+}
