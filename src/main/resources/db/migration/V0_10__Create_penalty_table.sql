2f46357fb2885acf549b192b607aa43355a917fa
NiceGuyMe <loica.etudiant@gmail.com>
2023-03-31 20:31:36 +0300

    chore: update flyway

--- src/main/resources/db/migration/V0_10__Create_penalty_table.sql
+++ src/main/resources/db/migration/V0_10__Create_penalty_table.sql
@@ -1,9 +1,17 @@
--- Create table for Penalty instances
-CREATE TABLE IF NOT EXISTS penalty (
-                         id VARCHAR PRIMARY KEY,
-                         interest_percent INTEGER NOT NULL,
-                         interest_time_rate TEXT NOT NULL,
-                         grace_delay INTEGER NOT NULL,
-                         applicability_delay_after_grace INTEGER NOT NULL,
-                         creation_date_time TIMESTAMP WITHOUT TIME ZONE NOT NULL
+DO
+$$
+BEGIN
+        IF NOT EXISTS(SELECT FROM pg_type WHERE typname = 'time_rate_type') THEN
+CREATE TYPE "time_rate_type" AS ENUM ('DAILY');
+END IF;
+END
+$$;
+
+CREATE TABLE IF NOT EXISTS "delay_penalty" (
+                                               id VARCHAR PRIMARY KEY,
+                                               interest_percent INTEGER NOT NULL,
+                                               interest_timerate time_rate_type NOT NULL,
+                                               grace_delay INTEGER NOT NULL,
+                                               applicability_delay_after_grace INTEGER NOT NULL,
+                                               creation_datetime timestamp with time zone not null
 );
--- src/main/resources/db/testdata/V99_5__Test_create_penalty.sql
+++ src/main/resources/db/testdata/V99_5__Test_create_penalty.sql
@@ -1,5 +1,4 @@
-INSERT INTO "penalty"
-    (id, interest_percent, interest_time_rate, grace_delay, applicability_delay_after_grace, creation_date_time)
+INSERT INTO "delay_penalty"
+(id,interest_percent, interest_timerate, grace_delay, applicability_delay_after_grace, creation_datetime)
 VALUES
-    ('1', 2, 'DAILY', 7, 25, '2022-02-01 10:00:00');
-
+    ('delay_penalty1_id',2, 'DAILY', 7, 30, '2022-11-08T08:25:24.00Z');
