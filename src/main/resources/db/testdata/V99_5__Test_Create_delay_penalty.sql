36558ae2fafddfd46d4d7b7149f8407ce9e5359c
laurenrasoanaivo <hei.lauren.3@gmail.com>
2023-04-01 11:28:08 +0300

    feat: add test data delay penalty

---
+++ src/main/resources/db/testdata/V99_5__Test_Create_delay_penalty.sql
@@ -0,0 +1,4 @@
+insert into "delay_penalty"
+    (id, creation_datetime, interest_percent, interest_time_rate, grace_delay, applicability_delay_after_grace)
+values
+    ('delay_penalty1_id', '2022-11-08T08:25:24.00Z', 0, 'DAILY', 0, 0);
