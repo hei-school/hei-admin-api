03c4817fa776bb093acdaddd1ccf3648951186de
Steven1805 <hei.steven.2@gmail.com>
2023-04-01 20:26:26 +0300

    feat: add delay penalty

--- src/main/java/school/hei/haapi/endpoint/rest/mapper/DelayPenaltyMapper.java
+++ src/main/java/school/hei/haapi/endpoint/rest/mapper/DelayPenaltyMapper.java
@@ -2,8 +2,30 @@ package school.hei.haapi.endpoint.rest.mapper;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
+import school.hei.haapi.model.DelayPenalty;
 
 @Component
 @AllArgsConstructor
 public class DelayPenaltyMapper {
+    public static school.hei.haapi.endpoint.rest.model.DelayPenalty toRest(DelayPenalty delayPenalty) {
+        var restDelayPenalty = new school.hei.haapi.endpoint.rest.model.DelayPenalty();
+        restDelayPenalty.setId(delayPenalty.getId());
+        restDelayPenalty.setCreationDatetime(delayPenalty.getCreationDatetime());
+        restDelayPenalty.setInterestPercent(delayPenalty.getInterestPercent());
+        restDelayPenalty.setInterestTimerate(delayPenalty.getInterestTimeRate());
+        restDelayPenalty.setGraceDelay(delayPenalty.getGraceDelay());
+        restDelayPenalty.setApplicabilityDelayAfterGrace(delayPenalty.getApplicabilityDelayAfterGrace());
+        return restDelayPenalty;
+    }
+
+    public DelayPenalty toDomain(school.hei.haapi.endpoint.rest.model.DelayPenalty restDelayPenalty) {
+        return DelayPenalty.builder()
+                .id(restDelayPenalty.getId())
+                .creationDatetime(restDelayPenalty.getCreationDatetime())
+                .interestPercent(restDelayPenalty.getInterestPercent())
+                .interestTimeRate(restDelayPenalty.getInterestTimerate())
+                .graceDelay(restDelayPenalty.getGraceDelay())
+                .applicabilityDelayAfterGrace(restDelayPenalty.getApplicabilityDelayAfterGrace())
+                .build();
+    }
 }
--- src/main/java/school/hei/haapi/service/DelayPenaltyService.java
+++ src/main/java/school/hei/haapi/service/DelayPenaltyService.java
@@ -2,8 +2,16 @@ package school.hei.haapi.service;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import school.hei.haapi.endpoint.rest.controller.DelayPenalty;
+import school.hei.haapi.model.Fee;
+import school.hei.haapi.repository.DelayPenaltyRepository;
 
 @Service
 @AllArgsConstructor
 public class DelayPenaltyService {
+    public DelayPenaltyRepository delayPenaltyRepository;
+
+    public DelayPenalty getById(String id) {
+        return (delayPenaltyRepository.getById(id));
+    }
 }
