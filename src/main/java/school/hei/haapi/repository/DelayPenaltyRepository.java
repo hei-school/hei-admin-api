f73241a113357ad3723b4dd1b6052360540b706b
HARIVOLA Andy <hei.andy.8@gmail.com>
2023-04-01 12:35:16 +0300

    chore: update delay_penalty_change

--- src/main/java/school/hei/haapi/repository/DelayPenaltyRepository.java
+++ src/main/java/school/hei/haapi/repository/DelayPenaltyRepository.java
@@ -1,10 +1,9 @@
 package school.hei.haapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import school.hei.haapi.endpoint.rest.model.DelayPenalty;
+import org.springframework.stereotype.Repository;
+import school.hei.haapi.model.DelayPenalty;
 
-import java.util.Optional;
-
-public interface DelayPenaltyRepository extends JpaRepository<DelayPenalty, Long> {
-    Optional<DelayPenalty> findFirstByOrderByCreationDatetimeDesc();
+@Repository
+public interface DelayPenaltyRepository extends JpaRepository<DelayPenalty, String> {
 }
