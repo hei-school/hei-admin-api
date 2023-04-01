6d4e3d75d5d97f5c4f4f4e28ed29cd021f441c8c
Anjaraniaina <hei.anjaraniaina@gmail.com>
2023-04-01 11:17:54 +0300

    feat(delay-penalty): add put handler

--- src/main/java/school/hei/haapi/endpoint/rest/controller/DelayPenaltyController.java
+++ src/main/java/school/hei/haapi/endpoint/rest/controller/DelayPenaltyController.java
@@ -2,7 +2,10 @@ package school.hei.haapi.endpoint.rest.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import school.hei.haapi.endpoint.rest.model.CreateDelayPenaltyChange;
 import school.hei.haapi.model.DelayPenalty;
 import school.hei.haapi.service.DelayPenaltyService;
 
@@ -13,7 +16,12 @@ public class DelayPenaltyController {
     private final DelayPenaltyService delayPenaltyService;
 
     @GetMapping("/delay_penalty")
-    public DelayPenalty getCurrentDelayPenalty() {
-        return delayPenaltyService.getAllDelayPenalty();
+    public DelayPenalty getDelayPenalty() {
+        return delayPenaltyService.getDelayPenalty();
+    }
+
+    @PutMapping("/delay_penalty_change")
+    public DelayPenalty createDelayPenaltyChange(@RequestBody CreateDelayPenaltyChange createDelayPenaltyChange) {
+        return delayPenaltyService.createDelayPenaltyChange(createDelayPenaltyChange);
     }
 }
--- src/main/java/school/hei/haapi/service/DelayPenaltyService.java
+++ src/main/java/school/hei/haapi/service/DelayPenaltyService.java
@@ -2,16 +2,33 @@ package school.hei.haapi.service;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import school.hei.haapi.endpoint.rest.model.CreateDelayPenaltyChange;
 import school.hei.haapi.model.DelayPenalty;
 import school.hei.haapi.repository.DelayPenaltyRepository;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 @AllArgsConstructor
 public class DelayPenaltyService {
     private final DelayPenaltyRepository repository;
-    public DelayPenalty getAllDelayPenalty() {
+    public DelayPenalty getDelayPenalty() {
         return repository.findAll().get(0);
+    }
+
+    public DelayPenalty createDelayPenaltyChange(CreateDelayPenaltyChange createDelayPenaltyChange) {
+        DelayPenalty current = getDelayPenalty();
+        DelayPenalty updated = DelayPenalty.builder()
+                .id(current.getId())
+                .interestPercent(createDelayPenaltyChange.getInterestPercent())
+                .interestTimerate(
+                        Objects.equals(createDelayPenaltyChange.getInterestTimerate(), current.getInterestTimerate()) ?
+                                current.getInterestTimerate() : null
+                        )
+                .graceDelay(createDelayPenaltyChange.getGraceDelay())
+                .applicabilityDelayAfterGrace(createDelayPenaltyChange.getApplicabilityDelayAfterGrace())
+                .build();
+        return repository.saveAll(List.of(updated)).get(0);
     }
 }
