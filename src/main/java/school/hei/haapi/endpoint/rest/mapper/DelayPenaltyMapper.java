5c9768fa70e0cf673a33f69d1aca3514559c7f22
RyanIaro <hei.ryan.3@gmail.com>
2023-04-01 16:15:25 +0300

    feat: added toDomain method to DelayPenaltyMapper

--- src/main/java/school/hei/haapi/endpoint/rest/mapper/DelayPenaltyMapper.java
+++ src/main/java/school/hei/haapi/endpoint/rest/mapper/DelayPenaltyMapper.java
@@ -6,14 +6,25 @@ import school.hei.haapi.endpoint.rest.model.DelayPenalty;
 @Component
 public class DelayPenaltyMapper {
 
-    public DelayPenalty toRest(school.hei.haapi.model.DelayPenalty restDelayPenality) {
+    public DelayPenalty toRest(school.hei.haapi.model.DelayPenalty delayPenalty) {
         var delay = new school.hei.haapi.endpoint.rest.model.DelayPenalty();
-        delay.setId(restDelayPenality.getId());
-        delay.setInterestPercent(restDelayPenality.getInterest_percent());
-        delay.setInterestTimerate(restDelayPenality.getInterest_timerate());
-        delay.setGraceDelay(restDelayPenality.getGrace_delay());
-        delay.setApplicabilityDelayAfterGrace(restDelayPenality.getApplicability_delay_after_grace());
-        delay.setCreationDatetime(restDelayPenality.getCreation_datetime());
+        delay.setId(delayPenalty.getId());
+        delay.setInterestPercent(delayPenalty.getInterestPercent());
+        delay.setInterestTimerate(delayPenalty.getInterestTimerate());
+        delay.setGraceDelay(delayPenalty.getGraceDelay());
+        delay.setApplicabilityDelayAfterGrace(delayPenalty.getApplicabilityDelayAfterGrace());
+        delay.setCreationDatetime(delayPenalty.getCreationDatetime());
         return delay;
+    }
+
+    public school.hei.haapi.model.DelayPenalty toDomain(DelayPenalty restDelayPenalty) {
+        return school.hei.haapi.model.DelayPenalty.builder()
+                .id(restDelayPenalty.getId())
+                .interestPercent(restDelayPenalty.getInterestPercent())
+                .interestTimerate(restDelayPenalty.getInterestTimerate())
+                .graceDelay(restDelayPenalty.getGraceDelay())
+                .applicabilityDelayAfterGrace(restDelayPenalty.getApplicabilityDelayAfterGrace())
+                .creationDatetime(restDelayPenalty.getCreationDatetime())
+                .build();
     }
 }
