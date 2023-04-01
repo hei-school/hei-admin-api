9fb6708dfab9a645a98cb89cfdb619042bce8496
Micael Jerry <hei.jerry.2@gmail.com>
2023-04-01 11:20:44 +0300

    chore: add fee to test interest

---
+++ src/main/resources/db/testdata/V99_6__Test_create_fees.sql
@@ -0,0 +1,4 @@
+insert into "fee"
+(id, user_id, type, comment,total_amount,  remaining_amount, status,creation_datetime, updated_at, due_datetime)
+values
+    ('fee7_id', 'student3_id', 'TUITION','Comment', 200000, 200000, 'LATE','2022-12-08T08:25:24.00Z','2022-12-08T08:25:24.00Z','2023-01-15T23:59:24.00Z')
