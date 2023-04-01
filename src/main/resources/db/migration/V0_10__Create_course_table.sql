536e49a8f3cdc72a44e3d70cae4c5ee49426a8ff
AnatoleRajoelison <hei.anatole@gmail.com>
2023-04-02 00:31:29 +0300

    modification create course tables.sql

--- src/main/resources/db/migration/V0_10__Create_course_table.sql
+++ src/main/resources/db/migration/V0_10__Create_course_table.sql
@@ -9,3 +9,19 @@ create table if not exists "course"
     main_teacher_id   varchar
         constraint main_teacher_id_fk references "user"(id)
 );
+do
+&&
+begin if not exists (select from pg_type where typname = 'delay_penalty_type') then
+    create type "delay_penalty_type" as enum ('DAILY');
+     end if;
+   end
+&&;
+create table if not exists 'delay_penalty'
+(id varchar
+    constraint delay_penalty_pk primary key default uuid_generate_v4(),
+    interest_percent integer,
+    interest_timerate   delay_penalty_type,
+    grace_delay integer,
+    applicability_delay_after_grace integer,
+    creation_datetime   timestamp with time zone not null default now()
+);
