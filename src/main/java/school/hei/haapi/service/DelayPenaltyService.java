888155da2f98bcb3d07c81bf5c10aa14a5f7acb2
AnatoleRajoelison <hei.anatole@gmail.com>
2023-04-02 00:14:21 +0300

    envoie de DelayPenaltyService

--- src/main/java/school/hei/haapi/service/DelayPenaltyService.java
+++ src/main/java/school/hei/haapi/service/DelayPenaltyService.java
@@ -1,2 +1,30 @@
-package school.hei.haapi.service;public class DelayPenaltyService {
+package school.hei.haapi.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import school.hei.haapi.model.DelayPenalty;
+import school.hei.haapi.repository.DelayPenaltyRepository;
+import software.amazon.awssdk.services.eventbridge.model.DeactivateEventSourceRequest;
+
+@Service
+@AllArgsConstructor
+public class DelayPenaltyService {
+    private final DelayPenaltyRepository repository;
+
+    public DelayPenalty getDelayPenalty(){
+        return repository.findAll().get(0);
+    }
+//Put DelayPenalty
+    public DelayPenalty getDelayPenalty(String id){
+        return repository.findById(id)
+                //si il trouve l'objet
+                .map(d->{
+                    d.setGraceDelay(d.getGraceDelay());
+                    d.setCreationDatetime(d.getCreationDatetime());
+                    d.setInterestPercent(d.getInterestPercent());
+                    d.setInterestTimeRate((d.getInterestTimeRate()));
+                    d.setApplicabilityDelayAfterGrace(d.getApplicabilityDelayAfterGrace());
+                    return repository.save(d);
+                }).orElseThrow(()->new RuntimeException("l'id du delay n'a pas pu etre trouvé"));
+    }
 }
