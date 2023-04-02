2b2294bfe59708bf31f09dd49eafee794521bf6c
Mihary Joel <miharyjoel@gmail.com>
2023-04-02 21:42:24 +0300

    feat: add resource db

---
+++ src/main/resources/db/testdata/V99_5__Test_create_delay_penalty.sql
@@ -0,0 +1,3 @@
+insert into "delay_penalty"
+(id, interest_percent, interest_timerate, grace_delay, applicability_delay_after_grace, creation_datetime)
+values ('penality1_id', 2, 'DAILY', 10, 5, '2023-01-15T08:25:25.00Z');
