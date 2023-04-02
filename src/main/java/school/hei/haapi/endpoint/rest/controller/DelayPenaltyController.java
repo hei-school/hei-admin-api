714290b8eab5daeaa9198539bae9ff84881ca7ec
Miaritiana <hei.miaritiana@gmail.com>
2023-04-02 18:39:47 +0300

    feat: add /delay_penalty_change endpoint

--- src/main/java/school/hei/haapi/endpoint/rest/controller/DelayPenaltyController.java
+++ src/main/java/school/hei/haapi/endpoint/rest/controller/DelayPenaltyController.java
@@ -3,21 +3,27 @@ package school.hei.haapi.endpoint.rest.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import school.hei.haapi.model.DelayPenalty;
+import school.hei.haapi.model.DelayPenaltyChange;
 import school.hei.haapi.service.DelayPenaltyService;
 
 @RestController
-@RequestMapping("/delay_penalty")
 public class DelayPenaltyController {
     @Autowired
     private DelayPenaltyService delayPenaltyService;
 
-    @GetMapping("/{delayPenaltyId}")
+    @GetMapping("/delay_penalty/{delayPenaltyId}")
     public ResponseEntity<DelayPenalty> getDelayPenalty(@PathVariable String delayPenaltyId) {
         DelayPenalty delayPenalty = delayPenaltyService.getDelayPenalty(delayPenaltyId);
         return ResponseEntity.ok(delayPenalty);
+    }
+
+    @PutMapping("/delay_penalty_change")
+    public DelayPenalty changeDelayPenaltyConfiguration(@RequestBody DelayPenaltyChange delayPenaltyChange) {
+        return delayPenaltyService.changeDelayPenaltyConfiguration(delayPenaltyChange);
     }
 }
