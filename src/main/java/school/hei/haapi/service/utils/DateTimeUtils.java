5a8e9d3abea10cbb107883c7ccd186984b92a96d
Tsirimaholy <hei.tsirimaholy@gmail.com>
2023-04-02 22:19:16 +0300

    fix: instant to date conversion

--- src/main/java/school/hei/haapi/service/utils/DateTimeUtils.java
+++ src/main/java/school/hei/haapi/service/utils/DateTimeUtils.java
@@ -1,17 +1,29 @@
 package school.hei.haapi.service.utils;
 
 import java.time.Instant;
-import java.time.temporal.ChronoField;
+import java.util.Arrays;
 import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class DateTimeUtils {
     public DateTimeUtils() {
     }
 
     public static GregorianCalendar instantToGregorianCalendar(Instant instant) {
-        int day = instant.get(ChronoField.DAY_OF_MONTH);
-        int month = instant.get(ChronoField.MONTH_OF_YEAR);
-        int year = instant.get(ChronoField.YEAR);
+        String[] ts = instant.toString().split("T");
+        List<String> strings = Arrays.stream(ts).collect(Collectors.toList());
+        strings.remove(1);
+
+        List<Integer> yearMonthDay = Arrays.stream(strings.get(0).split("-"))
+                .collect(Collectors.toList())
+                .stream()
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        int day = yearMonthDay.get(2);
+        int month = yearMonthDay.get(1);
+        int year = yearMonthDay.get(0);
         return new GregorianCalendar(year, month, day);
     }
 }
