294f5de028439c58f9c9b55296e410681fe32208
Jean Koletto Reichmann GALY <hei.reichmann.4@gmail.com>
2023-04-02 21:12:05 +0300

    fix: fix delay penalty mapper

--- src/main/java/school/hei/haapi/endpoint/rest/mapper/DelayPenaltyMapper.java
+++ src/main/java/school/hei/haapi/endpoint/rest/mapper/DelayPenaltyMapper.java
@@ -1,18 +1,19 @@
 package school.hei.haapi.endpoint.rest.mapper;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
-import school.hei.haapi.endpoint.rest.model.CreateDelayPenaltyChange;
 import school.hei.haapi.endpoint.rest.model.DelayPenalty;
-import school.hei.haapi.endpoint.rest.validator.CreateDelayPenaltyValidator;
+import school.hei.haapi.endpoint.rest.validator.UpdateDelayPenaltyValidator;
 import school.hei.haapi.model.exception.BadRequestException;
 import school.hei.haapi.service.DelayPenaltyService;
 
 import java.time.Instant;
 
 @Component
+@AllArgsConstructor
 public class DelayPenaltyMapper {
-    DelayPenaltyService service;
-    CreateDelayPenaltyValidator validator;
+    private final DelayPenaltyService service;
+    private final UpdateDelayPenaltyValidator validator;
     public DelayPenalty toRest(school.hei.haapi.model.DelayPenalty domain) {
         return new DelayPenalty()
                 .id(domain.getId())
@@ -22,21 +23,21 @@ public class DelayPenaltyMapper {
                 .applicabilityDelayAfterGrace(domain.getApplicabilityDelayAfterGrace())
                 .creationDatetime(domain.getCreationDatetime());
     }
-    public school.hei.haapi.model.DelayPenalty toDomain(CreateDelayPenaltyChange createDelayPenaltyChange) {
-        validator.accept(createDelayPenaltyChange);
+    public school.hei.haapi.model.DelayPenalty toDomain(DelayPenalty delayPenalty) {
+        validator.accept(delayPenalty);
         school.hei.haapi.model.DelayPenalty lastDelayPenalty = service.getLastUpdated();
         return school.hei.haapi.model.DelayPenalty.builder()
                 .id(lastDelayPenalty.getId())
-                .interestPercent(createDelayPenaltyChange.getInterestPercent())
+                .interestPercent(delayPenalty.getInterestPercent())
                 .creationDatetime(lastDelayPenalty.getCreationDatetime())
                 .lastUpdateDate(Instant.now())
-                .interestTimeRate(toDomainDelayInterest(createDelayPenaltyChange.getInterestTimerate()))
-                .graceDelay(createDelayPenaltyChange.getGraceDelay())
-                .applicabilityDelayAfterGrace(createDelayPenaltyChange.getApplicabilityDelayAfterGrace())
+                .interestTimeRate(toDomainDelayInterest(delayPenalty.getInterestTimerate()))
+                .graceDelay(delayPenalty.getGraceDelay())
+                .applicabilityDelayAfterGrace(delayPenalty.getApplicabilityDelayAfterGrace())
                 .build();
     }
 
-    private DelayPenalty.InterestTimerateEnum toDomainDelayInterest(CreateDelayPenaltyChange.InterestTimerateEnum interestTimerate) {
+    private DelayPenalty.InterestTimerateEnum toDomainDelayInterest(DelayPenalty.InterestTimerateEnum interestTimerate) {
         switch (interestTimerate) {
             case DAILY:
                 return school.hei.haapi.endpoint.rest.model.DelayPenalty.InterestTimerateEnum.DAILY;
