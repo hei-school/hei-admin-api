0a63be86bdc3537c052f71978f766d3b39343d42
Micael Jerry <hei.jerry.2@gmail.com>
2023-04-02 17:09:18 +0300

    chore: update interest fee update

--- src/main/java/school/hei/haapi/service/utils/InterestFeeUpdate.java
+++ src/main/java/school/hei/haapi/service/utils/InterestFeeUpdate.java
@@ -19,7 +19,7 @@ public class InterestFeeUpdate {
 
     public void updateInterestFees(DelayPenalty delayPenalty) {
         List<Fee> fees = new ArrayList<>();
-        feeRepository.findAll().forEach(fee -> {
+        feeRepository.getFeesByStatus(LATE).forEach(fee -> {
             if (this.updateFeeInterest(delayPenalty, fee) != null) {
                 fees.add(fee);
             }
@@ -30,25 +30,22 @@ public class InterestFeeUpdate {
     }
 
     public Fee updateFeeInterest(DelayPenalty delayPenalty, Fee fee) {
-        if (fee.getStatus().equals(LATE)) {
-            long NOW = Instant.now().getEpochSecond() / 86400;
-            long firstDayOfInterestApplication = (fee.getDueDatetime().getEpochSecond() / 86400) + delayPenalty.getGraceDelay();
-            long lastDayOfInterestApplication = firstDayOfInterestApplication + delayPenalty.getApplicabilityDelayAfterGrace();
-            if (NOW > firstDayOfInterestApplication && NOW <= lastDayOfInterestApplication) {
-                long numberOfDayToApplyInterest = NOW - firstDayOfInterestApplication;
-                fee.setInterest(this.calculateInterest(
-                        fee.getRemainingAmount(),
-                        delayPenalty.getInterestPercent(),
-                        numberOfDayToApplyInterest));
-            } else if (NOW > lastDayOfInterestApplication) {
-                fee.setInterest(this.calculateInterest(
-                        fee.getRemainingAmount(),
-                        delayPenalty.getInterestPercent(),
-                        delayPenalty.getApplicabilityDelayAfterGrace()));
-            }
-            return fee;
+        long NOW = Instant.now().getEpochSecond() / 86400;
+        long firstDayOfInterestApplication = (fee.getDueDatetime().getEpochSecond() / 86400) + delayPenalty.getGraceDelay();
+        long lastDayOfInterestApplication = firstDayOfInterestApplication + delayPenalty.getApplicabilityDelayAfterGrace();
+        if (NOW > firstDayOfInterestApplication && NOW <= lastDayOfInterestApplication) {
+            long numberOfDayToApplyInterest = NOW - firstDayOfInterestApplication;
+            fee.setInterest(this.calculateInterest(
+                    fee.getRemainingAmount(),
+                    delayPenalty.getInterestPercent(),
+                    numberOfDayToApplyInterest));
+        } else if (NOW > lastDayOfInterestApplication) {
+            fee.setInterest(this.calculateInterest(
+                    fee.getRemainingAmount(),
+                    delayPenalty.getInterestPercent(),
+                    delayPenalty.getApplicabilityDelayAfterGrace()));
         }
-        return null;
+        return fee;
     }
 
     private int calculateInterest(double baseAmount, int interestPercent, long numberOfDays) {
