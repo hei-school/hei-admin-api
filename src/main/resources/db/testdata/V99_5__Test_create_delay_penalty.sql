c2860bb48092b0190d2433324a3f46cd3e037eda
Onitsiky <hei.onitsiky@gmail.com>
2023-04-02 23:21:10 +0300

    chore: modify and add test datas

--- src/main/resources/db/testdata/V99_5__Test_create_delay_penalty.sql
+++ src/main/resources/db/testdata/V99_5__Test_create_delay_penalty.sql
@@ -1,4 +1,4 @@
 insert into "delay_penalty"
-    (id, interest_percent, interest_timerate, grace_delay,applicability_delay_after_grace, creation_datetime)
-values
-    ('delay_penalty1_id',0, 'DAILY', 0, 0, '2021-12-09T08:25:24.00Z');
+(id, interest_percent, interest_timerate, grace_delay, applicability_delay_after_grace, creation_datetime)
+values ('delay_penalty1_id', 0, 'DAILY', 0, 0, '2021-12-09T08:25:24.00Z'),
+       ('delay_penalty2_id', 2, 'DAILY', 3, 5, '2023-01-09T08:25:24.00Z');
