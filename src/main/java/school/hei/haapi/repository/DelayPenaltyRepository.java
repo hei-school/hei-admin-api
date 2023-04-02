773c86f5e8345858d0768e9d7370c182c64397b6
Onitsiky <hei.onitsiky@gmail.com>
2023-04-02 10:28:35 +0300

    chore: add order by for delayPenalty

--- src/main/java/school/hei/haapi/repository/DelayPenaltyRepository.java
+++ src/main/java/school/hei/haapi/repository/DelayPenaltyRepository.java
@@ -1,9 +1,14 @@
 package school.hei.haapi.repository;
 
+import java.util.List;
+import javax.persistence.OrderBy;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import school.hei.haapi.model.DelayPenalty;
 
 @Repository
 public interface DelayPenaltyRepository extends JpaRepository<DelayPenalty, String> {
+  @Override
+  @OrderBy(value = "creationDatetime DESC ")
+  List<DelayPenalty> findAll();
 }
--- src/main/resources/db/testdata/V99_5__Test_create_delay_penalty.sql
+++ src/main/resources/db/testdata/V99_5__Test_create_delay_penalty.sql
@@ -1,4 +1,5 @@
 insert into "delay_penalty"
-    (id, interest_percent, interest_timerate, grace_delay,applicability_delay_after_grace, creation_datetime)
-values
-    ('delay_penalty1_id',0, 'DAILY', 0, 0, '2021-12-09T08:25:24.00Z');
+(id, interest_percent, interest_timerate, grace_delay, applicability_delay_after_grace, creation_datetime)
+values ('delay_penalty1_id', 0, 'DAILY', 0, 0, '2021-12-09T08:25:24.00Z'),
+       ('delay_penalty2_id', 0, 'DAILY', 1, 0, '2020-12-09T08:25:24.00Z')
+;
