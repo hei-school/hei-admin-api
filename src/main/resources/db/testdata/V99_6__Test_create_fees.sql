7bb115658a2af6e4de0b9e0e5b5978aae0871f35
Micael Jerry <hei.jerry.2@gmail.com>
2023-04-02 18:14:25 +0300

    chore: update database, add test data to fee

---
+++ src/main/resources/db/testdata/V99_6__Test_create_fees.sql
@@ -0,0 +1,4 @@
+insert into "fee"
+(id, user_id, type, comment,total_amount,  remaining_amount, status,creation_datetime, updated_at, due_datetime)
+values
+    ('fee7_id', 'student3_id', 'TUITION','Comment', 200000, 200000, 'LATE','2021-11-08T08:25:24.00Z','2023-02-08T08:30:24.00Z','2023-03-22T08:25:25.00Z');
