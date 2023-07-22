b37cafaf7e21cd36d184b3d7e201b3983037c30a
Micael Jerry <hei.jerry.2@gmail.com>
2023-07-22 11:18:02 +0300

    chore: integration test read transcript

---
+++ src/main/resources/db/testdata/V99_7__Test_create_student_transcript.sql
@@ -0,0 +1,8 @@
+insert into "transcript"
+(id, user_id, semester, academic_year, is_definitive, creation_datetime)
+values     ('transcript1_id', 'student1_id', 'S1', 2021, true,'2021-12-08T08:25:24.00Z'),
+           ('transcript2_id', 'student1_id', 'S2', 2021, true,'2021-12-10T08:25:24.00Z'),
+           ('transcript3_id', 'student1_id', 'S3', 2022, true,'2021-12-09T08:25:24.00Z'),
+           ('transcript4_id', 'student2_id', 'S1', 2021, true,'2021-12-09T08:25:25.00Z'),
+           ('transcript5_id', 'student2_id', 'S2', 2021, true,'2021-12-08T08:25:25.00Z'),
+           ('transcript6_id', 'student1_id', 'S1', 2021, true,'2021-12-09T08:25:25.00Z');
