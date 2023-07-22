57e0c6c6fc5cd31929d454089296ae9a2a51164e
Micael Jerry <hei.jerry.2@gmail.com>
2023-07-22 10:02:11 +0300

    chore: update controller

--- src/main/java/school/hei/haapi/endpoint/rest/controller/TranscriptController.java
+++ src/main/java/school/hei/haapi/endpoint/rest/controller/TranscriptController.java
@@ -3,7 +3,6 @@ package school.hei.haapi.endpoint.rest.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import school.hei.haapi.endpoint.rest.mapper.TranscriptMapper;
 import school.hei.haapi.endpoint.rest.model.Transcript;
@@ -14,12 +13,11 @@ import java.util.stream.Collectors;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("/students/{student_id}")
 public class TranscriptController {
     private TranscriptService transcriptService;
     private TranscriptMapper transcriptMapper;
 
-    @GetMapping(value = "/transcripts")
+    @GetMapping(value = "/students/{student_id}/transcripts")
     public List<Transcript> getTranscripts(@PathVariable String student_id) {
         return transcriptService.getAllByStudentId(student_id)
                 .stream()
@@ -27,7 +25,7 @@ public class TranscriptController {
                 .collect(Collectors.toUnmodifiableList());
     }
 
-    @GetMapping(value = "/transcripts/{transcript_id}")
+    @GetMapping(value = "/students/{student_id}/transcripts/{transcript_id}")
     public Transcript getTranscriptById(@PathVariable String student_id, @PathVariable String transcript_id) {
         return transcriptMapper.toRest(transcriptService.getByStudentIdAndId(student_id, transcript_id));
     }
