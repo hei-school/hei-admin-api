22fc1971dda75f308cfba1a5542950fbdd36dc04
Cedric <99885260+Cenikl@users.noreply.github.com>
2023-07-27 11:33:53 +0300

    fix: insert in database

--- src/main/resources/db/testdata/V99_8__Test_create_transcript_versions.sql
+++ src/main/resources/db/testdata/V99_8__Test_create_transcript_versions.sql
@@ -6,4 +6,4 @@ values ('transcript_version1_id',1,'2023-10-01T08:25:24.00Z','test1','transcript
        ('transcript_version4_id',4,'2023-10-04T08:25:24.00Z','test4','transcript1_id','manager1_id'),
        ('transcript_version5_id',1,'2023-11-01T08:25:24.00Z','test5','transcript2_id','manager1_id'),
        ('transcript_version6_id',2,'2023-11-02T08:25:24.00Z','test6','transcript2_id','manager1_id'),
-       ('transcript_version7_id',3,'2023-11-03T08:25:24.00Z','test7','transcript2_id','manager1_id'),
+       ('transcript_version7_id',3,'2023-11-03T08:25:24.00Z','test7','transcript2_id','manager1_id');
