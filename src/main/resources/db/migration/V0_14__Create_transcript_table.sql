dedc04e1fe44eb78cded273119ab02c9bd4a5ccd
Haritiana Adriano <hei.adriano.4@gmail.com>
2023-08-03 08:53:03 +0300

    refactor: transcript table

--- src/main/resources/db/migration/V0_14__Create_transcript_table.sql
+++ src/main/resources/db/migration/V0_14__Create_transcript_table.sql
@@ -1,9 +1,11 @@
-create table if not exists "transcript" (
-    id varchar constraint transcript_pk
-    primary key default uuid_generate_v4(),
-    student_id varchar,
-    semester varhcar,
-    academic_year integer,
-    is_defenitive boolean,
-    creation_datetime timestamp with time zone not null default now()
-)
+create table if not exists "transcript"
+(
+    id                  varchar
+    constraint transcript_pk primary key default uuid_generate_v4(),
+    student_id          varchar not null
+    constraint transcript_student_id_fk references "user"(id),
+    semester            varchar,
+    academic_year       integer check( academic_year > 0),
+    is_definitive boolean,
+    creation_datetime   timestamp with time zone not null default now()
+    );
