1341ff61beae7a12f58a51fe195482b2c82f80dd
Ny Hasina VAGNO <hei.nyhasina@gmail.com>
2023-08-03 21:47:59 +0300

    chore: transcript table

--- src/main/resources/db/migration/V0_14__Create_transcript_table.sql
+++ src/main/resources/db/migration/V0_14__Create_transcript_table.sql
@@ -1,11 +1,20 @@
-create table if not exists "transcript"
-(
-    id                  varchar
-    constraint transcript_pk primary key default uuid_generate_v4(),
-    student_id          varchar not null
-    constraint transcript_student_id_fk references "user"(id),
-    semester            varchar,
+do
+$$
+begin
+        if not exists(select from pg_type where typname = 'semester') then
+create type "semester" as enum ('S1', 'S2', 'S3', 'S4', 'S5', 'S6');
+end if;
+end
+$$;
+
+
+create table if not exists "transcript" (
+    id varchar constraint transcript_pk
+    primary key default uuid_generate_v4(),
+    student_id varchar not null
+    constraint transcript_user_id_fk references "user"(id),
+    semester semester,
     academic_year       integer check( academic_year > 0),
-    is_definitive boolean,
-    creation_datetime   timestamp with time zone not null default now()
-    );
+    is_defenitive boolean,
+    creation_datetime timestamp with time zone not null default now()
+)
