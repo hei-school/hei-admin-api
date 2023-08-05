426554c55a73040e69bec33239a797222d642325
Micael Jerry <hei.jerry.2@gmail.com>
2023-08-05 13:10:58 +0300

    fix: reformat code

--- src/main/java/school/hei/haapi/endpoint/rest/controller/TranscriptController.java
+++ src/main/java/school/hei/haapi/endpoint/rest/controller/TranscriptController.java
@@ -1,7 +1,12 @@
 package school.hei.haapi.endpoint.rest.controller;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import school.hei.haapi.endpoint.rest.mapper.TranscriptMapper;
 import school.hei.haapi.endpoint.rest.model.Transcript;
 import school.hei.haapi.endpoint.rest.validator.CreateTranscriptValidator;
