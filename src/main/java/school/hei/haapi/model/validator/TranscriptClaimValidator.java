741cad57379e5d7da9694209c1209bf6b5560ca3
Lova Fenohasina Lala <fenohasinalala@gmail.com>
2023-08-06 14:51:11 +0300

    fix: TranscriptClaim model validator

--- src/main/java/school/hei/haapi/model/validator/TranscriptClaimValidator.java
+++ src/main/java/school/hei/haapi/model/validator/TranscriptClaimValidator.java
@@ -2,15 +2,11 @@ package school.hei.haapi.model.validator;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
-import school.hei.haapi.model.Course;
-import school.hei.haapi.model.Fee;
 import school.hei.haapi.model.TranscriptClaim;
-import school.hei.haapi.model.User;
 import school.hei.haapi.model.exception.BadRequestException;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.Validator;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.function.Consumer;
@@ -27,10 +23,10 @@ public class TranscriptClaimValidator implements Consumer<TranscriptClaim> {
     public void accept(TranscriptClaim transcriptClaim) {
         Set<ConstraintViolation<TranscriptClaim>> violations = validator.validate(transcriptClaim);
         if (transcriptClaim.getId() == null) {
-            throw new BadRequestException("transcriptClaim is mandatory");
+            throw new BadRequestException("TranscriptClaim id is mandatory");
         }
         if (transcriptClaim.getClaimStatus() == null) {
-            throw new BadRequestException("Due datetime is mandatory");
+            throw new BadRequestException("Status is mandatory");
         }
         if (transcriptClaim.getCreationDatetime() != null
                 && transcriptClaim.getClosedDatetime() != null
@@ -38,7 +34,7 @@ public class TranscriptClaimValidator implements Consumer<TranscriptClaim> {
             throw new BadRequestException("closed datetime should be after creation datetime");
         }
         if (transcriptClaim.getReason() == null) {
-            throw new BadRequestException("reason is mandatory");
+            throw new BadRequestException("Reason is mandatory");
         }
 
         if (!violations.isEmpty()) {
