9adaf074d792a2163f627cc9993a43c011164808
Jean Koletto Reichmann GALY <hei.reichmann.4@gmail.com>
2023-08-07 16:01:34 +0300

    chore: refactor transcript mapper

--- src/main/java/school/hei/haapi/endpoint/rest/mapper/TranscriptMapper.java
+++ src/main/java/school/hei/haapi/endpoint/rest/mapper/TranscriptMapper.java
@@ -3,7 +3,7 @@ package school.hei.haapi.endpoint.rest.mapper;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import school.hei.haapi.model.Transcript;
+import school.hei.haapi.endpoint.rest.model.Transcript;
 import school.hei.haapi.model.User;
 import school.hei.haapi.model.exception.NotFoundException;
 import school.hei.haapi.service.UserService;
@@ -15,30 +15,30 @@ import java.util.Objects;
 @Slf4j
 public class TranscriptMapper {
 
-  private final UserService userService;
-  public school.hei.haapi.endpoint.rest.model.Transcript toRest(Transcript transcript) {
-    var restTranscript = new school.hei.haapi.endpoint.rest.model.Transcript();
-    restTranscript.setId(transcript.getId());
-    restTranscript.setStudentId(transcript.getStudent().getId());
-    restTranscript.setSemester(transcript.getSemester());
-    restTranscript.setAcademicYear(transcript.getAcademicYear());
-    restTranscript.setIsDefinitive(transcript.getIsDefinitive());
-    restTranscript.setCreationDatetime(transcript.getCreationDatetime());
-    return restTranscript;
-  }
+    private final UserService userService;
 
-  public Transcript toDomain(school.hei.haapi.endpoint.rest.model.Transcript restTranscript, String studentId) {
-    final User student = userService.getById(studentId);
-    if (Objects.equals(student, null)) {
-      throw new NotFoundException("Student.id=" + studentId + " is not found");
+    public Transcript toRest(school.hei.haapi.model.Transcript transcript) {
+        return new Transcript()
+                .id(transcript.getId())
+                .studentId(transcript.getStudent().getId())
+                .semester(transcript.getSemester())
+                .academicYear(transcript.getAcademicYear())
+                .isDefinitive(transcript.getIsDefinitive())
+                .creationDatetime(transcript.getCreationDatetime());
     }
-    return Transcript.builder()
-        .id(restTranscript.getId())
-        .student(student)
-        .semester(restTranscript.getSemester())
-        .academicYear(restTranscript.getAcademicYear())
-        .isDefinitive(restTranscript.getIsDefinitive())
-        .creationDatetime(restTranscript.getCreationDatetime())
-        .build();
-  }
+
+    public school.hei.haapi.model.Transcript toDomain(Transcript restTranscript, String studentId) {
+        final User student = userService.getById(studentId);
+        if (Objects.equals(student, null)) {
+            throw new NotFoundException("Student.id=" + studentId + " is not found");
+        }
+        return school.hei.haapi.model.Transcript.builder()
+                .id(restTranscript.getId())
+                .student(student)
+                .semester(restTranscript.getSemester())
+                .academicYear(restTranscript.getAcademicYear())
+                .isDefinitive(restTranscript.getIsDefinitive())
+                .creationDatetime(restTranscript.getCreationDatetime())
+                .build();
+    }
 }
