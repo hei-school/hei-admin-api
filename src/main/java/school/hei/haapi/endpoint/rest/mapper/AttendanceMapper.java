3cfe4776d92703361e5de62dbde3d7796340cf7d
Haritiana Adriano <hei.adriano.4@gmail.com>
2023-09-14 16:51:02 +0300

    feat: bad request on attendance mapper if the student doesn't exist

--- src/main/java/school/hei/haapi/endpoint/rest/mapper/AttendanceMapper.java
+++ src/main/java/school/hei/haapi/endpoint/rest/mapper/AttendanceMapper.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 import school.hei.haapi.endpoint.rest.model.CreateAttendanceMovement;
 import school.hei.haapi.endpoint.rest.model.StudentAttendanceMovement;
 import school.hei.haapi.model.StudentAttendance;
+import school.hei.haapi.model.exception.BadRequestException;
 import school.hei.haapi.model.exception.NotFoundException;
 import school.hei.haapi.repository.UserRepository;
 
@@ -23,13 +24,14 @@ public class AttendanceMapper {
   }
 
   public StudentAttendance toDomain (CreateAttendanceMovement toCreate) {
+    if(userRepository.findById(toCreate.getStudentId()) == null) {
+      throw new BadRequestException("the student with #"+toCreate.getStudentId()+" doesn't exist");
+    }
     return StudentAttendance.builder()
         .attendanceMovementType(toCreate.getAttendanceMovementType())
         .place(toCreate.getPlace())
         .createdAt(toCreate.getCreatedAt())
-        .student(userRepository.findById(toCreate.getStudentId()).orElseThrow(() -> {
-          throw new NotFoundException("student with #"+toCreate.getStudentId()+" not found");
-        }))
+        .student(userRepository.findById(toCreate.getStudentId()).get())
         .build();
   }
 }
---
+++ src/main/java/school/hei/haapi/model/validator/AttendanceValidator.java
@@ -0,0 +1,38 @@
+package school.hei.haapi.model.validator;
+
+
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+import school.hei.haapi.model.StudentAttendance;
+import school.hei.haapi.model.exception.BadRequestException;
+import school.hei.haapi.repository.UserRepository;
+
+@Component
+@AllArgsConstructor
+public class AttendanceValidator implements Consumer<StudentAttendance> {
+  private final UserRepository userRepository;
+
+  public void accept(List<StudentAttendance> studentAttendances) {
+    studentAttendances.forEach(this::accept);
+  }
+
+  @Override
+  public void accept(StudentAttendance studentAttendance) {
+    Set<String> violationMessages = new HashSet<>();
+    if(studentAttendance.getStudent() == null) {
+      violationMessages.add("Student is mandatory");
+    }
+    if(!violationMessages.isEmpty()) {
+      String formatedViolationMessages = violationMessages.stream()
+          .map(String::toString)
+          .collect(Collectors.joining(". "));
+      throw new BadRequestException(formatedViolationMessages);
+    }
+  }
+}
