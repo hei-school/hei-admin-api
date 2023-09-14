89c4f5f98349fe5b299813e2b1777f2da0ab8392
Haritiana Adriano <hei.adriano.4@gmail.com>
2023-09-14 13:21:48 +0300

    feat: attendance and course session model

---
+++ src/main/java/school/hei/haapi/model/CourseSession.java
@@ -0,0 +1,35 @@
+package school.hei.haapi.model;
+
+import static javax.persistence.GenerationType.IDENTITY;
+import java.io.Serializable;
+import java.time.Instant;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "\"course_session\"")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CourseSession implements Serializable {
+  @Id
+  @GeneratedValue(strategy = IDENTITY)
+  private String id;
+
+  @ManyToOne
+  @JoinColumn(name = "course_id")
+  private Course course;
+
+  private Instant begin;
+
+  private Instant end;
+}
