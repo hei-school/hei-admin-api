a8cf774ea3beb51c21438ce248e6abfeb6fca2f5
Haritiana Adriano <hei.adriano.4@gmail.com>
2023-09-14 14:00:54 +0300

    feat: attendance mapper

--- src/main/java/school/hei/haapi/model/StudentAttendance.java
+++ src/main/java/school/hei/haapi/model/StudentAttendance.java
@@ -12,19 +12,28 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
 import school.hei.haapi.endpoint.rest.model.AttendanceMovementType;
 import school.hei.haapi.endpoint.rest.model.PlaceEnum;
+import school.hei.haapi.repository.types.PostgresEnumType;
 
 @Entity
 @Table(name = "\"attendance\"")
 @Getter
 @Setter
+@TypeDef(name = "pgsql_enum", typeClass = PostgresEnumType.class)
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder(toBuilder = true)
+@EqualsAndHashCode
+@ToString
 public class StudentAttendance implements Serializable {
   @Id
   @GeneratedValue(strategy = IDENTITY)
@@ -49,4 +58,8 @@ public class StudentAttendance implements Serializable {
   @ManyToOne
   @JoinColumn(name = "student_id")
   private User student;
+
+  public boolean isLateOf(Instant toCompare) {
+    return this.createdAt.isAfter(toCompare);
+  }
 }
