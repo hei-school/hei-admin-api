39e113b730ab864d6c6957b099dde571b9b7de9d
tanjonaaa <127602061+tanjonaaa@users.noreply.github.com>
2023-10-12 10:53:32 +0300

    fix: removed env variables export from build script

--- .shell/publish_gen_to_maven_local.sh
+++ .shell/publish_gen_to_maven_local.sh
@@ -1,1 +1,1 @@
-export JAVA_HOME=/usr/lib/jvm/java-1.17.0-openjdk-amd64 && export MAVEN_HOME=/home/tanjona/Programs/apache-maven-3.9.4 && export PATH=${PATH}:${JAVA_HOME}/bin:${MAVEN_HOME}/bin && cd build/gen && mvn clean install
+cd build/gen && mvn clean install
