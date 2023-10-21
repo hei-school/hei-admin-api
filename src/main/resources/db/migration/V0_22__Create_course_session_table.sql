eeecf687fb19af511882a74146b796b63e8d312c
Haritiana Adriano <hei.adriano.4@gmail.com>
2023-10-21 09:00:02 +0300

    refactor(alter-table with time zone): migration for course_session and attendance table

--- src/main/resources/db/migration/V0_22__Create_course_session_table.sql
+++ src/main/resources/db/migration/V0_22__Create_course_session_table.sql
@@ -4,5 +4,5 @@ create table if not exists "course_session" (
     awarded_course_id             varchar                     not null
     constraint course_session_awareded_course_fk references "awarded_course"(id),
     "begin"                 timestamp   not null,
-    "end"                   timestamp   not null
+    "end"                   timestamp   with time zone not null
 );
