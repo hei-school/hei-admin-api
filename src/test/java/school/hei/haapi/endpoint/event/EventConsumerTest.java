d7f693a95419a86d6a2a78654495c3d8ee701fe0
dinasoa <hei.dinasoa@gmail.com>
2023-11-10 16:56:51 +0300

    chore: upgrade poja to v2.3.0

---
+++ src/test/java/school/hei/haapi/endpoint/event/EventConsumerTest.java
@@ -0,0 +1,38 @@
+package school.hei.haapi.endpoint.event;
+
+import static java.util.UUID.randomUUID;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import school.hei.haapi.conf.FacadeTest;
+import school.hei.haapi.endpoint.event.gen.UuidCreated;
+import school.hei.haapi.repository.DummyUuidRepository;
+
+class EventConsumerTest extends FacadeTest {
+
+  @Autowired EventConsumer subject;
+  @Autowired DummyUuidRepository dummyUuidRepository;
+
+  @Test
+  void uuid_created_is_persisted() throws InterruptedException, JsonProcessingException {
+    var uuid = randomUUID().toString();
+    var uuidCreated = UuidCreated.builder().uuid(uuid).build();
+    var om = new ObjectMapper();
+    var payloadReceived = om.readValue(om.writeValueAsString(uuidCreated), UuidCreated.class);
+
+    subject.accept(
+        List.of(
+            new EventConsumer.AcknowledgeableTypedEvent(
+                new EventConsumer.TypedEvent(
+                    "school.hei.haapi.endpoint.event.gen.UuidCreated", payloadReceived),
+                () -> {})));
+
+    Thread.sleep(2_000);
+    var saved = dummyUuidRepository.findById(uuid).orElseThrow();
+    assertEquals(uuid, saved.getId());
+  }
+}
---
+++ src/test/java/school/hei/haapi/endpoint/rest/controller/HealthControllerTest.java
@@ -0,0 +1,24 @@
+package school.hei.haapi.endpoint.rest.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import school.hei.haapi.conf.FacadeTest;
+
+class HealthControllerTest extends FacadeTest {
+
+  @Autowired HealthController healthController;
+
+  @Test
+  void ping() {
+    assertEquals("pong", healthController.ping());
+  }
+
+  @Test
+  void can_read_from_dummy_table() {
+    var dummyTableEntries = healthController.dummyTable();
+    assertEquals(1, dummyTableEntries.size());
+    assertEquals("dummy-table-id-1", dummyTableEntries.get(0).getId());
+  }
+}
