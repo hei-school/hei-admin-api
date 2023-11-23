15566ab49b9f9f9e76ac15cfaa9b9aa3735038c7
Haritiana Adriano <hei.adriano.4@gmail.com>
2023-11-23 13:57:27 +0300

    chore: add creation datetime validator

--- src/main/java/school/hei/haapi/endpoint/rest/validator/CreatePaymentValidator.java
+++ src/main/java/school/hei/haapi/endpoint/rest/validator/CreatePaymentValidator.java
@@ -1,5 +1,6 @@
 package school.hei.haapi.endpoint.rest.validator;
 
+import java.time.Instant;
 import java.util.function.Consumer;
 import org.springframework.stereotype.Component;
 import school.hei.haapi.endpoint.rest.model.CreatePayment;
@@ -9,8 +10,15 @@ import school.hei.haapi.model.exception.BadRequestException;
 public class CreatePaymentValidator implements Consumer<CreatePayment> {
   @Override
   public void accept(CreatePayment createPayment) {
+    Instant now = Instant.now();
     if (createPayment.getAmount() == null) {
       throw new BadRequestException("Amount is mandatory");
+    }
+    if (createPayment.getCreationDatetime() == null) {
+      throw new BadRequestException("Creation datetime is mandatory");
+    }
+    if (createPayment.getCreationDatetime().isAfter(now)) {
+      throw new BadRequestException("Creation datetime must be before: " + now);
     }
   }
 }
