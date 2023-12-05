19841d78494ff18af706d91a57d00e30bcd7a53b
Haritiana Adriano <hei.adriano.4@gmail.com>
2023-12-05 14:02:58 +0300

    docs(api): add users file tags for scholarship certificate pdf
    feat: student file for scholarship certificate
    
    refactor: refactor student file service

---
+++ src/main/java/school/hei/haapi/endpoint/rest/controller/StudentFIleController.java
@@ -0,0 +1,21 @@
+package school.hei.haapi.endpoint.rest.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import school.hei.haapi.service.StudentFileService;
+
+@RestController
+@AllArgsConstructor
+public class StudentFIleController {
+  private final StudentFileService fileService;
+
+  @GetMapping(
+      value = "/students/{id}/scholarship_certificate/raw",
+      produces = {MediaType.APPLICATION_PDF_VALUE})
+  public byte[] getStudentScholarshipCertificatePdf(@PathVariable(name = "id") String studentId) {
+    return fileService.generatePdf(studentId);
+  }
+}
