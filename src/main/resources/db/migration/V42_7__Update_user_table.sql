94cff3dcbffbf3a909edcae800731879616c5575
Haritiana Adriano <hei.adriano.4@gmail.com>
2023-12-20 14:03:05 +0300

    fix: test failed and cannot be build

--- src/main/resources/db/migration/V42_7__Update_user_table.sql
+++ src/main/resources/db/migration/V42_7__Update_user_table.sql
@@ -1,3 +1,3 @@
 alter table "user" add column if not exists birth_place varchar default '';
 alter table "user" add column if not exists nic varchar default '';
-alter table "user" add column if not exists academic_stream academic_stream default null;
+alter table "user" add column if not exists academic_stream specialization_channel default null;
