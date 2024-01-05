134b5d60bf4c5efa8e2fae40c9fb595e90f44ca5
Haritiana Adriano <hei.adriano.4@gmail.com>
2024-01-05 14:50:54 +0300

    refactor: set column specialization default value to common core

--- src/main/resources/db/migration/V42_8__Update_user_table.sql
+++ src/main/resources/db/migration/V42_8__Update_user_table.sql
@@ -1,2 +1,1 @@
-alter table "user" drop column specialization_field;
-alter table "user" add column if not exists specialization_field specialization_field default 'COMMON_CORE';
+alter table "user" alter column specialization_field set default 'COMMON_CORE';
