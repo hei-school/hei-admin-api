2aa49a9176ae6d51cf979a3de6856741199324fe
Haritiana Adriano <hei.adriano.4@gmail.com>
2024-02-01 14:48:53 +0300

    refactor: refactor variable structure

--- src/main/java/school/hei/haapi/service/utils/SchoolYearUtils.java
+++ src/main/java/school/hei/haapi/service/utils/SchoolYearUtils.java
@@ -5,29 +5,27 @@ import java.time.Month;
 import java.util.List;
 import org.springframework.stereotype.Component;
 
+import static java.time.Month.DECEMBER;
+import static java.time.Month.NOVEMBER;
+import static java.time.Month.OCTOBER;
+
 @Component
 public class SchoolYearUtils {
+  private static List<Month> startOfSchoolYear = List.of(
+      OCTOBER,
+      NOVEMBER,
+      DECEMBER
+  );
+
   public static String getSchoolYear() {
     LocalDate now = LocalDate.now();
     LocalDate nextYear = now.plusYears(1L);
     LocalDate precedentYear = now.minusYears(1L);
 
-    if (!getNoMonthStartOfSchoolYear().contains(now.getMonth())) {
+    if (startOfSchoolYear.contains(now.getMonth())) {
       return now.getYear() + " - " + nextYear.getYear();
     }
 
     return precedentYear.getYear() + " - " + now.getYear();
-  }
-
-  public static List<Month> getNoMonthStartOfSchoolYear() {
-    return List.of(
-        Month.JANUARY,
-        Month.FEBRUARY,
-        Month.MARCH,
-        Month.APRIL,
-        Month.MAY,
-        Month.JUNE,
-        Month.AUGUST,
-        Month.SEPTEMBER);
   }
 }
