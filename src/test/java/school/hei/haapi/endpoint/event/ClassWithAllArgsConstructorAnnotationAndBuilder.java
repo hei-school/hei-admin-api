19a87a502c008881011230b18eeae3934d92ffb2
Mahefaa <hei.mahefa@gmail.com>
2024-02-02 16:27:35 +0300

    fix: add @AllArgsConstructor to endpoint.event.gen classes otherwise they cannot be deserialized from empty bean

---
+++ src/test/java/school/hei/haapi/endpoint/event/ClassWithAllArgsConstructorAnnotationAndBuilder.java
@@ -0,0 +1,9 @@
+package school.hei.haapi.endpoint.event;
+
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+@Builder
+@AllArgsConstructor
+public class ClassWithAllArgsConstructorAnnotationAndBuilder implements Serializable {}
