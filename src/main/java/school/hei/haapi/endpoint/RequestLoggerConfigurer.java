f84c42635955d5a1151dca64af263a8b1af31e8b
onitsiky <hei.onitsiky@gmail.com>
2024-03-13 18:00:59 +0300

    poja-upgrade: 12.2.0

--- src/main/java/school/hei/haapi/endpoint/RequestLoggerConfigurer.java
+++ src/main/java/school/hei/haapi/endpoint/RequestLoggerConfigurer.java
@@ -1,9 +1,10 @@
-package school.hei.haapi.endpoint.rest;
+package school.hei.haapi.endpoint;
 
 import static java.lang.System.currentTimeMillis;
 import static java.lang.Thread.currentThread;
-import static java.util.UUID.randomUUID;
 import static java.util.stream.Collectors.joining;
+import static school.hei.haapi.concurrency.ThreadRenamer.renameFrontalThread;
+import static school.hei.haapi.concurrency.ThreadRenamer.renameThread;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -11,17 +12,13 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.Nullable;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import school.hei.haapi.endpoint.rest.security.AuthProvider;
-import school.hei.haapi.endpoint.rest.security.model.Principal;
 
 @Configuration
+@AllArgsConstructor
 public class RequestLoggerConfigurer implements WebMvcConfigurer {
-
   @Override
   public void addInterceptors(InterceptorRegistry registry) {
     registry.addInterceptor(new RequestLogger());
@@ -32,18 +29,7 @@ public class RequestLoggerConfigurer implements WebMvcConfigurer {
   private static class RequestLogger implements HandlerInterceptor {
 
     private static final String THREAD_OLD_NAME = "threadOldName";
-    private static final int REQUEST_ID_LENGTH = 8;
     private static final String REQUEST_START_TIME = "startTime";
-
-    private static boolean shouldLog() {
-      return isAuthenticated();
-    }
-
-    private static boolean isAuthenticated() {
-      var securityContext = SecurityContextHolder.getContext();
-      return securityContext != null
-          && !(securityContext.getAuthentication() instanceof AnonymousAuthenticationToken);
-    }
 
     @Override
     public boolean preHandle(
@@ -53,26 +39,19 @@ public class RequestLoggerConfigurer implements WebMvcConfigurer {
       Thread current = currentThread();
       String oldThreadName = current.getName();
       request.setAttribute(THREAD_OLD_NAME, oldThreadName);
-      current.setName(randomUUID().toString().substring(0, REQUEST_ID_LENGTH));
+      renameFrontalThread(current);
 
       String parameters =
           request.getParameterMap().entrySet().stream()
               .map(entry -> entry.getKey() + "=" + String.join(",", entry.getValue()))
               .collect(joining(";"));
-      if (shouldLog()) {
-        Principal principal = AuthProvider.getPrincipal();
-        log.info(
-            "preHandle: "
-                + "userId={}, role={}, method={}, uri={}, parameters=[{}], "
-                + "handler={}, oldThreadName={}",
-            principal.getUserId(),
-            principal.getRole(),
-            request.getMethod(),
-            request.getRequestURI(),
-            parameters,
-            handler,
-            oldThreadName);
-      }
+      log.info(
+          "preHandle: " + "method={}, uri={}, parameters=[{}], " + "handler={}, oldThreadName={}",
+          request.getMethod(),
+          request.getRequestURI(),
+          parameters,
+          handler,
+          oldThreadName);
       return true;
     }
 
@@ -83,10 +62,8 @@ public class RequestLoggerConfigurer implements WebMvcConfigurer {
         Object handler,
         @Nullable Exception ex) {
       long duration = currentTimeMillis() - (long) request.getAttribute(REQUEST_START_TIME);
-      if (shouldLog()) {
-        log.info("afterCompletion: status={}, duration={}ms", response.getStatus(), duration, ex);
-      }
-      currentThread().setName(request.getAttribute(THREAD_OLD_NAME).toString());
+      log.info("afterCompletion: status={}, duration={}ms", response.getStatus(), duration, ex);
+      renameThread(currentThread(), request.getAttribute(THREAD_OLD_NAME).toString());
     }
   }
 }
