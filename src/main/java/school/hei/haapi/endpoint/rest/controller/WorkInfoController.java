76ceac51c62d30ba6e94fac66c74168dc6b4e8aa
Haritiana Adriano <hei.adriano.4@gmail.com>
2024-03-28 15:44:53 +0300

    test: managet GET /students/*/work_info ok

--- src/main/java/school/hei/haapi/endpoint/rest/controller/WorkInfoController.java
+++ src/main/java/school/hei/haapi/endpoint/rest/controller/WorkInfoController.java
@@ -2,7 +2,11 @@ package school.hei.haapi.endpoint.rest.controller;
 
 import java.util.List;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import school.hei.haapi.endpoint.rest.mapper.WorkInfoMapper;
 import school.hei.haapi.endpoint.rest.model.CrupdateWorkStudyStudent;
 import school.hei.haapi.endpoint.rest.model.WorkStudyStudent;
@@ -16,10 +20,10 @@ public class WorkInfoController {
   private final WorkInfoMapper workInfoMapper;
 
   @GetMapping("/students/{id}/work_info")
-  public WorkStudyStudent getStudentWorkInfo(@PathVariable(name = "id") String studentId) {
+  public List<WorkStudyStudent> getStudentWorkInfo(@PathVariable(name = "id") String studentId) {
     List<WorkInfo> studentWorkInfo = workInfoService.getWorkInfo(studentId);
 
-    return workInfoMapper.toRest(studentId, studentWorkInfo);
+    return List.of(workInfoMapper.toRest(studentId, studentWorkInfo));
   }
 
   @PutMapping("/students/{id}/work_info")
--- src/main/resources/db/migration/V42_22__Update_user_table.sql
+++ src/main/resources/db/migration/V42_22__Update_user_table.sql
@@ -1,2 +1,2 @@
 alter table "user" add column if not exists is_working_study boolean default false;
-alter table "user" add column if not exists taken_working_study default false;
+alter table "user" add column if not exists taken_working_study boolean default false;
