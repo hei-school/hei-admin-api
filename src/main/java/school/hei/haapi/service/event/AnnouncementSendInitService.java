0754512b9fe530cde918f5efa90ef17c6f3cede5
Iloniavo <hei.iloniavo@gmail.com>
2024-05-28 16:24:53 +0300

    log: users email

--- src/main/java/school/hei/haapi/service/event/AnnouncementSendInitService.java
+++ src/main/java/school/hei/haapi/service/event/AnnouncementSendInitService.java
@@ -1,14 +1,9 @@
 package school.hei.haapi.service.event;
 
-import static school.hei.haapi.model.User.Status.ENABLED;
-import static school.hei.haapi.service.utils.TemplateUtils.htmlToString;
-
 import jakarta.mail.internet.AddressException;
 import jakarta.mail.internet.InternetAddress;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Consumer;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.context.Context;
 import school.hei.haapi.endpoint.event.gen.AnnouncementSendInit;
@@ -18,8 +13,16 @@ import school.hei.haapi.model.User;
 import school.hei.haapi.model.notEntity.Group;
 import school.hei.haapi.service.UserService;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+import static school.hei.haapi.model.User.Status.ENABLED;
+import static school.hei.haapi.service.utils.TemplateUtils.htmlToString;
+
 @Service
 @AllArgsConstructor
+@Slf4j
 public class AnnouncementSendInitService implements Consumer<AnnouncementSendInit> {
 
   private final UserService userService;
@@ -42,6 +45,10 @@ public class AnnouncementSendInitService implements Consumer<AnnouncementSendIni
           case STUDENT -> getStudents(domain);
           case MANAGER -> userService.getByRoleAndStatus(User.Role.MANAGER, ENABLED);
         };
+
+    users.forEach(user -> {
+      log.info("email: {} , user_id: {}", user.getEmail(), user.getId());
+    });
 
     List<InternetAddress> targetListAddress =
         users.stream().map(this::getInternetAddressFromUser).toList();
