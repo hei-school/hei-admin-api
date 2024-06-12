e690abe51e1b42a7fd9ede45fb4b52445107741b
Mahefaa <hei.mahefa@gmail.com>
2024-06-12 12:43:23 +0300

    chore: send AnnouncementEmail in a separate event

---
+++ src/main/java/school/hei/haapi/service/event/AnnouncementEmailSendRequestedService.java
@@ -0,0 +1,49 @@
+package school.hei.haapi.service.event;
+
+import static school.hei.haapi.model.exception.ApiException.ExceptionType.SERVER_EXCEPTION;
+import static school.hei.haapi.service.utils.TemplateUtils.htmlToString;
+
+import jakarta.mail.internet.AddressException;
+import jakarta.mail.internet.InternetAddress;
+import java.util.List;
+import java.util.function.Consumer;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import school.hei.haapi.endpoint.event.gen.AnnouncementEmailSendRequested;
+import school.hei.haapi.mail.Email;
+import school.hei.haapi.mail.Mailer;
+import school.hei.haapi.model.exception.ApiException;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class AnnouncementEmailSendRequestedService
+    implements Consumer<AnnouncementEmailSendRequested> {
+  private final Mailer mailer;
+
+  private InternetAddress getInternetAddressFromUser(AnnouncementEmailSendRequested.MailUser user) {
+    try {
+      return new InternetAddress(user.email());
+    } catch (AddressException e) {
+      log.info("bad email format for {} {}", user.id(), user.email());
+      throw new ApiException(SERVER_EXCEPTION, e);
+    }
+  }
+
+  private static Context getMailContext(AnnouncementEmailSendRequested domain) {
+    Context initial = new Context();
+    initial.setVariable("fullName", domain.getSenderFullName());
+    initial.setVariable("id", domain.getAnnouncementId());
+    return initial;
+  }
+
+  @Override
+  public void accept(AnnouncementEmailSendRequested domain) {
+    var body = htmlToString("announcementEmail", getMailContext(domain));
+    InternetAddress to = getInternetAddressFromUser(domain.getTo());
+    mailer.accept(new Email(to, List.of(), List.of(), domain.getTitle(), body, List.of()));
+    log.info("mailSent {} {} to {}", domain.getTitle(), domain.getScope(), domain.getTo());
+  }
+}
--- src/main/java/school/hei/haapi/service/event/AnnouncementSendInitService.java
+++ src/main/java/school/hei/haapi/service/event/AnnouncementSendInitService.java
@@ -4,24 +4,19 @@ import static java.lang.Math.min;
 import static school.hei.haapi.model.User.Role.MANAGER;
 import static school.hei.haapi.model.User.Role.TEACHER;
 import static school.hei.haapi.model.User.Status.ENABLED;
-import static school.hei.haapi.model.exception.ApiException.ExceptionType.SERVER_EXCEPTION;
-import static school.hei.haapi.service.utils.TemplateUtils.htmlToString;
 
-import jakarta.mail.internet.AddressException;
-import jakarta.mail.internet.InternetAddress;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.thymeleaf.context.Context;
+import school.hei.haapi.endpoint.event.EventProducer;
+import school.hei.haapi.endpoint.event.gen.AnnouncementEmailSendRequested;
+import school.hei.haapi.endpoint.event.gen.AnnouncementEmailSendRequested.MailUser;
 import school.hei.haapi.endpoint.event.gen.AnnouncementSendInit;
-import school.hei.haapi.mail.Email;
-import school.hei.haapi.mail.Mailer;
 import school.hei.haapi.model.User;
-import school.hei.haapi.model.exception.ApiException;
 import school.hei.haapi.model.notEntity.Group;
 import school.hei.haapi.service.UserService;
 
@@ -29,10 +24,8 @@ import school.hei.haapi.service.UserService;
 @AllArgsConstructor
 @Slf4j
 public class AnnouncementSendInitService implements Consumer<AnnouncementSendInit> {
-  private static final int MAX_SES_RECIPIENT_SIZE = 50;
-
   private final UserService userService;
-  private final Mailer mailer;
+  private final EventProducer eventProducer;
 
   private List<User> getStudents(AnnouncementSendInit domain) {
     List<User> students = new ArrayList<>();
@@ -51,82 +44,35 @@ public class AnnouncementSendInitService implements Consumer<AnnouncementSendIni
           case STUDENT -> getStudents(domain);
           case MANAGER -> userService.getByRoleAndStatus(MANAGER, ENABLED);
         };
-    return users.stream().map(MailUser::of).toList();
-  }
-
-  public void sendEmail(AnnouncementSendInit domain) throws AddressException {
-    String htmlBody = htmlToString("announcementEmail", getMailContext(domain));
-    log.info("Send email : {}", htmlBody);
-    List<MailUser> users = getEmailUsers(domain);
-
-    log.info("nb of email recipients = {}", users.size());
-    List<InternetAddress> targetListAddresses =
-        users.stream().map(this::getInternetAddressFromUser).toList();
-
-    // mail admin first then CC other receivers in order to check for data arrival
-    InternetAddress adminMailReceiver = new InternetAddress("contact@mail.hei.school");
-    listGroups(targetListAddresses)
-        .forEach(
-            listGroup -> {
-              mailer.accept(
-                  new Email(
-                      adminMailReceiver,
-                      listGroup,
-                      List.of(),
-                      domain.getTitle(),
-                      htmlBody,
-                      List.of()));
-              log.info(
-                  "mailSent {} {} to {} users",
-                  domain.getTitle(),
-                  domain.getScope(),
-                  listGroup.size());
-            });
-  }
-
-  private InternetAddress getInternetAddressFromUser(MailUser user) {
-    try {
-      return new InternetAddress(user.email());
-    } catch (AddressException e) {
-      log.info("bad email format {}", user.email);
-      throw new ApiException(SERVER_EXCEPTION, e);
-    }
-  }
-
-  private static Context getMailContext(AnnouncementSendInit announcement) {
-    Context initial = new Context();
-    initial.setVariable("fullName", announcement.getSenderFullName());
-    initial.setVariable("id", announcement.getId());
-    return initial;
+    return users.stream().map(MailUser::from).toList();
   }
 
   @Override
-  public void accept(AnnouncementSendInit announcementSendInit) {
-    try {
-      sendEmail(announcementSendInit);
-    } catch (AddressException e) {
-      throw new RuntimeException(e);
+  public void accept(AnnouncementSendInit domain) {
+    List<MailUser> mailReceivers = getEmailUsers(domain);
+    log.info("nb of email recipients = {}", mailReceivers.size());
+    MailUser adminMailReceiver = new MailUser("admin_id", "contact@mail.hei.school");
+    var adminEmailRequest = AnnouncementEmailSendRequested.from(domain, adminMailReceiver);
+    List<AnnouncementEmailSendRequested> events =
+        mailReceivers.stream().map(to -> AnnouncementEmailSendRequested.from(domain, to)).toList();
+    var allReceivers = new ArrayList<>(events);
+    allReceivers.add(adminEmailRequest);
+    List<Object> list = Arrays.asList(allReceivers.toArray());
+    if (list.size() > EventProducer.Conf.MAX_PUT_EVENT_ENTRIES) {
+      group(list).forEach(eventProducer);
+    } else {
+      eventProducer.accept(list);
     }
   }
 
-  @Builder
-  private record MailUser(String id, String email) {
-    static MailUser of(User user) {
-      return new MailUser(user.getId(), user.getEmail());
-    }
-  }
+  // TODO: rm when poja ListGrouper is implemented in EventProducer
+  private List<List<Object>> group(List<Object> list) {
+    List<List<Object>> groupedList = new ArrayList<>();
+    int size = list.size();
 
-  private List<List<InternetAddress>> listGroups(List<InternetAddress> addresses) {
-    if (addresses.size() <= MAX_SES_RECIPIENT_SIZE) {
-      return List.of(addresses);
-    }
-    List<List<InternetAddress>> groupedList = new ArrayList<>();
-    int size = addresses.size();
-
-    int groupSize = MAX_SES_RECIPIENT_SIZE;
-    for (int i = 0; i < size; i += groupSize) {
-      int end = min(size, i + groupSize);
-      groupedList.add(new ArrayList<>(addresses.subList(i, end)));
+    for (int i = 0; i < size; i += EventProducer.Conf.MAX_PUT_EVENT_ENTRIES) {
+      int end = min(size, i + EventProducer.Conf.MAX_PUT_EVENT_ENTRIES);
+      groupedList.add(new ArrayList<>(list.subList(i, end)));
     }
 
     return groupedList;
