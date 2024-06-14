57382f8589f4a7b7d5a30a5edf0da8f27da70689
Haritiana Adriano <103512767+haritianaadriano@users.noreply.github.com>
2024-06-14 15:23:41 +0300

    fix: migration error when updating commitment begin default value

--- src/main/resources/db/migration/V43_33__Update_work_document_set_commitment_begin_not_null.sql
+++ src/main/resources/db/migration/V43_33__Update_work_document_set_commitment_begin_not_null.sql
@@ -1,1 +1,8 @@
-alter table "work_document" alter column commitment_begin set not null;
+-- Step 1: Update the `commitment_begin` column with `creation_datetime` where `commitment_begin` is NULL
+update work_document
+set commitment_begin = creation_datetime
+where commitment_begin is null;
+
+-- Step 2: Alter the `commitment_begin` column to set the NOT NULL constraint
+alter table work_document
+    alter column commitment_begin set not null;
