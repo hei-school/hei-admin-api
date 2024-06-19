d0ba735007750bc4d25e082706f03739537fe028
Iloniavo RANDRIAMANGA <99529538+Iloniavo@users.noreply.github.com>
2024-06-19 15:09:35 +0300

    feat: send an email reminder for unpaid fees

---
+++ src/main/java/school/hei/haapi/endpoint/event/gen/UnpaidFeesReminder.java
@@ -0,0 +1,33 @@
+package school.hei.haapi.endpoint.event.gen;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.time.Instant;
+import javax.annotation.processing.Generated;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@EqualsAndHashCode
+@Builder
+@ToString
+@AllArgsConstructor
+@Data
+@Generated("EventBridge")
+public class UnpaidFeesReminder {
+  @JsonProperty("id")
+  private String id;
+
+  @JsonProperty("email")
+  private String studentEmail;
+
+  @JsonProperty("remainingAmount")
+  private Integer remainingAmount;
+
+  @JsonProperty("dueDatetime")
+  private Instant dueDatetime;
+
+  @JsonProperty("comment")
+  private String comment;
+}
