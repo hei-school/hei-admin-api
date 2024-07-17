ef572acdcf643fea66cd241b8db4811b11e7d8fc
Haritiana Adriano <hei.adriano.4@gmail.com>
2024-07-17 13:47:28 +0300

    chore: add ToString annotation for loggs

--- src/main/java/school/hei/haapi/http/model/OrangeDailyTransactionScrappingDetails.java
+++ src/main/java/school/hei/haapi/http/model/OrangeDailyTransactionScrappingDetails.java
@@ -7,11 +7,13 @@ import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @Builder
 @EqualsAndHashCode
+@ToString
 public class OrangeDailyTransactionScrappingDetails {
   @JsonProperty("transactionDate")
   private String transactionDate;
