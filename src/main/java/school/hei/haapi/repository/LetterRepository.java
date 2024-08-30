2c2a9e82fdf5f6318f1e0f5958dba5c2cd74d5be
Iloniavo <hei.iloniavo@gmail.com>
2024-08-30 14:25:21 +0300

    feat: implements letterbox resources

--- src/main/java/school/hei/haapi/repository/LetterRepository.java
+++ src/main/java/school/hei/haapi/repository/LetterRepository.java
@@ -1,16 +1,12 @@
 package school.hei.haapi.repository;
 
-import org.springframework.data.domain.Page;
+import java.util.List;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import school.hei.haapi.model.Letter;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
 public interface LetterRepository extends JpaRepository<Letter, String> {
-    List<Letter> findAllByStudentId(String name, Pageable pageable);
+  List<Letter> findAllByStudentId(String name, Pageable pageable);
 }
