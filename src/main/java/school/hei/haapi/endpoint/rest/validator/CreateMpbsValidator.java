4cfb41d2ad2889cf412edd95b39bfc42304546fb
Haritiana Adriano <hei.adriano.4@gmail.com>
2024-09-06 14:41:46 +0300

    feat: handle psp id input format and validate it

--- src/main/java/school/hei/haapi/endpoint/rest/validator/CreateMpbsValidator.java
+++ src/main/java/school/hei/haapi/endpoint/rest/validator/CreateMpbsValidator.java
@@ -7,6 +7,8 @@ import school.hei.haapi.model.exception.BadRequestException;
 @Component
 public class CreateMpbsValidator {
   public void accept(String studentId, String feeId, CreateMpbs payload) {
+    String pspId = payload.getPspId();
+
     if (payload.getStudentId() == null
         || payload.getStudentId().isEmpty()
         || payload.getStudentId().isBlank()) {
@@ -24,13 +26,14 @@ public class CreateMpbsValidator {
     if (!feeId.equals(payload.getFeeId())) {
       throw new BadRequestException("Fee Path = " + feeId + " doesn't match to fee in payload");
     }
-    if (payload.getPspId() == null
-        || payload.getPspId().isBlank()
-        || payload.getPspId().isEmpty()) {
+    if (pspId == null || pspId.isBlank() || pspId.isEmpty()) {
       throw new BadRequestException("Psp id is mandatory");
     }
     if (payload.getPspType() == null) {
       throw new BadRequestException("Psp type is mandatory");
+    }
+    if (!pspId.matches("^MP.{18}$")) {
+      throw new BadRequestException("Psp id = must be 20 characters and must begin by MP");
     }
   }
 }
