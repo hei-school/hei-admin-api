5719a5067b5b66a276c2511ad6f54004d1639c44
Haritiana Adriano <hei.adriano.4@gmail.com>
2024-09-27 11:40:16 +0300

    feat: get actual remaining amount for actual payment

--- src/main/java/school/hei/haapi/service/utils/PaidFeeReceiptDataProvider.java
+++ src/main/java/school/hei/haapi/service/utils/PaidFeeReceiptDataProvider.java
@@ -2,6 +2,7 @@ package school.hei.haapi.service.utils;
 
 import static school.hei.haapi.service.utils.DataFormatterUtils.instantToCommonDate;
 
+import java.util.List;
 import school.hei.haapi.model.Fee;
 import school.hei.haapi.model.Payment;
 import school.hei.haapi.model.User;
@@ -10,11 +11,14 @@ public class PaidFeeReceiptDataProvider {
   private final User student;
   private final Fee fee;
   private final Payment payment;
+  private final List<Payment> paidPaymentsBefore;
 
-  public PaidFeeReceiptDataProvider(User student, Fee fee, Payment payment) {
+  public PaidFeeReceiptDataProvider(
+      User student, Fee fee, Payment payment, List<Payment> payments) {
     this.student = student;
     this.fee = fee;
     this.payment = payment;
+    this.paidPaymentsBefore = paymentsSinceActual(payments, payment);
   }
 
   public String getEntirePaymentAuthorName() {
@@ -34,7 +38,9 @@ public class PaidFeeReceiptDataProvider {
   }
 
   public int getRemainingAmount() {
-    return fee.getTotalAmount() - payment.getAmount();
+    List<Payment> paymentsSinceActual = paymentsSinceActual(paidPaymentsBefore, payment);
+    int actualRemainingAmount = defineRemainingAmountSinceActualPayment(paymentsSinceActual, fee);
+    return fee.getTotalAmount() - actualRemainingAmount;
   }
 
   public String getPaymentDate() {
@@ -43,5 +49,17 @@ public class PaidFeeReceiptDataProvider {
 
   public school.hei.haapi.endpoint.rest.model.Payment.TypeEnum getPaymentType() {
     return payment.getType();
+  }
+
+  private List<Payment> paymentsSinceActual(List<Payment> payments, Payment payment) {
+    int endIndex = payments.indexOf(payment);
+    return payments.subList(0, endIndex);
+  }
+
+  private int defineRemainingAmountSinceActualPayment(List<Payment> payments, Fee fee) {
+    int totalFeeAmount = fee.getTotalAmount();
+    int actualRemainingAmountSincePayment =
+        payments.stream().map(Payment::getAmount).reduce(0, Integer::sum);
+    return totalFeeAmount - actualRemainingAmountSincePayment;
   }
 }
