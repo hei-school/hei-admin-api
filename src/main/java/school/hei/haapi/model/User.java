69f9b03271e5c01a9222c6b13deeefa79f305cf6
tendryAxel <hei.tendry@gmail.com>
2024-10-11 22:21:19 +0300

    chore: static import FetchType.LAZY

--- src/main/java/school/hei/haapi/model/User.java
+++ src/main/java/school/hei/haapi/model/User.java
@@ -8,7 +8,6 @@ import static org.hibernate.type.SqlTypes.NAMED_ENUM;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
@@ -90,7 +89,7 @@ public class User implements Serializable {
   private String profilePictureKey;
 
   // RELATION (TEACHER): Awarded Courses
-  @OneToMany(fetch = FetchType.LAZY, mappedBy = "mainTeacher")
+  @OneToMany(mappedBy = "mainTeacher", fetch = LAZY)
   @ToString.Exclude
   private List<AwardedCourse> awardedCourses;
 
