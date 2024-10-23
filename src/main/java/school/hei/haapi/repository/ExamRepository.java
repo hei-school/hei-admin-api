f350ae139d9b974e2629acd3b9f5a9c443c8efd3
YlanDecode <rabenaly3@gmail.com>
2024-10-23 13:17:59 +0300

    feat: findByIdAndAwardedCourseId

--- src/main/java/school/hei/haapi/repository/ExamRepository.java
+++ src/main/java/school/hei/haapi/repository/ExamRepository.java
@@ -1,6 +1,8 @@
 package school.hei.haapi.repository;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -22,11 +24,13 @@ public interface ExamRepository extends JpaRepository<Exam, String> {
   @Query("select e from Exam e where e.awardedCourse.course.id = :course_id ")
   List<Exam> findExamsByCourseId(@Param("course_id") String courseId);
 
+  @Query("select e from Exam e where e.id = :exam_id and e.awardedCourse.id = :awardedCourseId")
+  Optional<Exam> findByIdAndAwardedCourseId(@Param("exam_id") String exam_id,
+                                            @Param("awardedCourseId") String awardedCourseId);
+
   @Query(
-      "select e from Exam e where e.awardedCourse.group.id = :group_id and "
-          + "e.awardedCourse.id = :awarded_course_id")
-  Page<Exam> findExamsByGroupIdAndAwardedGroupId(
-      @Param("group_id") String courseId,
+      "select e from Exam e where e.awardedCourse.id = :awarded_course_id")
+  Page<Exam> findExamsByAwardedGroupId(
       @Param("awarded_course_id") String awardedCourseId,
       Pageable pageable);
 }
