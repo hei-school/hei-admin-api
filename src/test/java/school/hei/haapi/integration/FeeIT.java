b1a0ee2ab597b67f90d090dbbe336242b9735fc9
mirado447 <103623110+mirado447@users.noreply.github.com>
2024-10-24 15:54:47 +0300

    feat(not-implemented): get monthy fees statistics
    * feat(not-implemented): payment statitics monthly
    
    * fix: add missing parameter

--- src/test/java/school/hei/haapi/integration/FeeIT.java
+++ src/test/java/school/hei/haapi/integration/FeeIT.java
@@ -141,7 +141,7 @@ class FeeIT extends MockedThirdParties {
     ApiClient manager1Client = anApiClient(MANAGER1_TOKEN);
     PayingApi api = new PayingApi(manager1Client);
 
-    List<Fee> actual = api.getFees(null, 1, 10, true, null);
+    List<Fee> actual = api.getFees(null, null, null, 1, 10, true, null);
 
     assertEquals(1, actual.size());
   }
@@ -153,7 +153,7 @@ class FeeIT extends MockedThirdParties {
 
     Fee actualFee = api.getStudentFeeById(STUDENT1_ID, FEE1_ID);
     List<Fee> actualFees1 = api.getStudentFees(STUDENT1_ID, 1, 5, null);
-    List<Fee> actualFees2 = api.getFees(PAID.toString(), 1, 10, false, null);
+    List<Fee> actualFees2 = api.getFees(PAID.toString(), null, null, 1, 10, false, null);
 
     assertEquals(fee1(), actualFee);
     assertEquals(2, actualFees2.size());
@@ -163,7 +163,7 @@ class FeeIT extends MockedThirdParties {
     assertTrue(actualFees2.contains(fee1()));
     assertTrue(actualFees2.contains(fee2()));
 
-    List<Fee> student2Fees = api.getFees(null, 1, 5, false, "STD21002");
+    List<Fee> student2Fees = api.getFees(null, null, null, 1, 5, false, "STD21002");
     assertEquals(student2Fees.getFirst(), fee4());
     assertFalse(student2Fees.contains(fee1()));
     assertFalse(student2Fees.contains(fee2()));
@@ -183,7 +183,7 @@ class FeeIT extends MockedThirdParties {
         () -> api.getStudentFees(STUDENT2_ID, null, null, null));
     assertThrowsApiException(
         "{\"type\":\"403 FORBIDDEN\",\"message\":\"Access is denied\"}",
-        () -> api.getFees(null, null, null, false, null));
+        () -> api.getFees(null, null, null, null, null, false, null));
   }
 
   @Test
@@ -193,7 +193,7 @@ class FeeIT extends MockedThirdParties {
 
     assertThrowsForbiddenException(() -> api.getStudentFeeById(STUDENT2_ID, FEE2_ID));
     assertThrowsForbiddenException(() -> api.getStudentFees(STUDENT2_ID, null, null, null));
-    assertThrowsForbiddenException(() -> api.getFees(null, null, null, false, null));
+    assertThrowsForbiddenException(() -> api.getFees(null, null, null, null, null, false, null));
   }
 
   @Test
@@ -209,7 +209,7 @@ class FeeIT extends MockedThirdParties {
         () -> api.getStudentFees(STUDENT2_ID, null, null, null));
     assertThrowsApiException(
         "{\"type\":\"403 FORBIDDEN\",\"message\":\"Access is denied\"}",
-        () -> api.getFees(null, null, null, false, null));
+        () -> api.getFees(null, null, null, null, null, false, null));
   }
 
   @Test
