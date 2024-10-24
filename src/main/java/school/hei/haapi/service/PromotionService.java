76dc47998ed4cda698f2280735ba93b439fde2d5
Haritiana Adriano <hei.adriano.4@gmail.com>
2024-10-24 13:31:24 +0300

    refactor: xlsx generator to be reusable

--- src/main/java/school/hei/haapi/service/PromotionService.java
+++ src/main/java/school/hei/haapi/service/PromotionService.java
@@ -1,19 +1,12 @@
 package school.hei.haapi.service;
 
 import static org.springframework.data.domain.Sort.Direction.ASC;
-import static school.hei.haapi.service.utils.DataFormatterUtils.formatLocalDate;
-import static school.hei.haapi.service.utils.DataFormatterUtils.instantToOcsDateFormat;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Optional;
 import lombok.AllArgsConstructor;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -26,6 +19,7 @@ import school.hei.haapi.model.User;
 import school.hei.haapi.model.exception.NotFoundException;
 import school.hei.haapi.repository.PromotionRepository;
 import school.hei.haapi.repository.dao.PromotionDao;
+import school.hei.haapi.service.utils.XlsxCellsGenerator;
 
 @Service
 @AllArgsConstructor
@@ -80,44 +74,7 @@ public class PromotionService {
   }
 
   private byte[] generateSheetsOfStudentsPromotionById(List<User> students) throws IOException {
-    Workbook workbook = new XSSFWorkbook();
-    Sheet sheet = workbook.createSheet("excel-sheet");
-    Row row0 = sheet.createRow(0);
-
-    row0.createCell(0).setCellValue("Réf");
-    row0.createCell(1).setCellValue("Nom");
-    row0.createCell(2).setCellValue("Prénom");
-    row0.createCell(3).setCellValue("email");
-    row0.createCell(4).setCellValue("CIN");
-    row0.createCell(5).setCellValue("Date de naissance");
-    row0.createCell(6).setCellValue("Lieu de naissance");
-    row0.createCell(7).setCellValue("Date d'entrée");
-    row0.createCell(8).setCellValue("Sex");
-
-    for (int i = 0; i < students.size(); i++) {
-      User useri = students.get(i);
-      Row rowi = sheet.createRow(i + 1);
-      generateStudentSheetCell(rowi, useri);
-    }
-
-    ByteArrayOutputStream bos = new ByteArrayOutputStream();
-
-    try {
-      workbook.write(bos);
-    } finally {
-      bos.close();
-    }
-    return bos.toByteArray();
-  }
-
-  private void generateStudentSheetCell(Row correspondingRow, User userToPrint) {
-    correspondingRow.createCell(0).setCellValue(userToPrint.getRef());
-    correspondingRow.createCell(1).setCellValue(userToPrint.getLastName());
-    correspondingRow.createCell(2).setCellValue(userToPrint.getFirstName());
-    correspondingRow.createCell(3).setCellValue(userToPrint.getEmail());
-    correspondingRow.createCell(4).setCellValue(userToPrint.getNic());
-    correspondingRow.createCell(5).setCellValue(formatLocalDate(userToPrint.getBirthDate()));
-    correspondingRow.createCell(6).setCellValue(userToPrint.getBirthPlace());
-    correspondingRow.createCell(8).setCellValue(String.valueOf(userToPrint.getSex()));
+    XlsxCellsGenerator<User> xlsxCellsGenerator = new XlsxCellsGenerator<>();
+    return xlsxCellsGenerator.apply(students);
   }
 }
