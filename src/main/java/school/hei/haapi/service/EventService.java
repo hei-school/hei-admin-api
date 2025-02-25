76b6cdb0da66a01a787fa62586545e70b643d647
tendryAxel <128225840+tendryAxel@users.noreply.github.com>
2025-02-25 14:35:14 +0300

    fix: creation event by frequency

--- src/main/java/school/hei/haapi/service/EventService.java
+++ src/main/java/school/hei/haapi/service/EventService.java
@@ -140,6 +140,7 @@ public class EventService {
   private List<Event> duplicateEventWithDifferentHour(
       Event event, Map<Instant, Instant> eventScheduled) {
     List<Event> duplicated = new ArrayList<>();
+    duplicated.add(event);
     eventScheduled.forEach(
         (b, e) -> {
           Event newEvent =
