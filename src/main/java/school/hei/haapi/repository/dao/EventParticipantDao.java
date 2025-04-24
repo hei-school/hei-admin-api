18ab5261280a9933a8e9b211d9b007d4e7d5abc0
tendryAxel <128225840+tendryAxel@users.noreply.github.com>
2025-04-24 10:45:32 +0300

    fix: getAllEventParticipants without date parameters

--- src/main/java/school/hei/haapi/repository/dao/EventParticipantDao.java
+++ src/main/java/school/hei/haapi/repository/dao/EventParticipantDao.java
@@ -7,8 +7,10 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.Path;
 import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.AllArgsConstructor;
@@ -108,16 +110,14 @@ public class EventParticipantDao {
       predicates.add(builder.equal(root.get("status"), attendanceStatus));
     }
 
+    Path<Instant> eventBeginDateTime = root.get("event").get(Event.BEGIN_DATETIME);
+    predicates.add(builder.isNotNull(eventBeginDateTime));
     if (eventBeginRange != null) {
       if (eventBeginRange.from() != null) {
-        predicates.add(
-            builder.greaterThanOrEqualTo(
-                root.get("event").get("beginDatetime"), eventBeginRange.from()));
+        predicates.add(builder.greaterThanOrEqualTo(eventBeginDateTime, eventBeginRange.from()));
       }
       if (eventBeginRange.to() != null) {
-        predicates.add(
-            builder.lessThanOrEqualTo(
-                root.get("event").get("beginDatetime"), eventBeginRange.to()));
+        predicates.add(builder.lessThanOrEqualTo(eventBeginDateTime, eventBeginRange.to()));
       }
     }
 
