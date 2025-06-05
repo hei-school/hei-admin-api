20fb7ecacd0559adbd98088adbe8484259821199
tendryAxel <hei.tendry@gmail.com>
2025-06-05 13:53:52 +0300

    chore: remove unused package

--- src/main/java/school/hei/haapi/handler/exceptionHandler/ExceptionHandler.java
+++
@@ -1,9 +0,0 @@
-package school.hei.haapi.handler.exceptionHandler;
-
-import school.hei.haapi.PojaGenerated;
-
-@PojaGenerated
-@SuppressWarnings("all")
-public interface ExceptionHandler<R> {
-  R handle(Throwable throwable);
-}
