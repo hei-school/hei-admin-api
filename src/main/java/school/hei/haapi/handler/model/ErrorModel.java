f7aa5622610dd5555f706dc03a39048efbf35dca
tendryAxel <128225840+tendryAxel@users.noreply.github.com>
2025-06-05 13:56:23 +0300

    chore: remove unused package

--- src/main/java/school/hei/haapi/handler/model/ErrorModel.java
+++
@@ -1,8 +0,0 @@
-package school.hei.haapi.handler.model;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-import school.hei.haapi.PojaGenerated;
-
-@PojaGenerated
-@SuppressWarnings("all")
-public record ErrorModel(@JsonProperty("message") String message) {}
