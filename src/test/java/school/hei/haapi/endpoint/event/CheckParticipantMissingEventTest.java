8ccb080f7739e63195e07059714c8560845cafd0
tendryAxel <hei.tendry@gmail.com>
2025-06-12 14:55:16 +0300

    chore: verify eventParticipantService::findByEventId call count

--- src/test/java/school/hei/haapi/endpoint/event/CheckParticipantMissingEventTest.java
+++ src/test/java/school/hei/haapi/endpoint/event/CheckParticipantMissingEventTest.java
@@ -1,6 +1,5 @@
 package school.hei.haapi.endpoint.event;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -8,7 +7,6 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static school.hei.haapi.endpoint.rest.model.AttendanceStatus.MISSING;
 
 import jakarta.mail.internet.AddressException;
 import java.time.Instant;
@@ -22,8 +20,6 @@ import school.hei.haapi.endpoint.event.model.MissedEventEmail;
 import school.hei.haapi.integration.conf.FacadeITMockedThirdParties;
 import school.hei.haapi.integration.conf.FakeDataProvider;
 import school.hei.haapi.mail.Mailer;
-import school.hei.haapi.model.Event;
-import school.hei.haapi.model.EventParticipant;
 import school.hei.haapi.model.User;
 import school.hei.haapi.repository.dao.EventDao;
 import school.hei.haapi.service.EventParticipantService;
@@ -52,18 +48,14 @@ class CheckParticipantMissingEventTest extends FacadeITMockedThirdParties {
 
   @Test
   void participant_missing_event_received_email() {
-    Event event1 = fakeDataProvider.someEvent();
     when(eventDao.findByCriteria(
             eq(null), any(Instant.class), any(Instant.class), eq(null), eq(null), eq(null)))
-        .thenReturn(List.of(event1));
-    when(eventParticipantService.findByEventId(anyString(), eq(null)))
-        .thenReturn(
-            List.of(
-                new EventParticipant("participant1", event1, new User(), MISSING, null),
-                new EventParticipant("participant2", event1, new User(), MISSING, null)));
+        .thenReturn(List.of(fakeDataProvider.someEvent(), fakeDataProvider.someEvent()));
 
-    // This function modify nothing, just test if not throw
-    assertDoesNotThrow(() -> subject.accept(new CheckParticipantMissedEventTriggered()));
+    subject.accept(new CheckParticipantMissedEventTriggered());
+
+    verify(eventParticipantService, times(2)).findByEventId(anyString(), eq(null));
+    verify(eventProducer, times(1)).accept(any());
   }
 
   @Test
