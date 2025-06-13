702ff15c437506892a067881d1394aa504cdbd9e
tendryAxel <hei.tendry@gmail.com>
2025-06-13 10:29:34 +0300

    chore: test AnnouncementSendInitService

--- src/test/java/school/hei/haapi/endpoint/event/AnnouncementSendInitServiceIT.java
+++ src/test/java/school/hei/haapi/endpoint/event/AnnouncementSendInitServiceIT.java
@@ -5,8 +5,12 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 import static school.hei.haapi.endpoint.rest.model.Scope.GLOBAL;
+import static school.hei.haapi.endpoint.rest.model.Scope.STUDENT;
+import static school.hei.haapi.endpoint.rest.model.Scope.TEACHER;
+import static school.hei.haapi.integration.conf.TestUtils.GROUP2_ID;
 import static school.hei.haapi.integration.conf.TestUtils.anAvailableRandomPort;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -15,8 +19,11 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import school.hei.haapi.endpoint.event.model.AnnouncementSendInit;
+import school.hei.haapi.endpoint.rest.model.Scope;
 import school.hei.haapi.integration.conf.AbstractContextInitializer;
 import school.hei.haapi.integration.conf.MockedThirdParties;
+import school.hei.haapi.model.notEntity.Group;
+import school.hei.haapi.service.GroupService;
 import school.hei.haapi.service.event.AnnouncementSendInitService;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
@@ -26,22 +33,47 @@ import school.hei.haapi.service.event.AnnouncementSendInitService;
 class AnnouncementSendInitServiceIT extends MockedThirdParties {
   @Autowired AnnouncementSendInitService announcementSendInitService;
   @MockBean EventProducer eventProducerMock;
+  @Autowired GroupService groupService;
 
-  static AnnouncementSendInit announcement() {
+  static AnnouncementSendInit announcement(Scope scope, List<Group> groups) {
     return AnnouncementSendInit.builder()
         .title("Title")
-        .scope(GLOBAL)
+        .scope(scope)
         .id("test_id")
+        .groups(groups)
         .senderFullName("John Doe")
         .build();
   }
 
+  static AnnouncementSendInit announcement(Scope scope) {
+    return announcement(scope, List.of());
+  }
+
   @Test
   void should_invoke_eventproducer() {
-    announcementSendInitService.accept(announcement());
+    announcementSendInitService.accept(announcement(GLOBAL));
 
     // 3 depends on actual data
     verify(eventProducerMock, times(3)).accept(any());
+  }
+
+  @Test
+  void should_invoke_eventproducer_teacher_scope() {
+    announcementSendInitService.accept(announcement(TEACHER));
+
+    verify(eventProducerMock, times(1)).accept(any());
+  }
+
+  @Test
+  void should_invoke_eventproducer_student_scope() {
+    announcementSendInitService.accept(
+        announcement(
+            STUDENT,
+            groupService.getAllById(List.of(GROUP2_ID)).stream()
+                .map(group -> new Group(group.getId(), group.getRef(), group.getName()))
+                .toList()));
+
+    verify(eventProducerMock, times(1)).accept(any());
   }
 
   static class ContextInitializer extends AbstractContextInitializer {
