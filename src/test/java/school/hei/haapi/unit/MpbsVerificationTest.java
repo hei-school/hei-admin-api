461667efe4543f8cdaf96096b1ef35cb4aab913a
tendryAxel <hei.tendry@gmail.com>
2025-07-03 11:41:08 +0300

    chore: test verification_skip_bad_mobile_payment

--- src/test/java/school/hei/haapi/unit/MpbsVerificationTest.java
+++ src/test/java/school/hei/haapi/unit/MpbsVerificationTest.java
@@ -1,6 +1,8 @@
 package school.hei.haapi.unit;
 
+import static java.time.temporal.ChronoUnit.DAYS;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.eq;
@@ -10,15 +12,22 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.time.Instant;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
+import school.hei.haapi.endpoint.event.EventProducer;
+import school.hei.haapi.endpoint.event.model.PaidFeeByMpbsFailedNotificationBody;
 import school.hei.haapi.http.mapper.ExternalResponseMapper;
 import school.hei.haapi.http.model.TransactionDetails;
+import school.hei.haapi.model.Fee;
 import school.hei.haapi.model.MobileTransactionDetails;
 import school.hei.haapi.model.Mpbs.Mpbs;
 import school.hei.haapi.model.Mpbs.MpbsVerification;
+import school.hei.haapi.model.Payment;
+import school.hei.haapi.model.User;
 import school.hei.haapi.service.ComputeVerifiedMobilePayement;
+import school.hei.haapi.service.FailedMobilePaymentNotification;
 import school.hei.haapi.service.MobilePaymentService;
 import school.hei.haapi.service.MobilePaymentUnverifiedHandler;
 import school.hei.haapi.service.MpbsVerificationService;
@@ -28,6 +37,7 @@ class MpbsVerificationTest {
   MobilePaymentUnverifiedHandler mobilePaymentUnverifiedHandlerMock = mock();
   ComputeVerifiedMobilePayement computeVerifiedMobilePayementMock = mock();
   ExternalResponseMapper externalResponseMapper = new ExternalResponseMapper();
+  EventProducer<PaidFeeByMpbsFailedNotificationBody> eventProducerMock = mock();
   MpbsVerificationService subject =
       new MpbsVerificationService(
           mock(),
@@ -37,6 +47,17 @@ class MpbsVerificationTest {
           mock(),
           mock(),
           mobilePaymentUnverifiedHandlerMock,
+          computeVerifiedMobilePayementMock);
+  MpbsVerificationService subjectWithUnverifiedPayementHandler =
+      new MpbsVerificationService(
+          mock(),
+          mock(),
+          mobilePaymentServiceMock,
+          externalResponseMapper,
+          mock(),
+          mock(),
+          new MobilePaymentUnverifiedHandler(
+              mock(), new FailedMobilePaymentNotification(eventProducerMock)),
           computeVerifiedMobilePayementMock);
 
   @Test
@@ -69,5 +90,60 @@ class MpbsVerificationTest {
     assertEquals(mbpsPending, mobilePaymentUnverified.getFirst());
     assertEquals(1, verifiedMpbs.size());
     assertEquals(fakeComputedVerifiedMpbs, verifiedMpbs.getFirst());
+  }
+
+  @Test
+  void verification_skip_bad_mobile_payment() {
+    var pendingCreationDatetime = Instant.now().minus(1, DAYS);
+    var failedCreationDatetime = Instant.now().minus(4, DAYS);
+    var student = User.builder().email("email@gmail.com").build();
+    var fee = Fee.builder().id("feeId").student(student).build();
+    var badMpbs =
+        Mpbs.builder().pspId("bad").creationDatetime(failedCreationDatetime).fee(fee).build();
+    var mpbsVerified = someMpbs("verified", pendingCreationDatetime, fee, student);
+    var mpbsFailed = someMpbs("pending", failedCreationDatetime, fee, student);
+    var correspondingMockTransactionsFromVerifiedMpbs =
+        MobileTransactionDetails.builder()
+            .pspTransactionRef(mpbsVerified.getPspId())
+            .pspTransactionAmount(0)
+            .build();
+    var fakeComputedVerifiedMpbs = new MpbsVerification();
+    when(mobilePaymentServiceMock.findAllTransactionByMpbsWithoutException(anyList()))
+        .thenReturn(List.of(correspondingMockTransactionsFromVerifiedMpbs));
+    when(computeVerifiedMobilePayementMock.saveTheVerifiedMpbs(
+            mpbsVerified,
+            externalResponseMapper.toRestMobileTransactionDetails(
+                correspondingMockTransactionsFromVerifiedMpbs)))
+        .thenReturn(fakeComputedVerifiedMpbs);
+
+    List<MpbsVerification> verifiedMpbs =
+        subjectWithUnverifiedPayementHandler.verifyMobilePaymentAndSaveResult(
+            List.of(badMpbs, mpbsVerified, mpbsFailed));
+
+    verify(computeVerifiedMobilePayementMock, never()).saveTheVerifiedMpbs(eq(badMpbs), any());
+    assertEquals(1, verifiedMpbs.size());
+    assertEquals(fakeComputedVerifiedMpbs, verifiedMpbs.getFirst());
+
+    ArgumentCaptor<List<PaidFeeByMpbsFailedNotificationBody>> argumentCaptor =
+        ArgumentCaptor.forClass(List.class);
+    verify(eventProducerMock, times(1)).accept(argumentCaptor.capture());
+    List<PaidFeeByMpbsFailedNotificationBody> notificationsRequestSend =
+        argumentCaptor.getAllValues().getLast();
+    assertEquals(2, notificationsRequestSend.size());
+    assertNull(notificationsRequestSend.getFirst());
+    assertEquals(
+        PaidFeeByMpbsFailedNotificationBody.from(
+            Payment.builder().fee(fee).amount(mpbsFailed.getAmount()).build()),
+        notificationsRequestSend.get(1));
+  }
+
+  private Mpbs someMpbs(String pspId, Instant creationDateTime, Fee fee, User student) {
+    return Mpbs.builder()
+        .pspId(pspId)
+        .creationDatetime(creationDateTime)
+        .fee(fee)
+        .student(student)
+        .amount(0)
+        .build();
   }
 }
