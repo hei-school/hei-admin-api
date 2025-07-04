a6f4a1076e5c1362d32eb916468a4234bbfc6013
tokyramarozaka <tokyramarozaka@gmail.com>
2025-07-04 16:31:13 +0300

    chore(test): add other scenarios to AdvancedFeeStatsServiceIT

--- src/test/java/school/hei/haapi/integration/AdvancedFeeStatsServiceIT.java
+++ src/test/java/school/hei/haapi/integration/AdvancedFeeStatsServiceIT.java
@@ -3,23 +3,28 @@ package school.hei.haapi.integration;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.when;
 import static school.hei.haapi.endpoint.rest.model.FeeCategory.L1;
 import static school.hei.haapi.endpoint.rest.model.FeeFrequency.MONTHLY;
-import static school.hei.haapi.endpoint.rest.model.FeeStatusEnum.PAID;
-import static school.hei.haapi.endpoint.rest.model.FeeStatusEnum.PENDING;
+import static school.hei.haapi.endpoint.rest.model.FeeStatusEnum.*;
 import static school.hei.haapi.endpoint.rest.model.FeeTypeEnum.TUITION;
 import static school.hei.haapi.integration.conf.TestUtils.setUpEventBridge;
-import static school.hei.haapi.model.statistics.AdvancedFeeStats.AdvancedFeeStatsCountType.RECEIPT;
+import static school.hei.haapi.model.statistics.AdvancedFeeStats.AdvancedFeeStatsCountType.ACCOUNTING;
 
 import java.time.Instant;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import school.hei.haapi.endpoint.rest.model.FeeStatusEnum;
 import school.hei.haapi.integration.conf.FacadeITMockedThirdParties;
 import school.hei.haapi.model.Fee;
 import school.hei.haapi.model.FeeStatusHistory;
@@ -27,10 +32,95 @@ import school.hei.haapi.repository.FeeRepository;
 import school.hei.haapi.service.AdvancedFeeStatsService;
 import software.amazon.awssdk.services.eventbridge.EventBridgeClient;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class AdvancedFeeStatsServiceIT extends FacadeITMockedThirdParties {
+  private static List<Fee> feeDueJunePaidInJuly;
+  private static List<Fee> feeDueJunePaidInMay;
+  private static List<Fee> feeDueJunePending;
+  private static List<Fee> feeDueJuneUnpaid;
+  private static List<Fee> feeDueJuneLate;
+
   @MockBean private FeeRepository feeRepositoryMock;
   @MockBean private EventBridgeClient eventBridgeClientMock;
   @Autowired private AdvancedFeeStatsService subject;
+
+  @BeforeAll
+  static void initializeMocks() {
+    feeDueJunePaidInJuly = List.of(createFeeDueJunePaidJuly());
+    feeDueJunePaidInMay = List.of(createFeeDueJunePaidMay());
+    feeDueJunePending = List.of(createFeeDueJunePending());
+    feeDueJuneUnpaid = List.of(createFeeDueJuneUnpaid());
+    feeDueJuneLate = List.of(createFeeDueJuneLate());
+  }
+
+  private static Fee createFeeDueJunePaidJuly() {
+    List<FeeStatusHistory> statusHistories =
+        List.of(
+            createStatus(PENDING, "2025-06-27T00:00:00.00Z"),
+            createStatus(PAID, "2025-07-17T00:00:00.00Z"));
+    return createFee(statusHistories, "2025-06-30T23:59:59Z");
+  }
+
+  private static Fee createFeeDueJunePaidMay() {
+    List<FeeStatusHistory> statusHistories =
+        List.of(
+            createStatus(PENDING, "2025-05-07T00:00:00.00Z"),
+            createStatus(PAID, "2025-05-17T00:00:00.00Z"));
+    return createFee(statusHistories, "2025-06-30T23:59:59Z");
+  }
+
+  private static FeeStatusHistory createStatus(FeeStatusEnum status, String datetime) {
+    return FeeStatusHistory.builder()
+        .id(UUID.randomUUID().toString())
+        .status(status)
+        .datetime(Instant.parse(datetime))
+        .build();
+  }
+
+  private static Fee createFee(List<FeeStatusHistory> statusHistories, String dueDatetime) {
+    return Fee.builder()
+        .id(UUID.randomUUID().toString())
+        .statusHistories(statusHistories)
+        .category(L1)
+        .type(TUITION)
+        .status(PAID)
+        .frequency(MONTHLY)
+        .mobilePayments(List.of())
+        .dueDatetime(Instant.parse(dueDatetime))
+        .build();
+  }
+
+  private static Fee createFeeDueJunePending() {
+    List<FeeStatusHistory> statusHistories =
+        List.of(createStatus(PENDING, "2025-06-27T00:00:00.00Z"));
+    return createFee(statusHistories, "2025-06-30T23:59:59Z");
+  }
+
+  private static Fee createFeeDueJuneUnpaid() {
+    List<FeeStatusHistory> statusHistories =
+        List.of(
+            createStatus(PENDING, "2025-06-27T00:00:00.00Z"),
+            createStatus(UNPAID, "2025-06-30T23:59:59Z"));
+    return createFee(statusHistories, "2025-06-30T23:59:59Z");
+  }
+
+  private static Fee createFeeDueJuneLate() {
+    List<FeeStatusHistory> statusHistories =
+        List.of(
+            createStatus(PENDING, "2025-06-27T00:00:00.00Z"),
+            createStatus(LATE, "2025-07-01T00:00:00.00Z"));
+    return createFee(statusHistories, "2025-06-30T23:59:59Z");
+  }
+
+  @AfterAll
+  void cleanUp() {
+    feeDueJunePaidInJuly = null;
+    feeDueJunePaidInMay = null;
+    feeDueJunePending = null;
+    feeDueJuneUnpaid = null;
+    feeDueJuneLate = null;
+    reset(feeRepositoryMock);
+  }
 
   @BeforeEach
   void setUp() {
@@ -38,38 +128,13 @@ class AdvancedFeeStatsServiceIT extends FacadeITMockedThirdParties {
   }
 
   @Test
-  void receipt_generated_fee_late_june_paid_july_stats_paid_june() {
-    List<FeeStatusHistory> mockedStatusHistory =
-        List.of(
-            FeeStatusHistory.builder()
-                .id("1")
-                .status(PENDING)
-                .datetime(Instant.parse("2025-06-27T00:00:00.00Z"))
-                .build(),
-            FeeStatusHistory.builder()
-                .id("2")
-                .status(PAID)
-                .datetime(Instant.parse("2025-07-17T00:00:00.00Z"))
-                .build());
-
-    List<Fee> mockedFees =
-        List.of(
-            Fee.builder()
-                .id("1")
-                .category(L1)
-                .type(TUITION)
-                .status(PAID)
-                .frequency(MONTHLY)
-                .mobilePayments(List.of())
-                .statusHistories(mockedStatusHistory)
-                .dueDatetime(Instant.parse("2025-06-30T00:00:00.00Z"))
-                .build());
-    when(feeRepositoryMock.findAllByDueDatetimeBetween(any(), any())).thenReturn(mockedFees);
+  void accounting_fee_due_june_paid_july_counts_as_paid_june() {
+    when(feeRepositoryMock.findAllByDueDatetimeBetween(any(), any()))
+        .thenReturn(feeDueJunePaidInJuly);
     subject.updateAdvancedFeeStats(
         Optional.of(Instant.parse("2025-06-01T00:00:00Z")),
         Optional.of(Instant.parse("2025-06-30T23:59:59Z")),
-        Optional.of(RECEIPT));
-
+        Optional.of(ACCOUNTING));
     var generatedJuneStats =
         subject.getAdvancedFeeStats(
             LocalDate.of(2025, 6, 1), LocalDate.of(2025, 6, 30), Optional.empty());
@@ -78,7 +143,7 @@ class AdvancedFeeStatsServiceIT extends FacadeITMockedThirdParties {
     subject.updateAdvancedFeeStats(
         Optional.of(Instant.parse("2025-07-01T00:00:00Z")),
         Optional.of(Instant.parse("2025-07-31T23:59:59Z")),
-        Optional.of(RECEIPT));
+        Optional.of(ACCOUNTING));
     var generatedJulyStats =
         subject.getAdvancedFeeStats(
             LocalDate.of(2025, 7, 1), LocalDate.of(2025, 7, 31), Optional.empty());
@@ -88,11 +153,54 @@ class AdvancedFeeStatsServiceIT extends FacadeITMockedThirdParties {
   }
 
   @Test
-  void request_receipt_not_avaialble_trigger_generation() {
+  void request_accounting_not_available_trigger_generation() {
     var generatedAugustStats =
         subject.getAdvancedFeeStats(
             LocalDate.of(2025, 7, 1), LocalDate.of(2025, 7, 31), Optional.empty());
 
     assertTrue(generatedAugustStats.getExpired());
+  }
+
+  @Test
+  void accounting_fee_due_june_paid_may_counts_as_paid_june() {
+    when(feeRepositoryMock.findAllByDueDatetimeBetween(any(), any()))
+        .thenReturn(feeDueJunePaidInMay);
+    subject.updateAdvancedFeeStats(
+        Optional.of(Instant.parse("2025-06-01T00:00:00Z")),
+        Optional.of(Instant.parse("2025-06-30T23:59:59Z")),
+        Optional.of(ACCOUNTING));
+    when(feeRepositoryMock.findAllByDueDatetimeBetween(any(), any())).thenReturn(List.of());
+    subject.updateAdvancedFeeStats(
+        Optional.of(Instant.parse("2025-05-01T00:00:00Z")),
+        Optional.of(Instant.parse("2025-05-31T23:59:59Z")),
+        Optional.of(ACCOUNTING));
+
+    var generatedJuneStats =
+        subject.getAdvancedFeeStats(
+            LocalDate.of(2025, 6, 1), LocalDate.of(2025, 6, 30), Optional.empty());
+    var generatedMayStats =
+        subject.getAdvancedFeeStats(
+            LocalDate.of(2025, 5, 1), LocalDate.of(2025, 5, 31), Optional.empty());
+
+    assertEquals(1, generatedJuneStats.getPaidFeesCount().getMonthly());
+    assertEquals(0, generatedMayStats.getPaidFeesCount().getMonthly());
+  }
+
+  @Test
+  void unpaid_or_pending_or_late_fees_do_not_affect_paid_stats() {
+    List<Fee> nonPaidFees =
+        List.of(
+            feeDueJunePending.getFirst(), feeDueJuneUnpaid.getFirst(), feeDueJuneLate.getFirst());
+    when(feeRepositoryMock.findAllByDueDatetimeBetween(any(), any())).thenReturn(nonPaidFees);
+    subject.updateAdvancedFeeStats(
+        Optional.of(Instant.parse("2025-06-01T00:00:00Z")),
+        Optional.of(Instant.parse("2025-06-30T23:59:59Z")),
+        Optional.of(ACCOUNTING));
+
+    var generatedJuneStats =
+        subject.getAdvancedFeeStats(
+            LocalDate.of(2025, 6, 1), LocalDate.of(2025, 6, 30), Optional.empty());
+
+    assertEquals(0, generatedJuneStats.getPaidFeesCount().getMonthly());
   }
 }
