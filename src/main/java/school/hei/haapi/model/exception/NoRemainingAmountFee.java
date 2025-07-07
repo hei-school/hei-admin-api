a96ed805e38c63385530a8670386040fb1807954
tendryAxel <hei.tendry@gmail.com>
2025-07-07 17:49:45 +0300

    chore: NoRemainingAmountFee only display fee id

--- src/main/java/school/hei/haapi/model/exception/NoRemainingAmountFee.java
+++ src/main/java/school/hei/haapi/model/exception/NoRemainingAmountFee.java
@@ -4,6 +4,6 @@ import school.hei.haapi.model.Fee;
 
 public class NoRemainingAmountFee extends ApiException {
   public NoRemainingAmountFee(Fee fee) {
-    super(ExceptionType.SERVER_EXCEPTION, "Remaining amount of %s is already 0".formatted(fee));
+    super(ExceptionType.SERVER_EXCEPTION, "Remaining amount of %s is already 0".formatted(fee.getId()));
   }
 }
