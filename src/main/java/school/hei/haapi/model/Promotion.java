e26edd92b13d6198bf472ab19510f504b896a7bb
tendryAxel <hei.tendry@gmail.com>
2025-07-24 15:56:30 +0300

    feat: Promotion::findLevelAt

--- src/main/java/school/hei/haapi/model/Promotion.java
+++ src/main/java/school/hei/haapi/model/Promotion.java
@@ -1,6 +1,11 @@
 package school.hei.haapi.model;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
+import static school.hei.haapi.endpoint.rest.model.StudentLevel.L1;
+import static school.hei.haapi.endpoint.rest.model.StudentLevel.L2;
+import static school.hei.haapi.endpoint.rest.model.StudentLevel.L3;
+import static school.hei.haapi.endpoint.rest.model.StudentLevel.M1;
+import static school.hei.haapi.endpoint.rest.model.StudentLevel.M2;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -8,6 +13,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,6 +23,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import org.hibernate.annotations.CreationTimestamp;
+import school.hei.haapi.endpoint.rest.model.StudentLevel;
+import school.hei.haapi.model.promotion.PromotionLevelOutOfRange;
 
 @Entity
 @Table(name = "\"promotion\"")
@@ -38,4 +47,33 @@ public class Promotion {
 
   @OneToMany(mappedBy = "promotion")
   private List<Group> groups;
+
+  public StudentLevel getLevelAt(Instant levelInstant) {
+    int firstYear = startDatetime.atZone(ZoneId.systemDefault()).getYear();
+
+    LocalDate date = levelInstant.atZone(ZoneId.systemDefault()).toLocalDate();
+    int year = date.getYear();
+    int month = date.getMonthValue();
+
+    int scholarYear = (month >= 11) ? year : year - 1;
+
+    int yearOfStudying = scholarYear - firstYear;
+
+    return switch (yearOfStudying) {
+      case 0 -> L1;
+      case 1 -> L2;
+      case 2 -> L3;
+      case 3 -> M1;
+      case 4 -> M2;
+      default -> throw new PromotionLevelOutOfRange(yearOfStudying);
+    };
+  }
+
+  public StudentLevel findLevelAt(Instant levelInstant) {
+    try {
+      return getLevelAt(levelInstant);
+    } catch (PromotionLevelOutOfRange e) {
+      return null;
+    }
+  }
 }
---
+++ src/main/java/school/hei/haapi/model/promotion/PromotionLevelOutOfRange.java
@@ -0,0 +1,9 @@
+package school.hei.haapi.model.promotion;
+
+public class PromotionLevelOutOfRange extends RuntimeException {
+  public PromotionLevelOutOfRange(int yearOfStudying) {
+    super(
+        "Promotion level out of range, already %d years old"
+            .formatted(yearOfStudying));
+  }
+}
