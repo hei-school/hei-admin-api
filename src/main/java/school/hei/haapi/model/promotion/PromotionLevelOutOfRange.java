407faee8f39da69c50cde1ea0448b4e40fc21f79
kirisaki_vk <kirisaki.vk@gmail.com>
2025-07-25 14:42:50 +0300

    chore: implement getStudentResultSummary

--- src/main/java/school/hei/haapi/model/promotion/PromotionLevelOutOfRange.java
+++ src/main/java/school/hei/haapi/model/promotion/PromotionLevelOutOfRange.java
@@ -2,8 +2,6 @@ package school.hei.haapi.model.promotion;
 
 public class PromotionLevelOutOfRange extends RuntimeException {
   public PromotionLevelOutOfRange(int yearOfStudying) {
-    super(
-        "Promotion level out of range, already %d years old"
-            .formatted(yearOfStudying));
+    super("Promotion level out of range, already %d years old".formatted(yearOfStudying));
   }
 }
--- src/main/java/school/hei/haapi/service/GradeResultService.java
+++ src/main/java/school/hei/haapi/service/GradeResultService.java
@@ -1,7 +1,12 @@
 package school.hei.haapi.service;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import school.hei.haapi.endpoint.rest.model.ResultSummary;
 import school.hei.haapi.endpoint.rest.model.StudentLevel;
 import school.hei.haapi.endpoint.rest.model.YearlyResult;
 import school.hei.haapi.model.exception.NotImplementedException;
@@ -13,4 +18,29 @@ public class GradeResultService {
     throw new NotImplementedException("Need to be implemented");
   }
 
+  public ResultSummary getStudentResultSummary(String studentId) {
+    List<YearlyResult> yearlyResultList =
+        Arrays.stream(StudentLevel.values())
+            .map(level -> getLeveledYearlyResultByStudentId(level, studentId))
+            .filter(Objects::nonNull)
+            .toList();
+
+    int obtainedCredits =
+        yearlyResultList.stream()
+            .map(YearlyResult::getObtainedCredits)
+            .filter(Objects::nonNull)
+            .mapToInt(BigDecimal::intValue)
+            .sum();
+
+    double average =
+        yearlyResultList.stream()
+            .mapToDouble(YearlyResult::getWeightedAverage)
+            .average()
+            .orElse(0.0);
+
+    return new ResultSummary()
+        .yearlyResults(yearlyResultList)
+        .obtainedCredits(BigDecimal.valueOf(obtainedCredits))
+        .weightedAverage(average);
+  }
 }
