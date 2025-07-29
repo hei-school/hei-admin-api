cf4d60db1a422fe1c164e5e6d36a6a8f1a86ae91
tendryAxel <hei.tendry@gmail.com>
2025-07-29 09:51:22 +0300

    chore: setup GradeResultServiceTest

--- src/test/java/school/hei/haapi/service/GradeResultServiceTest.java
+++ src/test/java/school/hei/haapi/service/GradeResultServiceTest.java
@@ -8,6 +8,7 @@ import static org.mockito.Mockito.when;
 import static school.hei.haapi.endpoint.rest.model.StudentLevel.L1;
 
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import school.hei.haapi.endpoint.rest.mapper.CourseMapper;
 import school.hei.haapi.endpoint.rest.model.YearlyResult;
@@ -25,30 +26,28 @@ class GradeResultServiceTest {
   private final GradeResultService gradeResultService =
       new GradeResultService(new CourseResultUtils(courseDao, gradeDao, new CourseMapper()));
 
-  @Test
-  void correct_result() {
-    var targetLevel = L1;
-    var student1 = User.builder().id("id").build();
-    var mgt1Grade =
-        Grade.builder().score(17.75).exam(Exam.builder().coefficient(1).build()).build();
-    var prog1Grade =
-        Grade.builder().score(13.59).exam(Exam.builder().coefficient(1).build()).build();
-    var donnees1Grade =
-        Grade.builder().score(15.4375).exam(Exam.builder().coefficient(1).build()).build();
-    var web1Grade =
-        Grade.builder().score(18.75).exam(Exam.builder().coefficient(1).build()).build();
-    var sys1Grade = Grade.builder().score(13.).exam(Exam.builder().coefficient(1).build()).build();
-    var lv1Grade = Grade.builder().score(13.91).exam(Exam.builder().coefficient(1).build()).build();
-    var mgt1Course = Course.builder().id("mgt1").credits(4).build();
-    var prog1Course = Course.builder().id("prog1").credits(6).build();
-    var donne1Course = Course.builder().id("donne1").credits(4).build();
-    var web1Course = Course.builder().id("web1").credits(6).build();
-    var sys1Course = Course.builder().id("sys1").credits(6).build();
-    var lv1Course = Course.builder().id("lv1").credits(4).build();
-    when(courseDao.findByCriteria(
-            any(), any(), any(), any(), any(), any(), any(), eq(targetLevel), any()))
-        .thenReturn(
-            List.of(mgt1Course, prog1Course, donne1Course, web1Course, sys1Course, lv1Course));
+  private final User student1 = User.builder().id("id").build();
+  private final Grade mgt1Grade =
+      Grade.builder().score(17.75).exam(Exam.builder().coefficient(1).build()).build();
+  private final Grade prog1Grade =
+      Grade.builder().score(13.59).exam(Exam.builder().coefficient(1).build()).build();
+  private final Grade donnees1Grade =
+      Grade.builder().score(15.4375).exam(Exam.builder().coefficient(1).build()).build();
+  private final Grade web1Grade =
+      Grade.builder().score(18.75).exam(Exam.builder().coefficient(1).build()).build();
+  private final Grade sys1Grade =
+      Grade.builder().score(13.).exam(Exam.builder().coefficient(1).build()).build();
+  private final Grade lv1Grade =
+      Grade.builder().score(13.91).exam(Exam.builder().coefficient(1).build()).build();
+  private final Course mgt1Course = Course.builder().id("mgt1").credits(4).build();
+  private final Course prog1Course = Course.builder().id("prog1").credits(6).build();
+  private final Course donne1Course = Course.builder().id("donne1").credits(4).build();
+  private final Course web1Course = Course.builder().id("web1").credits(6).build();
+  private final Course sys1Course = Course.builder().id("sys1").credits(6).build();
+  private final Course lv1Course = Course.builder().id("lv1").credits(4).build();
+
+  @BeforeEach
+  void setUp() {
     when(gradeDao.getStudentGradesByCourseId(mgt1Course.getId(), student1.getId()))
         .thenReturn(List.of(mgt1Grade));
     when(gradeDao.getStudentGradesByCourseId(prog1Course.getId(), student1.getId()))
@@ -61,6 +60,16 @@ class GradeResultServiceTest {
         .thenReturn(List.of(sys1Grade));
     when(gradeDao.getStudentGradesByCourseId(lv1Course.getId(), student1.getId()))
         .thenReturn(List.of(lv1Grade));
+
+    when(courseDao.findByCriteria(
+            any(), any(), any(), any(), any(), any(), any(), eq(L1), any()))
+            .thenReturn(
+                    List.of(mgt1Course, prog1Course, donne1Course, web1Course, sys1Course, lv1Course));
+  }
+
+  @Test
+  void correct_result_yearly_result() {
+    var targetLevel = L1;
 
     YearlyResult result =
         gradeResultService.getLeveledYearlyResultByStudentId(targetLevel, student1.getId());
