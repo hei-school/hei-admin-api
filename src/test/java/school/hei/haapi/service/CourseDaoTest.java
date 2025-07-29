5b7f8b4f876e236fcaaad424bd326a93a8bd6ff6
tendryAxel <hei.tendry@gmail.com>
2025-07-29 16:33:57 +0300

    chore: test filter contains target

--- src/test/java/school/hei/haapi/service/CourseDaoTest.java
+++ src/test/java/school/hei/haapi/service/CourseDaoTest.java
@@ -3,13 +3,11 @@ package school.hei.haapi.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static school.hei.haapi.endpoint.rest.model.StudentLevel.L2;
 
-import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import school.hei.haapi.integration.conf.FacadeITMockedThirdParties;
-import school.hei.haapi.model.Course;
 import school.hei.haapi.repository.dao.CourseDao;
 
 @Testcontainers
@@ -18,12 +16,11 @@ class CourseDaoTest extends FacadeITMockedThirdParties {
 
   @Test
   void filter_per_level_work() {
-    List<Course> courses =
-        subject.findByCriteria(null, null, null, null, null, null, null, L2, Pageable.unpaged());
+    var target = L2;
+    var courses =
+        subject.findByCriteria(
+            null, null, null, null, null, null, null, target, Pageable.unpaged());
 
-    assertEquals(3, courses.size());
-    assertEquals(L2, courses.getFirst().getStudentLevel());
-    assertEquals(L2, courses.get(1).getStudentLevel());
-    assertEquals(L2, courses.get(2).getStudentLevel());
+    courses.forEach(course -> assertEquals(target, course.getStudentLevel()));
   }
 }
