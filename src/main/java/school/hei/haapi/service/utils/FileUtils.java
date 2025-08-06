ef0e32be5c1a9995affafcef078935cc6ababd2d
kirisaki-vk <kirisaki.vk@gmail.com>
2025-08-06 13:10:21 +0300

    chore: refactor createFileFromBytes to FileUtils

--- src/main/java/school/hei/haapi/service/utils/FileUtils.java
+++ src/main/java/school/hei/haapi/service/utils/FileUtils.java
@@ -1,4 +1,17 @@
 package school.hei.haapi.service.utils;
 
+import java.io.File;
+import java.io.IOException;
+
 public class FileUtils {
+
+  public static File createFileFromBytes(byte[] bytes, String filename, String suffix) {
+    try {
+      File file = File.createTempFile(filename, suffix);
+      org.apache.commons.io.FileUtils.writeByteArrayToFile(file, bytes);
+      return file;
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
 }
