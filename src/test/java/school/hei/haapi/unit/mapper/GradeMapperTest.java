14a012098e2116cd03b5b29ab969d074c544d439
tendry axel <hei.tendry@gmail.com>
2025-08-12 16:22:08 +0300

    chore: test GradeMapper

---
+++ src/test/java/school/hei/haapi/unit/mapper/GradeMapperTest.java
@@ -0,0 +1,57 @@
+package school.hei.haapi.unit.mapper;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import school.hei.haapi.endpoint.rest.mapper.GradeMapper;
+import school.hei.haapi.endpoint.rest.model.CreateGrade;
+import school.hei.haapi.endpoint.rest.model.UpdateGrade;
+import school.hei.haapi.endpoint.rest.validator.GradeValidator;
+import school.hei.haapi.endpoint.rest.validator.UpdateGradeValidator;
+import school.hei.haapi.model.User;
+import school.hei.haapi.model.exception.BadRequestException;
+import school.hei.haapi.service.UserService;
+
+class GradeMapperTest {
+  private final GradeValidator gradeValidator = new GradeValidator();
+  private final UserService userService = mock();
+  private final GradeMapper subject =
+      new GradeMapper(
+          mock(),
+          mock(),
+          userService,
+          gradeValidator,
+          new UpdateGradeValidator(gradeValidator),
+          mock(),
+          mock());
+
+  @Test
+  void valid_grade_update_ok() {
+    UpdateGrade grade =
+        new UpdateGrade().comment("New comment").grade(new CreateGrade().score(20.));
+    String studentRef = "ref";
+    when(userService.findByRef(anyString())).thenReturn(User.builder().ref(studentRef).build());
+
+    var domain = subject.toDomain(grade, "", studentRef);
+
+    assertEquals(grade.getGrade().getScore(), domain.getGrade().getScore());
+    assertEquals(grade.getComment(), domain.getComment());
+    assertEquals(studentRef, domain.getStudent().getRef());
+  }
+
+  @Test
+  void invalid_grade_update_ko() {
+    UpdateGrade updateGrade = new UpdateGrade().comment("").grade(new CreateGrade().score(20.));
+
+    BadRequestException badRequestException =
+        assertThrows(BadRequestException.class, () -> subject.toDomain(updateGrade, "", ""));
+
+    assertEquals(
+        "Grade modification must be followed by comment about the change",
+        badRequestException.getMessage());
+  }
+}
