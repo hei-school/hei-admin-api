1ff36a2e32d90df14d47c1892774f219037d375d
tendry axel <hei.tendry@gmail.com>
2025-08-14 14:09:07 +0300

    chore: format

--- src/main/java/school/hei/haapi/model/GradeChangeHistory.java
+++ src/main/java/school/hei/haapi/model/GradeChangeHistory.java
@@ -8,7 +8,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import java.io.Serializable;
 import java.time.Instant;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
