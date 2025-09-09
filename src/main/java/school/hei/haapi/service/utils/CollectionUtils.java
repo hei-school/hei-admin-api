76e3427f4ac58d3829f2193998cb558abc02d177
tendryAxel <hei.tendry@gmail.com>
2025-09-09 13:37:34 +0300

    chore: CollectionUtils::findCommonElement return Collection

--- src/main/java/school/hei/haapi/service/utils/CollectionUtils.java
+++ src/main/java/school/hei/haapi/service/utils/CollectionUtils.java
@@ -2,15 +2,14 @@ package school.hei.haapi.service.utils;
 
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.Set;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CollectionUtils {
-  public <T> Set<T> findCommonElement(
-      Collection<T> excludeGroupIds, Collection<T> includeGroupIds) {
-    var common = new HashSet<>(excludeGroupIds);
-    common.retainAll(includeGroupIds);
+  public <T> Collection<T> findCommonElement(
+      Collection<T> collection1, Collection<T> collection2) {
+    Collection<T> common = new HashSet<>(collection1);
+    common.retainAll(collection2);
     return common;
   }
 }
