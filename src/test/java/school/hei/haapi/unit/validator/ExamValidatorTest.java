fc42b11570f419e487d576bdaaf0b5f433e37920
kirisaki_vk <kirisaki.vk@gmail.com>
2025-09-09 15:12:23 +0300

    chore: cleanup code

--- src/test/java/school/hei/haapi/unit/validator/ExamValidatorTest.java
+++ src/test/java/school/hei/haapi/unit/validator/ExamValidatorTest.java
@@ -134,7 +134,6 @@ class ExamValidatorTest {
     var exam = validExam();
     exam.setCoefficientFraction(aFraction(3, 2));
     var exception = assertThrowsDomainBadRequestException(() -> subject.accept(exam));
-
     assertTrue(
         exception
             .getMessage()
