d01c534d96e1c25b55ed4db4f1f6007ae24582f4
tendryAxel <hei.tendry@gmail.com>
2025-09-09 13:46:19 +0300

    chore: format

--- src/main/java/school/hei/haapi/service/utils/CollectionUtils.java
+++ src/main/java/school/hei/haapi/service/utils/CollectionUtils.java
@@ -6,8 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class CollectionUtils {
-  public <T> Collection<T> findCommonElement(
-      Collection<T> collection1, Collection<T> collection2) {
+  public <T> Collection<T> findCommonElement(Collection<T> collection1, Collection<T> collection2) {
     Collection<T> common = new HashSet<>(collection1);
     common.retainAll(collection2);
     return common;
