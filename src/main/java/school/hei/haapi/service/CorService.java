d172918c28ccfdc61d42a6c3be742653e083f181
tendryAxel <hei.tendry@gmail.com>
2025-09-16 11:10:45 +0300

    chore: CorService::savaAllStudentCor -> CorService::savaAll

--- src/main/java/school/hei/haapi/service/CorService.java
+++ src/main/java/school/hei/haapi/service/CorService.java
@@ -3,7 +3,6 @@ package school.hei.haapi.service;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import school.hei.haapi.model.BoundedPageSize;
@@ -16,14 +15,14 @@ import school.hei.haapi.repository.CorRepository;
 public class CorService {
   private final CorRepository corRepository;
 
-  public List<Cor> getCorByStudentId(String studentId, PageFromOne page, BoundedPageSize size) {
-    Pageable pageable =
+  public List<Cor> getByStudentId(String studentId, PageFromOne page, BoundedPageSize size) {
+    var pageable =
         PageRequest.of(
             page.getValue() - 1, size.getValue(), Sort.by("creationDatetime").descending());
     return corRepository.findAllByConcernedStudentId(studentId, pageable);
   }
 
-  public List<Cor> savaAllStudentCor(List<Cor> cors) {
+  public List<Cor> savaAll(List<Cor> cors) {
     return corRepository.saveAll(cors);
   }
 }
