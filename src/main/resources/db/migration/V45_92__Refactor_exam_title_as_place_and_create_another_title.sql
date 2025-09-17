a0bd0cfff1d997413d9fd6934880efbe81ee54d7
tendryAxel <hei.tendry@gmail.com>
2025-09-17 11:52:29 +0300

    fix: fill room based on the title

--- src/main/resources/db/migration/V45_92__Refactor_exam_title_as_place_and_create_another_title.sql
+++ src/main/resources/db/migration/V45_92__Refactor_exam_title_as_place_and_create_another_title.sql
@@ -7,10 +7,12 @@ ALTER TABLE event
     ADD COLUMN place place_name;
 
 UPDATE event
-SET room = CASE
-               WHEN title = ANY (enum_range(NULL::room_name)::text[])
-                   THEN title::room_name
-               ELSE NULL
-    END;
+SET room = COALESCE(
+        (SELECT rn::room_name
+         FROM unnest(enum_range(NULL::room_name)) rn
+         WHERE event.title ILIKE rn ::text
+        LIMIT 1 ),
+    'UNKNOWN'::room_name
+);
 
 COMMIT;
