1b3e81ccf2f49663dc9872b91d0e6cd02896ad68
kirisaki-vk <kirisaki.vk@gmail.com>
2025-09-29 11:05:06 +0300

    fix: handle all exceptions in attemptSaveTrasaction

--- src/main/java/school/hei/haapi/service/MobilePaymentService.java
+++ src/main/java/school/hei/haapi/service/MobilePaymentService.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Optional;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import school.hei.haapi.http.mapper.TransactionDetailsMapper;
 import school.hei.haapi.http.model.TransactionDetails;
@@ -73,7 +72,7 @@ public class MobilePaymentService implements MobileTransactionProvider {
     try {
       var saved = mobileTransactionDetailsRepository.save(transaction);
       savedTransactions.add(saved);
-    } catch (DataIntegrityViolationException e) {
+    } catch (RuntimeException e) {
       log.info(
           "Error saving mobile transaction of ref {} because of error",
           transaction.getPspTransactionRef(),
