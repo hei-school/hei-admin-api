af7b581a58ecaaa402303c98c57e17a0e4c4ed59
tendryAxel <hei.tendry@gmail.com>
2025-10-01 11:50:30 +0300

    test: reenable PaymentServiceTest::compute_user_status_after_paying_fee_ok

--- src/test/java/school/hei/haapi/service/PaymentServiceTest.java
+++ src/test/java/school/hei/haapi/service/PaymentServiceTest.java
@@ -4,9 +4,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 import static school.hei.haapi.endpoint.rest.model.EnableStatus.ENABLED;
 import static school.hei.haapi.endpoint.rest.model.EnableStatus.SUSPENDED;
-import static school.hei.haapi.endpoint.rest.model.FeeStatusEnum.LATE;
-import static school.hei.haapi.endpoint.rest.model.FeeTypeEnum.HARDWARE;
-import static school.hei.haapi.endpoint.rest.model.FeeTypeEnum.TUITION;
 import static school.hei.haapi.integration.conf.FakeDataProvider.someLateFee;
 import static school.hei.haapi.integration.conf.FakeDataProvider.someMpbs;
 import static school.hei.haapi.integration.conf.FakeDataProvider.someStudent;
@@ -15,18 +12,13 @@ import static school.hei.haapi.integration.conf.TestUtils.anAvailableRandomPort;
 import static school.hei.haapi.integration.conf.TestUtils.setUpCognitoAndCasdoor;
 import static school.hei.haapi.integration.conf.TestUtils.setUpEventBridge;
 import static school.hei.haapi.integration.conf.TestUtils.setUpS3Service;
-import static school.hei.haapi.model.User.Sex.F;
-import static school.hei.haapi.model.User.Sex.M;
 
-import java.time.Instant;
-import java.time.LocalDate;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import school.hei.haapi.endpoint.rest.api.UsersApi;
@@ -52,6 +44,7 @@ class PaymentServiceTest extends MockedThirdParties {
   @Autowired private UserRepository userRepository;
   @Autowired private FeeRepository feeRepository;
 
+  private User student;
   private User studentSuspend;
   private Fee feeSuspendStudent;
 
@@ -61,6 +54,7 @@ class PaymentServiceTest extends MockedThirdParties {
     setUpS3Service(fileService, TestUtils.student1());
     setUpEventBridge(eventBridgeClientMock);
 
+    student = userRepository.save(someStudent());
     studentSuspend = userRepository.save(someStudent(User.Status.SUSPENDED));
     feeSuspendStudent = feeRepository.save(someLateFee(studentSuspend, 1_000_000));
   }
@@ -91,10 +85,9 @@ class PaymentServiceTest extends MockedThirdParties {
   }
 
   @Test
-  @DirtiesContext
   void compute_user_status_after_paying_fee_ok() {
-    User userWithUnpaidFees = student2();
-    User userWithoutUnpaidFees = student3();
+    var userWithUnpaidFees = studentSuspend;
+    var userWithoutUnpaidFees = student;
 
     subject.computeUserStatusAfterPayingFee(userWithUnpaidFees);
     subject.computeUserStatusAfterPayingFee(userWithoutUnpaidFees);
@@ -105,90 +98,12 @@ class PaymentServiceTest extends MockedThirdParties {
     assertEquals(User.Status.ENABLED, updatedUserWithoutUnpaidFees.getStatus());
 
     // here student2 has paid all their fees late
-    subject.computeRemainingAmount(student2UnpaidFee1().getId(), 5000);
-    subject.computeRemainingAmount(student2UnpaidFee2().getId(), 5000);
+    subject.computeRemainingAmount(
+        feeSuspendStudent.getId(), feeSuspendStudent.getRemainingAmount());
     subject.computeUserStatusAfterPayingFee(userWithUnpaidFees);
     User userPaidAllLateFees = userService.getById(userWithUnpaidFees.getId());
 
     assertEquals(User.Status.ENABLED, userPaidAllLateFees.getStatus());
-  }
-
-  public static Fee student2UnpaidFee1() {
-    return Fee.builder()
-        .id("fee4_id")
-        .student(student2())
-        .type(TUITION)
-        .comment("Comment")
-        .remainingAmount(5000)
-        .totalAmount(5000)
-        .status(LATE)
-        .creationDatetime(Instant.parse("2021-11-08T08:25:24.00Z"))
-        .dueDatetime(Instant.parse("2023-02-08T08:30:24.00Z"))
-        .updatedAt(Instant.parse("2021-12-09T08:25:25.00Z"))
-        .build();
-  }
-
-  public static Fee student2UnpaidFee2() {
-    return Fee.builder()
-        .id("fee5_id")
-        .student(student2())
-        .type(HARDWARE)
-        .comment("Comment")
-        .remainingAmount(5000)
-        .totalAmount(5000)
-        .status(LATE)
-        .creationDatetime(Instant.parse("2021-11-08T08:25:24.00Z"))
-        .dueDatetime(Instant.parse("2023-02-08T08:30:24.00Z"))
-        .updatedAt(Instant.parse("2021-12-08T08:25:25.00Z"))
-        .build();
-  }
-
-  public static User student1() {
-    User student1 = new User();
-    student1.setId("student1_id");
-    student1.setFirstName("Ryan");
-    student1.setLastName("Andria");
-    student1.setEmail("test+ryan@hei.school");
-    student1.setRef("STD21001");
-    student1.setStatus(User.Status.ENABLED);
-    student1.setSex(M);
-    student1.setBirthDate(LocalDate.parse("2000-01-01"));
-    student1.setEntranceDatetime(Instant.now());
-    student1.setPhone("0123456789");
-    student1.setAddress("Example Address");
-    return student1;
-  }
-
-  public static User student2() {
-    User student2 = new User();
-    student2.setId("student2_id");
-    student2.setFirstName("Two");
-    student2.setLastName("Student");
-    student2.setEmail("test+student2@hei.school");
-    student2.setRef("STD21002");
-    student2.setStatus(User.Status.ENABLED);
-    student2.setSex(F);
-    student2.setBirthDate(LocalDate.parse("2000-01-02"));
-    student2.setEntranceDatetime(Instant.now());
-    student2.setPhone("0322411124");
-    student2.setAddress("Adr 2");
-    return student2;
-  }
-
-  public static User student3() {
-    User student2 = new User();
-    student2.setId("student3_id");
-    student2.setFirstName("Three");
-    student2.setLastName("Student");
-    student2.setEmail("test+student3@hei.school");
-    student2.setRef("STD21003");
-    student2.setStatus(User.Status.ENABLED);
-    student2.setSex(F);
-    student2.setBirthDate(LocalDate.parse("2000-01-02"));
-    student2.setEntranceDatetime(Instant.now());
-    student2.setPhone("0322411124");
-    student2.setAddress("Adr 2");
-    return student2;
   }
 
   private static ApiClient anApiClient(String token) {
