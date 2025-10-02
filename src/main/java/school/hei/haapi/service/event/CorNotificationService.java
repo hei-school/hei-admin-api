f08b0ea82af29f44ea925fb59d50f783f96eda19
tendryAxel <hei.tendry@gmail.com>
2025-10-02 11:35:37 +0300

    chore: notify WIP

--- src/main/java/school/hei/haapi/service/event/CorNotificationService.java
+++ src/main/java/school/hei/haapi/service/event/CorNotificationService.java
@@ -1,12 +1,17 @@
 package school.hei.haapi.service.event;
 
+import static school.hei.haapi.service.utils.DataFormatterUtils.instantToCommonDate;
+import static school.hei.haapi.service.utils.TemplateUtils.htmlToString;
+
 import jakarta.mail.internet.AddressException;
 import jakarta.mail.internet.InternetAddress;
+import java.time.temporal.ChronoField;
 import java.util.List;
 import java.util.function.Consumer;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
 import school.hei.haapi.endpoint.event.model.CorNotification;
 import school.hei.haapi.mail.Email;
 import school.hei.haapi.mail.Mailer;
@@ -20,8 +25,27 @@ public class CorNotificationService implements Consumer<CorNotification> {
 
   @Override
   public void accept(CorNotification corNotification) {
+    var htmlBody = htmlToString("corNotification", getMailContext(corNotification));
     mailer.accept(
-        new Email(getDestinationEmail(corNotification), List.of(), List.of(), "", "", List.of()));
+        new Email(
+            getDestinationEmail(corNotification),
+            List.of(),
+            List.of(),
+            "[COR] Convocation des parents",
+            htmlBody,
+            List.of()));
+  }
+
+  private Context getMailContext(CorNotification corNotification) {
+    var interviewDate = corNotification.getCor().getInterviewDate();
+    if (interviewDate == null) {
+      throw new BadRequestException("Interview date is null");
+    }
+    var initial = new Context();
+    initial.setVariable("description", corNotification.getCor().getDescription());
+    initial.setVariable("date", instantToCommonDate(interviewDate));
+    initial.setVariable("hour", interviewDate.get(ChronoField.HOUR_OF_DAY));
+    return initial;
   }
 
   private static InternetAddress getDestinationEmail(CorNotification corNotification) {
