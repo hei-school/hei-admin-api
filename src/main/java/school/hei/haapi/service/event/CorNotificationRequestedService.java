d9bcd363e37540398bdc73c4103577aa28b78530
tendryAxel <hei.tendry@gmail.com>
2025-10-10 14:26:33 +0300

    chore: CorNotificationRequestedService throw runtime server exception instead of client exception

--- src/main/java/school/hei/haapi/service/event/CorNotificationRequestedService.java
+++ src/main/java/school/hei/haapi/service/event/CorNotificationRequestedService.java
@@ -19,7 +19,6 @@ import school.hei.haapi.mail.Email;
 import school.hei.haapi.mail.Mailer;
 import school.hei.haapi.model.Cor;
 import school.hei.haapi.model.User;
-import school.hei.haapi.model.exception.BadRequestException;
 import school.hei.haapi.service.CorService;
 
 @Slf4j
@@ -36,7 +35,7 @@ public class CorNotificationRequestedService implements Consumer<CorNotification
     var htmlBody = htmlToString("corNotification", getMailContext(cor));
     mailer.accept(
         new Email(
-            getDestinationEmail(cor),
+            getEmailFromUser(cor.getStudent()),
             List.of(),
             getInterviewerEmails(cor),
             "[COR] Convocation des parents",
@@ -58,7 +57,7 @@ public class CorNotificationRequestedService implements Consumer<CorNotification
   private static LocalDateTime getInteviewLocalDateTime(Cor cor) {
     var interviewInstant = cor.getInterviewDatetime();
     if (interviewInstant == null) {
-      throw new BadRequestException(
+      throw new IllegalArgumentException(
           "Interview date for the cor with id #%s is null".formatted(cor.getId()));
     }
     return LocalDateTime.ofInstant(interviewInstant, UTC3);
@@ -68,16 +67,6 @@ public class CorNotificationRequestedService implements Consumer<CorNotification
     return "%dh%d".formatted(insertViewDate.getHour(), insertViewDate.getMinute());
   }
 
-  private static InternetAddress getDestinationEmail(Cor cor) {
-    var concernedStudent = cor.getStudent();
-    if (concernedStudent == null) {
-      throw new BadRequestException(
-          "Concerned student for the cor with id #%s is null".formatted(cor.getId()));
-    }
-
-    return getEmailFromUserIdentifier(concernedStudent);
-  }
-
   private static List<InternetAddress> getInterviewerEmails(Cor cor) {
     var interviewers = cor.getInterviewers();
     if (interviewers == null) {
@@ -85,31 +74,28 @@ public class CorNotificationRequestedService implements Consumer<CorNotification
     }
 
     return interviewers.stream()
-        .map(CorNotificationRequestedService::findEmailFromUserIdentifier)
+        .map(CorNotificationRequestedService::findEmailFromUser)
         .filter(Optional::isPresent)
         .map(Optional::get)
         .toList();
   }
 
-  private static Optional<InternetAddress> findEmailFromUserIdentifier(User user) {
+  private static Optional<InternetAddress> findEmailFromUser(User user) {
     try {
-      return Optional.of(getEmailFromUserIdentifier(user));
-    } catch (BadRequestException e) {
+      return Optional.of(getEmailFromUser(user));
+    } catch (IllegalArgumentException e) {
       return Optional.empty();
     }
   }
 
-  private static InternetAddress getEmailFromUserIdentifier(User user) {
+  private static InternetAddress getEmailFromUser(User user) {
     var email = user.getEmail();
-    if (email == null) {
-      throw new BadRequestException(
-          "Email of the user with id #%s is null".formatted(user.getId()));
-    }
 
     try {
       return new InternetAddress(email);
     } catch (AddressException e) {
-      throw new BadRequestException("Email # " + email + "is not a valid address\n" + e);
+      throw new IllegalArgumentException(
+          "Email # %s is not a valid address\n%s".formatted(email, e));
     }
   }
 }
