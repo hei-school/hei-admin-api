4673c4e27d9e4ce688f40b3794c440305dc43b3f
tendryAxel <hei.tendry@gmail.com>
2025-10-16 14:14:43 +0300

    chore: CorNotificationRequested default empty constructor

--- src/main/java/school/hei/haapi/endpoint/event/model/CorNotificationRequested.java
+++ src/main/java/school/hei/haapi/endpoint/event/model/CorNotificationRequested.java
@@ -3,12 +3,18 @@ package school.hei.haapi.endpoint.event.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.Duration;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+@Data
 @Getter
+@Builder
 @EqualsAndHashCode
+@NoArgsConstructor
 @AllArgsConstructor
 @ToString(callSuper = true)
 public class CorNotificationRequested extends PojaEvent {
