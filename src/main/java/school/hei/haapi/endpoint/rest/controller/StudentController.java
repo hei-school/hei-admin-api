c025321fc6b90485fb6c403167c36bc56dd7b344
kirisaki-vk <kirisaki.vk@gmail.com>
2025-10-23 14:42:51 +0300

    fix: student import xlsx multipart form handling

--- src/main/java/school/hei/haapi/endpoint/rest/controller/StudentController.java
+++ src/main/java/school/hei/haapi/endpoint/rest/controller/StudentController.java
@@ -153,7 +153,7 @@ public class StudentController {
   @PostMapping(value = "students/import", consumes = MULTIPART_FORM_DATA_VALUE)
   public StudentImportValidationResult importStudents(
       @RequestParam("due_datetime") Instant dueDatetime,
-      @RequestParam("file_to_upload") MultipartFile fileToUpload) {
+      @RequestPart("file_to_upload") MultipartFile fileToUpload) {
     return userService.initStudentImportFromXlsx(fileConverter.apply(fileToUpload), dueDatetime);
   }
 }
