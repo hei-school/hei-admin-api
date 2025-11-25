516dd7d8749516d95516c3615b5f20bdf83ebddc
DyferHerioss <freddyandriamanohiniaina@gmail.com>
2025-11-25 14:53:40 +0300

    fix: test handle grade import

--- src/test/java/school/hei/haapi/service/GradeImportTest.java
+++ src/test/java/school/hei/haapi/service/GradeImportTest.java
@@ -7,22 +7,50 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.when;
 import static school.hei.haapi.integration.conf.TestUtils.getMockedFile;
+import static school.hei.haapi.integration.test_data.CourseAssignmentTestData.createCourseAssignment;
+import static school.hei.haapi.integration.test_data.CourseTestData.prog1;
+import static school.hei.haapi.integration.test_data.CourseTestData.prog2;
+import static school.hei.haapi.integration.test_data.ExamTestData.createExam;
+import static school.hei.haapi.integration.test_data.GroupTestData.createGroupFlow;
+import static school.hei.haapi.integration.test_data.GroupTestData.g1;
+import static school.hei.haapi.integration.test_data.GroupTestData.g2;
+import static school.hei.haapi.integration.test_data.MonitorTestData.monitorOfAxel;
+import static school.hei.haapi.integration.test_data.MonitorTestData.monitorOfTolojanahary;
+import static school.hei.haapi.integration.test_data.StudentTestData.axel;
+import static school.hei.haapi.integration.test_data.StudentTestData.tolojanahary;
+import static school.hei.haapi.integration.test_data.TeacherTestData.toky;
 
+import java.time.Instant;
 import java.util.List;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import school.hei.haapi.endpoint.event.EventProducer;
 import school.hei.haapi.endpoint.event.model.GradeImportEvent;
+import school.hei.haapi.endpoint.rest.mapper.GradeMapper;
 import school.hei.haapi.endpoint.rest.security.AuthProvider;
 import school.hei.haapi.endpoint.rest.security.model.Principal;
 import school.hei.haapi.file.bucket.BucketComponent;
 import school.hei.haapi.integration.conf.FacadeITMockedThirdParties;
 import school.hei.haapi.mail.Mailer;
+import school.hei.haapi.model.Course;
+import school.hei.haapi.model.CourseAssignment;
+import school.hei.haapi.model.Exam;
+import school.hei.haapi.model.Group;
+import school.hei.haapi.model.GroupFlow;
 import school.hei.haapi.model.User;
 import school.hei.haapi.model.dto.GradeImportDto;
 import school.hei.haapi.model.exception.BadRequestException;
+import school.hei.haapi.repository.CourseAssignmentRepository;
+import school.hei.haapi.repository.CourseRepository;
+import school.hei.haapi.repository.ExamRepository;
+import school.hei.haapi.repository.GradeRepository;
+import school.hei.haapi.repository.GroupFlowRepository;
+import school.hei.haapi.repository.GroupRepository;
+import school.hei.haapi.repository.MonitoringStudentRepository;
+import school.hei.haapi.repository.UserRepository;
 
 public class GradeImportTest extends FacadeITMockedThirdParties {
   @Autowired private GradeService subject;
@@ -31,6 +59,70 @@ public class GradeImportTest extends FacadeITMockedThirdParties {
   @MockBean BucketComponent bucketComponent;
   @MockBean private EventProducer eventProducer;
   @Autowired private UserService userService;
+  @Autowired private GradeRepository gradeRepository;
+  @Autowired UserRepository userRepository;
+  @Autowired CourseRepository courseRepository;
+  @Autowired GroupFlowRepository groupFlowRepository;
+  @Autowired GroupRepository groupRepository;
+  @Autowired CourseAssignmentRepository courseAssignmentRepository;
+  @Autowired MonitoringStudentRepository monitoringStudentRepository;
+  @Autowired ExamRepository examRepository;
+  private static User studentAxel;
+  private static User studentTolojanahary;
+  private User monitorOfAxel;
+  private User monitorOfTolojanahary;
+  private Course courseProg1;
+  private Course courseProg2;
+  private User teacherToky;
+  private Exam exam1Prog1;
+  private Exam exam2Prog1;
+  private CourseAssignment assign_prog1_toToky_forGroup1;
+  private CourseAssignment assign_prog2_toToky_forGroup2;
+  private Group groupG1;
+  private Group groupG2;
+  private GroupFlow groupFlowsAxel;
+  private GroupFlow groupFlowsTolojanahary;
+  private static Exam exam1Prog1Saved;
+
+  @BeforeEach
+  public void setUpTestData() {
+    groupG1 = g1();
+    groupG2 = g2();
+    studentAxel = axel();
+    studentTolojanahary = tolojanahary();
+    courseProg1 = prog1();
+    courseProg2 = prog2();
+    teacherToky = toky();
+    assign_prog1_toToky_forGroup1 =
+        createCourseAssignment(courseProg1, teacherToky, List.of(groupG1));
+    assign_prog2_toToky_forGroup2 =
+        createCourseAssignment(courseProg2, teacherToky, List.of(groupG2));
+
+    monitorOfAxel = monitorOfAxel();
+    monitorOfTolojanahary = monitorOfTolojanahary();
+    studentAxel.setMonitors(List.of(monitorOfAxel));
+    studentTolojanahary.setMonitors(List.of(monitorOfTolojanahary));
+    groupFlowsAxel = createGroupFlow(studentAxel, groupG1);
+    groupFlowsTolojanahary = createGroupFlow(studentTolojanahary, groupG1);
+
+    exam1Prog1 = createExam(Instant.parse("2025-07-22T10:15:30Z"), assign_prog1_toToky_forGroup1);
+    exam2Prog1 = createExam(Instant.parse("2025-09-22T10:15:30Z"), assign_prog1_toToky_forGroup1);
+
+    groupRepository.saveAll(List.of(groupG1, groupG2));
+    userRepository.saveAll(List.of(monitorOfAxel, monitorOfTolojanahary));
+    userRepository.saveAll(List.of(studentAxel, studentTolojanahary));
+    monitoringStudentRepository.saveMonitorFollowingStudents(
+        monitorOfAxel.getId(), List.of(studentAxel.getId()));
+    monitoringStudentRepository.saveMonitorFollowingStudents(
+        monitorOfTolojanahary.getId(), List.of(studentTolojanahary.getId()));
+    userRepository.saveAll(List.of(teacherToky));
+    courseRepository.saveAll(List.of(courseProg1, courseProg2));
+    groupFlowRepository.saveAll(List.of(groupFlowsAxel, groupFlowsTolojanahary));
+    courseAssignmentRepository.saveAll(
+        List.of(assign_prog1_toToky_forGroup1, assign_prog2_toToky_forGroup2));
+    exam1Prog1Saved = examRepository.save(exam1Prog1);
+    examRepository.save(exam2Prog1);
+  }
 
   @BeforeAll
   static void setUp() {
@@ -58,7 +150,8 @@ public class GradeImportTest extends FacadeITMockedThirdParties {
   @Test
   void handle_grade_import_xlsx() {
     assertDoesNotThrow(() -> gradeImportEventService.accept(gradeImportEventMock()));
-    assertNotNull(subject.getGradeByExamIdAndStudentRef("exam1_id", "STD21002"));
+    assertNotNull(
+        subject.getGradeByExamIdAndStudentRef(exam1Prog1Saved.getId(), studentAxel.getRef()));
   }
 
   @Test
@@ -72,9 +165,10 @@ public class GradeImportTest extends FacadeITMockedThirdParties {
 
   private static GradeImportEvent gradeImportEventMock() {
     return GradeImportEvent.builder()
-        .examId("exam1_id")
+        .examId(exam1Prog1Saved.getId())
         .coordinatorEmail("test+manager1@hei.school")
-        .grades(List.of(GradeImportDto.builder().ref("STD21002").score(12.5).build()))
+        .grades(List.of(GradeImportDto.builder().ref(studentAxel.getRef()).score(12.5).build(),
+                GradeImportDto.builder().ref(studentTolojanahary.getRef()).score(13.5).build()))
         .build();
   }
 
