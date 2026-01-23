d4a1ae9d7f0abc69304c167a708f0c1b084b9d29
manitra <ma.andriamparani@gmail.com>
2026-01-23 19:12:58 +0300

    chore: rollback to previous state

--- src/main/java/school/hei/haapi/model/dto/UserDto.java
+++ src/main/java/school/hei/haapi/model/dto/UserDto.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import school.hei.haapi.model.User;
-import school.hei.haapi.service.aws.FileService;
 
 @Builder
 @Getter
@@ -16,22 +15,15 @@ public class UserDto {
   private String email;
   private String profilePicture;
   private String ref;
-  private static final long ONE_DAY_DURATION_AS_LONG = 24 * 60 * 60 * 1000L;
 
-  public static UserDto from(User user, FileService fileService) {
-    String profilePictureUrl = null;
-
-    if (user.getProfilePictureKey() != null) {
-      profilePictureUrl =
-          fileService.getPresignedUrl(user.getProfilePictureKey(), ONE_DAY_DURATION_AS_LONG);
-    }
+  public static UserDto from(User user) {
     return UserDto.builder()
         .id(user.getId())
         .firstName(user.getFirstName())
         .lastName(user.getLastName())
         .email(user.getEmail())
         .ref(user.getRef())
-        .profilePicture(profilePictureUrl)
+        .profilePicture(user.getProfilePictureKey())
         .build();
   }
 }
