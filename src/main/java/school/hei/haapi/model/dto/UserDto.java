30846b30df53bfbfa2c2c40404ab3ac4b39ebc68
manitra <ma.andriamparani@gmail.com>
2026-01-23 18:51:45 +0300

    chore: update profilPicture in UserDto

--- src/main/java/school/hei/haapi/model/dto/UserDto.java
+++ src/main/java/school/hei/haapi/model/dto/UserDto.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import school.hei.haapi.model.User;
+import school.hei.haapi.service.aws.FileService;
 
 @Builder
 @Getter
@@ -15,15 +16,22 @@ public class UserDto {
   private String email;
   private String profilePicture;
   private String ref;
+  private static final long ONE_DAY_DURATION_AS_LONG = 24 * 60 * 60 * 1000L;
 
-  public static UserDto from(User user) {
+  public static UserDto from(User user, FileService fileService) {
+    String profilePictureUrl = null;
+
+    if (user.getProfilePictureKey() != null) {
+      profilePictureUrl =
+          fileService.getPresignedUrl(user.getProfilePictureKey(), ONE_DAY_DURATION_AS_LONG);
+    }
     return UserDto.builder()
         .id(user.getId())
         .firstName(user.getFirstName())
         .lastName(user.getLastName())
         .email(user.getEmail())
         .ref(user.getRef())
-        .profilePicture(user.getProfilePictureKey())
+        .profilePicture(profilePictureUrl)
         .build();
   }
 }
