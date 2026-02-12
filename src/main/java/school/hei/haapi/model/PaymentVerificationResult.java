213a10acde803cf8cba35d1e36771ed9c62bc481
Tiavina-Andriamamivony <hei.tiavina.1@gmail.com>
2026-02-12 21:11:53 +0300

    chore: delete PaymentVerificationResult

--- src/main/java/school/hei/haapi/model/PaymentVerificationResult.java
+++
@@ -1,8 +0,0 @@
-package school.hei.haapi.model;
-
-import java.util.List;
-import school.hei.haapi.model.mpbs.Mpbs;
-import school.hei.haapi.model.mpbs.MpbsVerification;
-
-public record PaymentVerificationResult(
-    List<MpbsVerification> verifiedMpbs, List<Mpbs> unverifiedMpbs) {}
