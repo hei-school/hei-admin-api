fc81dcfd477ecdb39cdf77cfa52690055e834c3d
Tiavina-Andriamamivony <hei.tiavina.1@gmail.com>
2026-02-13 13:17:47 +0300

    test: update VolaPspIt  after refactor

--- src/test/java/school/hei/haapi/model/psp/vola/VolaPspIT.java
+++ src/test/java/school/hei/haapi/model/psp/vola/VolaPspIT.java
@@ -1,11 +1,13 @@
 package school.hei.haapi.model.psp.vola;
 
 import static org.junit.Assert.assertEquals;
+import static school.hei.haapi.model.psp.PspType.ORANGE_MONEY;
 import static school.hei.haapi.model.psp.vola.api.gen.client.model.Payment.VerificationStatusEnum.SUCCEEDED;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import school.hei.haapi.integration.conf.FacadeITMockedThirdParties;
+import school.hei.haapi.model.psp.PspType;
 import school.hei.haapi.model.psp.vola.api.VolaClient;
 import school.hei.haapi.model.psp.vola.api.VolaPsp;
 import school.hei.haapi.model.psp.vola.api.gen.client.model.PaymentId;
@@ -22,7 +24,7 @@ class VolaPspIT extends FacadeITMockedThirdParties {
   // Will be used in more tests later
   private final String TEST_EMAIL = "tiavina.3@mail.hei.school";
   private final String TEST_PSP_ID = "MP250917.1604.D33118";
-  private final PaymentId.PspTypeEnum TEST_MOBILE_MONEY_TYPE = PaymentId.PspTypeEnum.ORANGE_MONEY;
+  private final PspType TEST_MOBILE_MONEY_TYPE = ORANGE_MONEY;
 
   @Test
   void read_succeeded_payment() {
