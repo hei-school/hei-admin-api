7d1102d50a24468e46cab317a7e07699815d5b62
DyferHerioss <freddyandriamanohiniaina@gmail.com>
2026-02-18 11:10:55 +0300

    feat(doc): add new endpoint to export all fees

--- src/test/java/school/hei/haapi/service/GradeImportTest.java
+++ src/test/java/school/hei/haapi/service/GradeImportTest.java
@@ -1,7 +1,7 @@
 package school.hei.haapi.service;
 
-import static org.junit.Assert.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static school.hei.haapi.integration.StudentIT.student1;
 import static school.hei.haapi.integration.conf.TestUtils.getMockedFile;
