d919224efb32fc1d2027443783fddeb69593d7bb
Tiavina-Andriamamivony <hei.tiavina.1@gmail.com>
2026-02-20 14:17:40 +0300

    chore: simplify volaPspIT

--- src/test/java/school/hei/haapi/model/psp/vola/VolaPspIT.java
+++ src/test/java/school/hei/haapi/model/psp/vola/VolaPspIT.java
@@ -5,29 +5,23 @@ import static school.hei.haapi.model.psp.PspType.ORANGE_MONEY;
 import static school.hei.haapi.model.psp.vola.api.gen.client.model.Payment.VerificationStatusEnum.SUCCEEDED;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Value;
-import school.hei.haapi.integration.conf.FacadeITMockedThirdParties;
 import school.hei.haapi.model.psp.PspType;
 import school.hei.haapi.model.psp.vola.api.VolaClient;
 import school.hei.haapi.model.psp.vola.api.VolaPsp;
 import school.hei.haapi.model.psp.vola.api.gen.client.model.PaymentId;
 
-class VolaPspIT extends FacadeITMockedThirdParties {
-  private final VolaPsp volaPsp;
-
-  public VolaPspIT(
-      @Value("${vola.api.url}") String volaApiUrl, @Value("${vola.api.key}") String volaApiKey) {
-    VolaClient volaclient = new VolaClient(volaApiUrl, volaApiKey);
-    this.volaPsp = new VolaPsp(volaclient);
-  }
-
-  // Will be used in more tests later
+public class VolaPspIT {
   private final String TEST_EMAIL = "tiavina.3@mail.hei.school";
   private final String TEST_PSP_ID = "MP250917.1604.D33118";
   private final PspType TEST_MOBILE_MONEY_TYPE = ORANGE_MONEY;
 
   @Test
   void read_succeeded_payment() {
+
+    var baseUrl = System.getenv("VOLA_API_URL");
+    var apiKey = System.getenv("VOLA_API_KEY");
+    var volaPsp = new VolaPsp(new VolaClient(baseUrl, apiKey));
+
     var verifiedPayment =
         volaPsp.getPayment(
             PaymentId.builder()
