802233012c427dd2e7e6f81a72481da2e5a86c13
tokyramarozaka <tokyramarozaka@gmail.com>
2026-02-21 01:15:16 +0300

    chore: fix PromotionLevelOutOfRange exception to revert to previous code

--- src/main/java/school/hei/haapi/model/promotion/PromotionLevelOutOfRange.java
+++ src/main/java/school/hei/haapi/model/promotion/PromotionLevelOutOfRange.java
@@ -6,8 +6,6 @@ public class PromotionLevelOutOfRange extends RuntimeException {
   }
 
   public PromotionLevelOutOfRange(int yearOfStudying) {
-    super(
-        "Promotion level out of range, level %s is not part of the %s cycle"
-            .formatted(level, cycle));
+    super("Promotion level out of range, already %d years old".formatted(yearOfStudying));
   }
 }
