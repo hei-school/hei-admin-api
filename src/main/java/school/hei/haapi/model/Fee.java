91f00e65ebd930658ad803d45d9c3d4ffc63d220
manitra <ma.andriamparani@gmail.com>
2026-04-29 23:27:18 +0300

    fix: add @Builder.Default to RoomName and FeeCategory field to preserve default value

--- src/main/java/school/hei/haapi/model/Fee.java
+++ src/main/java/school/hei/haapi/model/Fee.java
@@ -102,6 +102,7 @@ public class Fee implements Serializable {
 
   @JdbcTypeCode(NAMED_ENUM)
   @Enumerated(STRING)
+  @Builder.Default
   private FeeCategory category = UNKNOWN;
 
   @JdbcTypeCode(NAMED_ENUM)
