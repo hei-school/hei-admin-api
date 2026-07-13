ac02c3dbf4d2e3f9b21be101d949d8aaa5944d41
DyferHerioss <freddyandriamanohiniaina@gmail.com>
2026-07-13 20:51:49 +0300

    chore: getStudentGradesByCourse replaced by getStudentGradesByCourseAssignment

--- src/main/java/school/hei/haapi/model/dto/GroupFlowPeriodCourseAssignment.java
+++
@@ -1,7 +0,0 @@
-package school.hei.haapi.model.dto;
-
-import java.util.List;
-import school.hei.haapi.model.CourseAssignment;
-
-public record GroupFlowPeriodCourseAssignment(
-    GroupFlowPeriod groupFlowPeriod, List<CourseAssignment> courseAssignments) {}
