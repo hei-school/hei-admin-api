8e110aa86f4584f6d6f262b93d8377eb536a5826
DyferHerioss <freddyandriamanohiniaina@gmail.com>
2026-08-11 21:55:37 +0300

    feat: reject credit payment

--- src/main/java/school/hei/haapi/endpoint/rest/security/SecurityConf.java
+++ src/main/java/school/hei/haapi/endpoint/rest/security/SecurityConf.java
@@ -1,5 +1,20 @@
 package school.hei.haapi.endpoint.rest.security;
 
+import static org.springframework.http.HttpMethod.DELETE;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.OPTIONS;
+import static org.springframework.http.HttpMethod.PATCH;
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.http.HttpMethod.PUT;
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+import static school.hei.haapi.endpoint.rest.security.model.Role.ADMIN;
+import static school.hei.haapi.endpoint.rest.security.model.Role.MANAGER;
+import static school.hei.haapi.endpoint.rest.security.model.Role.MONITOR;
+import static school.hei.haapi.endpoint.rest.security.model.Role.ORGANIZER;
+import static school.hei.haapi.endpoint.rest.security.model.Role.STAFF_MEMBER;
+import static school.hei.haapi.endpoint.rest.security.model.Role.STUDENT;
+import static school.hei.haapi.endpoint.rest.security.model.Role.TEACHER;
+
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -21,21 +36,6 @@ import school.hei.haapi.model.exception.ForbiddenException;
 import school.hei.haapi.repository.CorRepository;
 import school.hei.haapi.service.CourseAssignmentService;
 import school.hei.haapi.service.MonitoringStudentService;
-
-import static org.springframework.http.HttpMethod.DELETE;
-import static org.springframework.http.HttpMethod.GET;
-import static org.springframework.http.HttpMethod.OPTIONS;
-import static org.springframework.http.HttpMethod.PATCH;
-import static org.springframework.http.HttpMethod.POST;
-import static org.springframework.http.HttpMethod.PUT;
-import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
-import static school.hei.haapi.endpoint.rest.security.model.Role.ADMIN;
-import static school.hei.haapi.endpoint.rest.security.model.Role.MANAGER;
-import static school.hei.haapi.endpoint.rest.security.model.Role.MONITOR;
-import static school.hei.haapi.endpoint.rest.security.model.Role.ORGANIZER;
-import static school.hei.haapi.endpoint.rest.security.model.Role.STAFF_MEMBER;
-import static school.hei.haapi.endpoint.rest.security.model.Role.STUDENT;
-import static school.hei.haapi.endpoint.rest.security.model.Role.TEACHER;
 
 @Configuration
 @Slf4j
@@ -163,6 +163,7 @@ public class SecurityConf {
                     antMatcher(POST, "/students/*/fees/*/payments"),
                     antMatcher(DELETE, "/students/*/fees/*/payments/*"),
                     antMatcher(PATCH, "/students/payments/validate"),
+                    antMatcher(PATCH, "/students/payments/reject"),
                     antMatcher(GET, "/students/credit-payments"),
                     antMatcher(GET, "/students/{student_id}/credit"),
                     antMatcher(GET, "/students/{student_id}/credit-transactions"),
@@ -655,6 +656,8 @@ public class SecurityConf {
                     .requestMatchers(POST, "/students/*/fees/*/payments")
                     .hasAnyRole(MANAGER.getRole(), ADMIN.getRole())
                     .requestMatchers(PATCH, "/students/payments/validate")
+                    .hasAnyRole(MANAGER.getRole(), ADMIN.getRole())
+                    .requestMatchers(PATCH, "/students/payments/reject")
                     .hasAnyRole(MANAGER.getRole(), ADMIN.getRole())
                     .requestMatchers(GET, "/students/credit-payments")
                     .hasAnyRole(MANAGER.getRole(), ADMIN.getRole())
