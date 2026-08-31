49e046afefbc8032a733405297da53e58ffc4519
DyferHerioss <freddyandriamanohiniaina@gmail.com>
2026-08-31 22:13:08 +0300

    fix: student yearly result

--- src/main/java/school/hei/haapi/repository/GroupFlowRepository.java
+++ src/main/java/school/hei/haapi/repository/GroupFlowRepository.java
@@ -2,10 +2,23 @@ package school.hei.haapi.repository;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import school.hei.haapi.endpoint.rest.model.StudentLevel;
 import school.hei.haapi.model.GroupFlow;
 
 @Repository
 public interface GroupFlowRepository extends JpaRepository<GroupFlow, String> {
-  List<GroupFlow> findByStudentId(String studentId);
+  @Query(
+      """
+      SELECT gf FROM GroupFlow gf
+      JOIN gf.group g
+      JOIN CourseAssignment ca ON g MEMBER OF ca.groups
+      JOIN ca.course c
+      WHERE gf.student.id = :student_id
+      AND c.studentLevel = :level
+      """)
+  List<GroupFlow> findByFlowTypeAndStudentAndLevel(
+      @Param("student_id") String studentId, @Param("level") StudentLevel level);
 }
