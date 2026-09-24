6dc8fa159fa6a0f5fd2ac879ea88d58e9a3ecef5
mbomain <ma.andriamparani@gmail.com>
2026-09-24 11:45:59 +0300

    test: increase test coverage

--- src/main/java/school/hei/haapi/service/DocumensoFolderService.java
+++ src/main/java/school/hei/haapi/service/DocumensoFolderService.java
@@ -47,6 +47,7 @@ public class DocumensoFolderService {
         documensoClient.findFolders(parentId, DOCUMENT_FOLDER_TYPE).stream()
             .filter(folder -> name.equals(folder.getName()))
             .findFirst()
+            .map(RemoteFolder::getId)
             .orElseGet(
                 () -> {
                   log.info("Creating Documenso folder {}", path);
